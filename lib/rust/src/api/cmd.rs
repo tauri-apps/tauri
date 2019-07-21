0bdb23151fcbdfb25a7b463d46eb5db712dc7981
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2019-07-21 10:35:45 -0300

    feat(proton) whitelist API features

--- lib/rust/src/api/cmd.rs
+++ lib/rust/src/api/cmd.rs
@@ -1,36 +1,41 @@
 #[derive(Deserialize)]
 #[serde(tag = "cmd", rename_all = "camelCase")]
+#[cfg(feature = "api")]
 pub enum Cmd {
-  Init,
+  #[cfg(any(feature = "all-api", feature = "readAsString"))]
   ReadAsString {
     path: String,
     callback: String,
     error: String,
   },
+  #[cfg(any(feature = "all-api", feature = "readAsBinary"))]
   ReadAsBinary {
     path: String,
     callback: String,
     error: String,
   },
+  #[cfg(any(feature = "all-api", feature = "write"))]
   Write {
     file: String,
     contents: String,
     callback: String,
     error: String,
   },
+  #[cfg(any(feature = "all-api", feature = "list"))]
   List {
     path: String,
     callback: String,
     error: String,
   },
+  #[cfg(any(feature = "all-api", feature = "listDirs"))]
   ListDirs {
     path: String,
     callback: String,
     error: String,
   },
-  SetTitle {
-    title: String,
-  },
+  #[cfg(any(feature = "all-api", feature = "setTitle"))]
+  SetTitle { title: String },
+  #[cfg(any(feature = "all-api", feature = "call"))]
   Call {
     command: String,
     args: Vec<String>,
--- lib/rust/src/api/mod.rs
+++ lib/rust/src/api/mod.rs
@@ -2,62 +2,74 @@ mod cmd;
 
 use proton_ui::WebView;
 
+#[allow(unused_variables)]
 pub fn handler<T: 'static>(webview: &mut WebView<T>, arg: &str) -> bool {
-  use cmd::Cmd::*;
-  match serde_json::from_str(arg) {
-    Err(_) => false,
-    Ok(command) => {
-      match command {
-        Init => (),
-        ReadAsString {
-          path,
-          callback,
-          error,
-        } => {
-          super::file_system::read_text_file(webview, path, callback, error);
+  #[cfg(feature = "api")]
+  {
+    use cmd::Cmd::*;
+    match serde_json::from_str(arg) {
+      Err(_) => false,
+      Ok(command) => {
+        match command {
+          #[cfg(any(feature = "all-api", feature = "readAsString"))]
+          ReadAsString {
+            path,
+            callback,
+            error,
+          } => {
+            super::file_system::read_text_file(webview, path, callback, error);
+          }
+          #[cfg(any(feature = "all-api", feature = "readAsBinary"))]
+          ReadAsBinary {
+            path,
+            callback,
+            error,
+          } => {
+            super::file_system::read_binary_file(webview, path, callback, error);
+          }
+          #[cfg(any(feature = "all-api", feature = "write"))]
+          Write {
+            file,
+            contents,
+            callback,
+            error,
+          } => {
+            super::file_system::write_file(webview, file, contents, callback, error);
+          }
+          #[cfg(any(feature = "all-api", feature = "listDirs"))]
+          ListDirs {
+            path,
+            callback,
+            error,
+          } => {
+            super::file_system::list_dirs(webview, path, callback, error);
+          }
+          #[cfg(any(feature = "all-api", feature = "list"))]
+          List {
+            path,
+            callback,
+            error,
+          } => {
+            super::file_system::list(webview, path, callback, error);
+          }
+          #[cfg(any(feature = "all-api", feature = "setTitle"))]
+          SetTitle { title } => {
+            webview.set_title(&title).unwrap();
+          }
+          #[cfg(any(feature = "all-api", feature = "call"))]
+          Call {
+            command,
+            args,
+            callback,
+            error,
+          } => {
+            super::command::call(webview, command, args, callback, error);
+          }
         }
-        ReadAsBinary {
-          path,
-          callback,
-          error,
-        } => {
-          super::file_system::read_binary_file(webview, path, callback, error);
-        }
-        Write {
-          file,
-          contents,
-          callback,
-          error,
-        } => {
-          super::file_system::write_file(webview, file, contents, callback, error);
-        }
-        ListDirs {
-          path,
-          callback,
-          error,
-        } => {
-          super::file_system::list_dirs(webview, path, callback, error);
-        }
-        List {
-          path,
-          callback,
-          error,
-        } => {
-          super::file_system::list(webview, path, callback, error);
-        }
-        SetTitle { title } => {
-          webview.set_title(&title).unwrap();
-        }
-        Call {
-          command,
-          args,
-          callback,
-          error,
-        } => {
-          super::command::call(webview, command, args, callback, error);
-        }
+        true
       }
-      true
     }
   }
+  #[cfg(not(feature = "api"))]
+  false
 }

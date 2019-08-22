751cb8d8073a11d6df15c8583f2354316f4424fb
Daniel Thompson-Yvetot <denjell@quasar.dev>
2019-08-22 17:16:37 +0200

    feat(tauri) setup tauri object
    Signed-off-by: Daniel Thompson-Yvetot <denjell@quasar.dev>

---
+++ mode/config.js
@@ -0,0 +1,26 @@
+const init = {
+  embeddedServer: {},
+  bundle: {},
+  whitelist: {},
+  window: {},
+  security: {}
+}
+
+const defaultObject = {
+  embeddedServer: {
+    active: true
+  },
+  bundle: {
+    active: true
+  },
+  whitelist: {},
+  window: {
+    title: 'Quasar Tauri App'
+  },
+  security: {
+    csp: 'default-src data: filesystem: ws: http: https: \'unsafe-eval\' \'unsafe-inline\''
+  }
+}
+
+module.exports.init = init
+module.exports.defaultObject = defaultObject

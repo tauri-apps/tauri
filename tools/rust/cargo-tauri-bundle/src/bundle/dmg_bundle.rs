a5ab03bd7dd3a980c884d9a8acc87910e46b4395
tensor-programming <abeltensor@tensor-programming.com>
2019-09-25 13:01:23 -0400

    output both templates

--- tools/rust/cargo-tauri-bundle/src/bundle/dmg_bundle.rs
+++ tools/rust/cargo-tauri-bundle/src/bundle/dmg_bundle.rs
@@ -29,5 +29,29 @@ lazy_static! {
 pub fn bundle_project(settings: &Settings) -> crate::Result<Vec<PathBuf>> {
   let bundle_path = osx_bundle::bundle_project(settings)?;
 
+  let upcase = settings.binary_name().to_uppercase();
+
+  let mut sh_map = BTreeMap::new();
+  sh_map.insert("app_name", settings.binary_name());
+  sh_map.insert("app_name_upcase", &upcase);
+
+  let launch_temp = HANDLEBARS
+    .render("macos_launch", &sh_map)
+    .or_else(|e| Err(e.to_string()))?;
+
+  let bundle_temp = HANDLEBARS
+    .render("bundle_dmg", &sh_map)
+    .or_else(|e| Err(e.to_string()))?;
+
+  let output_path = settings.project_out_directory();
+
+  let launch_sh = output_path.join("macos_launch.sh");
+  let bundle_sh = output_path.join("bundle_dmg.sh");
+
+  common::print_bundling(format!("{:?}", &output_path.join(format!("{}.dmg", &upcase))).as_str())?;
+
+  write(&launch_sh, launch_temp).or_else(|e| Err(e.to_string()))?;
+  write(&bundle_sh, bundle_temp).or_else(|e| Err(e.to_string()))?;
+
   Ok(bundle_path)
 }

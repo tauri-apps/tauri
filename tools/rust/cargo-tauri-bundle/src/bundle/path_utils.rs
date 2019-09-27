f09d2c6de7b82108a0782779c63466452ccafcf6
tensor-programming <abeltensor@tensor-programming.com>
2019-09-27 01:06:38 -0400

    copy file function

--- tools/rust/cargo-tauri-bundle/src/bundle/path_utils.rs
+++ tools/rust/cargo-tauri-bundle/src/bundle/path_utils.rs
@@ -84,3 +84,38 @@ pub fn remove<P: AsRef<Path>>(path: P) -> crate::Result<()> {
     Ok(())
   }
 }
+
+pub fn copy_file<P, Q>(from: P, to: Q, options: &FileOpts) -> crate::Result<u64>
+where
+  P: AsRef<Path>,
+  Q: AsRef<Path>,
+{
+  let from = from.as_ref();
+  if !from.exists() {
+    if let Some(msg) = from.to_str() {
+      let msg = format!("Path \"{}\" does not exist or you don't have access", msg);
+      return Err(msg.into());
+    }
+    return Err("Path does not exist Or you don't have access!".into());
+  }
+
+  if !from.is_file() {
+    if let Some(msg) = from.to_str() {
+      let msg = format!("Path \"{}\" is not a file!", msg);
+      return Err(msg.into());
+    }
+    return Err("Path is not a file!".into());
+  }
+  if !options.overwrite && to.as_ref().exists() {
+    if options.skip {
+      return Ok(0);
+    }
+
+    if let Some(msg) = to.as_ref().to_str() {
+      let msg = format!("Path \"{}\" is exist", msg);
+      return Err(msg.into());
+    }
+  }
+
+  Ok(std::fs::copy(from, to)?)
+}

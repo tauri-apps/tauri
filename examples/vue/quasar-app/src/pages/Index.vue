8007713ce0f78f37e91dae5533b81c4ed9915692
Daniel Thompson-Yvetot <denjell@sfosc.org>
2019-12-01 22:10:24 +0100

    feat(examples): init quasar example

---
+++ examples/vue/quasar-app/src/pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>

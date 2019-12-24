7b628277da29a7ef6e448ddb8046630ce7d4cdf2
Rajiv Shah <rajivshah1@icloud.com>
2019-12-24 16:25:50 -0500

    feat(examples): Add Svelte app example
    Created with `npx degit sveltejs/template svelte-app`

---
+++ examples/svelte/svelte-app/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>

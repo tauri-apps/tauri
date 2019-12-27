16744b9bebfc4ed24bd31fa4689bf9ffde91e32f
tensor-programming <abeltensor@tensor-programming.com>
2019-12-27 15:12:38 -0500

    add proptest for property testing

--- tauri/test/fixture/dev-scripts/setup.ps1
+++ tauri/test/fixture/dev-scripts/setup.ps1
@@ -1,7 +1,9 @@
+Write-Output "Setting up enviromental Variables"
+
 $dist_path = "../dist"
 $src_path = "../src-tauri"
 
 $env:TAURI_DIST_DIR = Resolve-Path $dist_path
 $env:TAURI_DIR = Resolve-Path $src_path 
 
-Write-Output "Finished Setup"
+Write-Output "Ready to work"

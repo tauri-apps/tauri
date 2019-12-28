6a48060c130db0a8ae98c46cfdfe54f93393f4dc
tensor-programming <abeltensor@tensor-programming.com>
2019-12-28 02:43:26 -0500

    add more comments.

--- tauri-api/src/rpc.rs
+++ tauri-api/src/rpc.rs
@@ -43,30 +43,32 @@ mod test {
   // check arbitrary strings in format_callback_result
   #[quickcheck]
   fn test_format_res(result: Result<String, String>, c: String, ec: String) -> bool {
+    // match on result to decide how to call the function.
     match result {
+      // if ok, get length of result and callback strings.
       Ok(r) => {
         let rlen = r.len();
         let clen = c.len();
 
-        if r != "" && c != "" {
-          let resp = format_callback_result(Ok(r), c, ec);
-          let reslen = resp.len();
-          reslen == rlen + clen + 12
-        } else {
-          true
-        }
-      }
-      Err(err) => {
-        if ec != "" {
-          let eclen = ec.len();
-          let errlen = err.len();
-          let resp = format_callback_result(Err(err), c, ec);
-          let reslen = resp.len();
+        // take the ok string from result and pass it into format_callback_result as an ok.
+        let resp = format_callback_result(Ok(r), c, ec);
+        // get response string length
+        let reslen = resp.len();
 
-          reslen == eclen + errlen + 14
-        } else {
-          true
-        }
+        // if response string length equals result and callback length plus 12 characters then it is correct.
+        reslen == rlen + clen + 12
+      }
+      // If Err, get length of Err and Error callback
+      Err(err) => {
+        let eclen = ec.len();
+        let errlen = err.len();
+        // pass err as Err into format_callback_result with callback and error callback
+        let resp = format_callback_result(Err(err), c, ec);
+        // get response string length
+        let reslen = resp.len();
+
+        // if length of response string equals the error length and the error callback length plus 14 characters then its is correct.
+        reslen == eclen + errlen + 14
       }
     }
   }

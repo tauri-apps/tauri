1daa01e76cc9c64a710944272430a282694ab23d
Tensor-Programming <tensordeveloper@gmail.com>
2019-12-29 18:13:08 -0500

    Refactor-feat(core) Refactor the code and add proptesting to the core library (#242)
    * fix updater doc test.
    
    * add basic proptest to tauri-core
    
    * add proptest data to gitignore
    
    * add dummy func
    
    * increased regex to all character types
    
    * update vue spa listen/emit API to use Strings.
    
    * add listeners_check_fn test
    
    * fix mispelling.
    
    * add on_event test
    
    * add basic dummy test for spawn.

--- examples/vue/quasar-app/src-tauri/src/main.rs
+++ examples/vue/quasar-app/src-tauri/src/main.rs
@@ -13,19 +13,23 @@ fn main() {
   tauri::AppBuilder::new()
     .setup(|_webview| {
       let handle = _webview.handle();
-      tauri::event::listen("hello", move |msg| {
+      tauri::event::listen(String::from("hello"), move |msg| {
         #[derive(Serialize)]
         pub struct Reply {
           pub msg: String,
-          pub rep: String
+          pub rep: String,
         }
 
         let reply = Reply {
           msg: format!("{}", msg).to_string(),
-          rep: "something else".to_string()
+          rep: "something else".to_string(),
         };
 
-        tauri::event::emit(&handle, "reply",  serde_json::to_string(&reply).unwrap());
+        tauri::event::emit(
+          &handle,
+          String::from("reply"),
+          serde_json::to_string(&reply).unwrap(),
+        );
 
         println!("Message from emit:hello => {}", msg);
       });

f7e5fcff9534c39043d7915df43b22ab00042abe
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2019-12-29 12:20:07 -0300

    Update entry.ts

--- cli/tauri.js/src/entry.ts
+++ cli/tauri.js/src/entry.ts
@@ -6,8 +6,8 @@ import { TauriConfig } from './types/config'
 export const generate = (outDir: string, cfg: TauriConfig): void => {
   // this MUST be from the templates repo
   const apiTemplate = require('../templates/tauri.js').default
-  const apiContent = template(apiTemplate)
+  const compiledApi = template(apiTemplate)
 
   ensureDirSync(outDir)
-  writeFileSync(path.join(outDir, 'tauri.js'), apiContent(cfg), 'utf-8')
+  writeFileSync(path.join(outDir, 'tauri.js'), compiledApi(cfg), 'utf-8')
 }

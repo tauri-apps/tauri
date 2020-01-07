c0ae1b07164504d5f64494d0f3ebaaf0de0703d2
tensor-programming <tensordeveloper@gmail.com>
2020-01-06 22:44:29 -0500

    line to rebuild on a change to the dist folder.

--- tauri/build.rs
+++ tauri/build.rs
@@ -4,13 +4,12 @@ extern crate tauri_includedir_codegen;
 pub fn main() {
   #[cfg(not(feature = "dev-server"))]
   {
+    println!("cargo:rerun-if-changed={}", std::env::var("TAURI_DIST_DIR"));
     match std::env::var("TAURI_DIST_DIR") {
       Ok(dist_path) => {
         let inlined_assets = match std::env::var("TAURI_INLINED_ASSETS") {
-          Ok(assets) => {
-            assets.split("|").map(|s| s.to_string()).collect()
-          }
-          Err(_) => Vec::new()
+          Ok(assets) => assets.split("|").map(|s| s.to_string()).collect(),
+          Err(_) => Vec::new(),
         };
         // include assets
         tauri_includedir_codegen::start("ASSETS")

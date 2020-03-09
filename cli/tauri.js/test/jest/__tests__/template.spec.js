499a6a9f68ed602fba2bd7c3d72726c30f1fa912
Lucas Nogueira <lucas@tauri.studio>
2020-03-09 15:11:32 -0300

    chore(test) try with done cb

--- cli/tauri.js/test/jest/__tests__/template.spec.js
+++ cli/tauri.js/test/jest/__tests__/template.spec.js
@@ -3,29 +3,35 @@ const { resolve } = require('path')
 const { rmdirSync, existsSync, writeFileSync, readFileSync } = require('fs')
 
 describe('[CLI] tauri.js template', () => {
-  it('init a project and builds it', async () => {
-    const cwd = process.cwd()
-    const fixturePath = resolve(__dirname, '../fixtures/empty')
-    const tauriFixturePath = resolve(fixturePath, 'src-tauri')
+  it('init a project and builds it', done => {
+    try {
+      const cwd = process.cwd()
+      const fixturePath = resolve(__dirname, '../fixtures/empty')
+      const tauriFixturePath = resolve(fixturePath, 'src-tauri')
 
-    fixtureSetup.initJest('empty')
+      fixtureSetup.initJest('empty')
 
-    process.chdir(fixturePath)
+      process.chdir(fixturePath)
 
-    if (existsSync(tauriFixturePath)) {
-      rmdirSync(tauriFixturePath, { recursive: true })
+      if (existsSync(tauriFixturePath)) {
+        rmdirSync(tauriFixturePath, { recursive: true })
+      }
+
+      const { tauri } = require('bin/tauri')
+      tauri('init')
+      process.chdir(tauriFixturePath)
+
+      const manifestPath = resolve(tauriFixturePath, 'Cargo.toml')
+      const manifestFile = readFileSync(manifestPath).toString()
+      writeFileSync(manifestPath, `workspace = { }\n\n${manifestFile}`)
+    } catch (e) {
+      done(e)
     }
-  
-    const { tauri } = require('bin/tauri')
-    tauri('init')
-    process.chdir(tauriFixturePath)
-
-    const manifestPath = resolve(tauriFixturePath, 'Cargo.toml')
-    const manifestFile = readFileSync(manifestPath).toString()
-    writeFileSync(manifestPath, `workspace = { }\n\n${manifestFile}`)
 
     const build = require('api/build')
-    await build().promise
-    process.chdir(cwd)
+    build().promise.then(() => {
+      process.chdir(cwd)
+      done()
+    }).catch(done)
   })
 })

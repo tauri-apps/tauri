7cb5b598b28f338085072b4c4e9b14174334b6db
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2020-03-16 10:46:10 +0700

    refactor(tauri): use Default trait

--- tauri/src/config.rs
+++ tauri/src/config.rs
@@ -3,117 +3,63 @@ use serde::Deserialize;
 use std::env;
 
 #[derive(PartialEq, Deserialize, Clone, Debug)]
-#[serde(tag = "window", rename_all = "camelCase")]
+#[serde(default, tag = "window", rename_all = "camelCase")]
 pub struct WindowConfig {
-  #[serde(default = "default_width")]
   pub width: i32,
-  #[serde(default = "default_height")]
   pub height: i32,
-  #[serde(default = "default_resizable")]
   pub resizable: bool,
-  #[serde(default = "default_frameless")]
   pub frameless: bool,
-  #[serde(default = "default_title")]
-  pub title: String,
-  #[serde(default)]
   pub fullscreen: bool,
+  pub title: String,
 }
 
-fn default_width() -> i32 {
-  800
-}
-
-fn default_height() -> i32 {
-  600
-}
-
-fn default_resizable() -> bool {
-  true
-}
-
-fn default_frameless() -> bool {
-  false
-}
-
-fn default_title() -> String {
-  "Tauri App".to_string()
-}
-
-fn default_window() -> WindowConfig {
-  WindowConfig {
-    width: default_width(),
-    height: default_height(),
-    resizable: default_resizable(),
-    frameless: default_frameless(),
-    title: default_title(),
-    fullscreen: false,
+impl Default for WindowConfig {
+  fn default() -> Self {
+    Self {
+      width: 800,
+      height: 600,
+      resizable: true,
+      frameless: false,
+      fullscreen: false,
+      title: "Tauri App".to_string(),
+    }
   }
 }
 
 #[derive(PartialEq, Deserialize, Clone, Debug)]
-#[serde(tag = "embeddedServer", rename_all = "camelCase")]
+#[serde(default, tag = "embeddedServer", rename_all = "camelCase")]
 pub struct EmbeddedServerConfig {
-  #[serde(default = "default_host")]
   pub host: String,
-  #[serde(default = "default_port")]
   pub port: String,
 }
 
-fn default_host() -> String {
-  "http://127.0.0.1".to_string()
-}
-
-fn default_port() -> String {
-  "random".to_string()
-}
-
-fn default_embedded_server() -> EmbeddedServerConfig {
-  EmbeddedServerConfig {
-    host: default_host(),
-    port: default_port(),
+impl Default for EmbeddedServerConfig {
+  fn default() -> Self {
+    Self {
+      host: "http://127.0.0.1".to_string(),
+      port: "random".to_string(),
+    }
   }
 }
 
-#[derive(PartialEq, Deserialize, Clone, Debug)]
-#[serde(tag = "tauri", rename_all = "camelCase")]
+#[derive(PartialEq, Default, Deserialize, Clone, Debug)]
+#[serde(default, tag = "tauri", rename_all = "camelCase")]
 pub struct TauriConfig {
-  #[serde(default = "default_window")]
   pub window: WindowConfig,
-  #[serde(default = "default_embedded_server")]
   pub embedded_server: EmbeddedServerConfig,
 }
 
-#[derive(PartialEq, Deserialize, Clone, Debug)]
-#[serde(tag = "build", rename_all = "camelCase")]
+#[derive(PartialEq, Default, Deserialize, Clone, Debug)]
+#[serde(default, tag = "build", rename_all = "camelCase")]
 pub struct BuildConfig {
-  #[serde(default = "default_dev_path")]
   pub dev_path: String,
 }
 
-fn default_dev_path() -> String {
-  "".to_string()
-}
-
-#[derive(PartialEq, Deserialize, Clone, Debug)]
-#[serde(rename_all = "camelCase")]
+#[derive(PartialEq, Default, Deserialize, Clone, Debug)]
+#[serde(default, rename_all = "camelCase")]
 pub struct Config {
-  #[serde(default = "default_tauri")]
   pub tauri: TauriConfig,
-  #[serde(default = "default_build")]
   pub build: BuildConfig,
-}
-
-fn default_tauri() -> TauriConfig {
-  TauriConfig {
-    window: default_window(),
-    embedded_server: default_embedded_server(),
-  }
-}
-
-fn default_build() -> BuildConfig {
-  BuildConfig {
-    dev_path: default_dev_path(),
-  }
 }
 
 pub fn get() -> crate::Result<Config> {

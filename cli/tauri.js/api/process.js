313f9697f01e3d4dc3c189cce8d8fcf3b7d1d39b
Lucas Nogueira <lucas@tauri.studio>
2020-03-29 18:45:34 -0300

    docs(api) fix JSDoc types

--- cli/tauri.js/api/process.js
+++ cli/tauri.js/api/process.js
@@ -3,9 +3,9 @@ import tauri from './tauri'
 /**
  * spawns a process
  *
- * @param {string} command the name of the cmd to execute e.g. 'mkdir' or 'node'
- * @param {(string[]|string)} [args] command args
- * @return {Promise<string>} promise resolving to the stdout text
+ * @param {String} command the name of the cmd to execute e.g. 'mkdir' or 'node'
+ * @param {(String[]|String)} [args] command args
+ * @return {Promise<String>} promise resolving to the stdout text
  */
 function execute (command, args) {
   return tauri.execute(command, args)

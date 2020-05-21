5efc1303a66ac79478af28d35b53b92fbedfc9f6
YuWeiWu <wusyong9104@gmail.com>
2020-05-21 22:38:40 +0800

    Update files by building with tarui build

--- tauri/examples/communication/src-tauri/tauri.js
+++ tauri/examples/communication/src-tauri/tauri.js
@@ -18,6 +18,40 @@
  * and also whitelist them based upon the developer's settings.
  */
 
+// polyfills
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function (searchString, position) {
+    position = position || 0
+    return this.substr(position, searchString.length) === searchString
+  }
+}
+
+// makes the window.external.invoke API available after window.location.href changes
+
+switch (navigator.platform) {
+  case "Macintosh":
+  case "MacPPC":
+  case "MacIntel":
+  case "Mac68K":
+    window.external = this
+    invoke = function (x) {
+      webkit.messageHandlers.invoke.postMessage(x);
+    }
+    break;
+  case "Windows":
+  case "WinCE":
+  case "Win32":
+  case "Win64":
+    break;
+  default: 
+    window.external = this
+    invoke = function (x) {
+      window.webkit.messageHandlers.external.postMessage(x);
+    }
+    break;
+}
+
+
 function s4() {
   return Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
@@ -38,6 +72,33 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+/**
+ * @typedef {number} BaseDirectory
+ */
+/**
+ * @enum {BaseDirectory}
+ */
+var Dir = {
+  Audio: 1,
+  Cache: 2,
+  Config: 3, 
+  Data: 4,
+  LocalData: 5,
+  Desktop: 6,
+  Document: 7,
+  Download: 8,
+  Executable: 9,
+  Font: 10,
+  Home: 11,
+  Picture: 12,
+  Public: 13,
+  Runtime: 14,
+  Template: 15,
+  Video: 16,
+  Resource: 17,
+  App: 18
+}
+
 
 
 
@@ -48,6 +109,7 @@ var __reject = function () {
 }
 
 window.tauri = {
+  Dir: Dir,
   
   invoke: function invoke(args) {
     window.external.invoke(JSON.stringify(args));
@@ -56,23 +118,31 @@ window.tauri = {
   
   listen: function listen(event, handler) {
     
-      
-        return __reject()
+    var once = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+      this.invoke({
+        cmd: 'listen',
+        event: event,
+        handler: window.tauri.transformCallback(handler, once),
+        once: once
+      });
     
   },
 
   
   emit: function emit(evt, payload) {
     
-      
-        return __reject()
+      this.invoke({
+        cmd: 'emit',
+        event: evt,
+        payload: payload
+      });
     
   },
 
   
   transformCallback: function transformCallback(callback) {
     var once = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
-    var identifier = Object.freeze(uid());
+    var identifier = uid();
 
     window[identifier] = function (result) {
       if (once) {
@@ -98,83 +168,183 @@ window.tauri = {
   },
 
   
-  readTextFile: function readTextFile(path) {
+  readTextFile: function readTextFile(path, options) {
     
-      
-        return __reject()
-    
-  },
-
-  
-  readBinaryFile: function readBinaryFile(path) {
-    
-      
-        return __reject()
+      return this.promisified({
+        cmd: 'readTextFile',
+        path: path,
+        options: options
+      });
     
   },
 
   
-  writeFile: function writeFile(cfg) {
+  readBinaryFile: function readBinaryFile(path, options) {
     
-      
-        return __reject()
-    
-  },
-
-  
-  listFiles: function listFiles(path) {
-    
-      
-        return __reject()
+      return this.promisified({
+        cmd: 'readBinaryFile',
+        path: path,
+        options: options
+      });
     
   },
 
   
-  listDirs: function listDirs(path) {
+  writeFile: function writeFile(cfg, options) {
     
-      
-        return __reject()
+      if (_typeof(cfg) === 'object') {
+        Object.freeze(cfg);
+      }
+      return this.promisified({
+        cmd: 'writeFile',
+        file: cfg.file,
+        contents: cfg.contents,
+        options: options
+      });
+    
+  },
+
+  
+  readDir: function readDir(path, options) {
+    
+      return this.promisified({
+        cmd: 'readDir',
+        path: path,
+        options: options
+      });
+    
+  },
+
+  
+  createDir: function createDir(path, options) {
+    
+      return this.promisified({
+        cmd: 'createDir',
+        path: path,
+        options: options
+      });
+    
+  },
+
+  
+  removeDir: function removeDir(path, options) {
+    
+      return this.promisified({
+        cmd: 'removeDir',
+        path: path,
+        options: options
+      });
+    
+  },
+
+  
+  copyFile: function copyFile(source, destination, options) {
+    
+      return this.promisified({
+        cmd: 'copyFile',
+        source: source,
+        destination: destination,
+        options: options
+      });
+    
+  },
+
+  
+  removeFile: function removeFile(path, options) {
+    
+      return this.promisified({
+        cmd: 'removeFile',
+        path: path,
+        options: options
+      });
+    
+  },
+
+  
+  renameFile: function renameFile(oldPath, newPath, options) {
+    
+      return this.promisified({
+        cmd: 'renameFile',
+        old_path: oldPath,
+        new_path: newPath,
+        options: options
+      });
     
   },
 
   
   setTitle: function setTitle(title) {
     
-      
-    return __reject()
+      this.invoke({
+        cmd: 'setTitle',
+        title: title
+      });
     
   },
 
   
   open: function open(uri) {
     
+      this.invoke({
+        cmd: 'open',
+        uri: uri
+      });
     
-    return __reject()
-      
   },
 
   
   execute: function execute(command, args) {
     
-      
-        return __reject()
+
+      if (_typeof(args) === 'object') {
+        Object.freeze(args);
+      }
+
+      return this.promisified({
+        cmd: 'execute',
+        command: command,
+        args: typeof args === 'string' ? [args] : args
+      });
     
   },
 
-  bridge: function bridge(command, payload) {
+  
+  openDialog: function openDialog(options) {
     
-      
-            return __reject()
+      var opts = options || {}
+      if (_typeof(options) === 'object') {
+        opts.default_path = opts.defaultPath
+        Object.freeze(options);
+      }
+      return this.promisified({
+        cmd: 'openDialog',
+        options: opts
+      });
     
   },
 
-  loadAsset: function loadAsset(assetName, assetType) {
-    return this.promisified({
-      cmd: 'loadAsset',
-      asset: assetName,
-      asset_type: assetType || 'unknown'
-    })
-  }
+  
+  saveDialog: function saveDialog(options) {
+    
+      var opts = options || {}
+      if (_typeof(options) === 'object') {
+        opts.default_path = opts.defaultPath
+        Object.freeze(options);
+      }
+      return this.promisified({
+        cmd: 'saveDialog',
+        options: opts
+      });
+    
+  },
+
+loadAsset: function loadAsset(assetName, assetType) {
+  return this.promisified({
+    cmd: 'loadAsset',
+    asset: assetName,
+    asset_type: assetType || 'unknown'
+  })
+}
 };
 
 // init tauri API
@@ -195,7 +365,7 @@ document.addEventListener('error', function (e) {
   while (target != null) {
     if (target.matches ? target.matches('img') : target.msMatchesSelector('img')) {
       window.tauri.loadAsset(target.src, 'image')
-        .then(img => {
+        .then(function (img) {
           target.src = img
         })
       break
@@ -204,16 +374,27 @@ document.addEventListener('error', function (e) {
   }
 }, true)
 
-window.addEventListener('DOMContentLoaded', function () {
-  // open <a href="..."> links with the Tauri API
+// open <a href="..."> links with the Tauri API
+function __openLinks() {
   document.querySelector('body').addEventListener('click', function (e) {
     var target = e.target
     while (target != null) {
       if (target.matches ? target.matches('a') : target.msMatchesSelector('a')) {
-        window.tauri.open(target.href)
+        if (target.href && target.href.startsWith('http') && target.target === '_blank') {
+          window.tauri.open(target.href)
+          e.preventDefault()
+        }
         break
       }
       target = target.parentElement
     }
   }, true)
-}, true)
+}
+
+if (document.readyState === 'complete' || document.readyState === 'interactive') {
+  __openLinks()
+} else {
+  window.addEventListener('DOMContentLoaded', function () {
+    __openLinks()
+  }, true)
+}

d83e38e5d8a2485de75bb29143dc8353bc8d4f5a
Noah Klayman <noahklayman@gmail.com>
2020-05-22 14:40:41 -0700

    fix(api/info): use singlequote

--- cli/tauri.js/src/api/info.ts
+++ cli/tauri.js/src/api/info.ts
@@ -80,7 +80,7 @@ function printAppInfo(tauriDir: string): void {
     const tauriVersion = (): string => {
       const tauri = tomlContents.dependencies.tauri
       if (tauri) {
-        if (typeof tauri === "string") {
+        if (typeof tauri === 'string') {
           return chalk.green(tauri)
         }
         if (tauri.version) {

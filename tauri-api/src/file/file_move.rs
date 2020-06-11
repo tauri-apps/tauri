4ecdb0e51d63babc010f68a2a4773d7cefad71c1
David L <david@lemarier.ca>
2020-06-11 07:38:59 -0400

    feat(api): Added walk_to_dest in file::Move
    Instead of simply rename the source to dest, we walk the source and replace elements in the dest. (Similar to cp -R in OSX/Linux)
    
    It allow to extract our update archive recursively by overwriting what we want.
    
    Signed-off-by: David L <david@lemarier.ca>

--- tauri-api/src/file/file_move.rs
+++ tauri-api/src/file/file_move.rs
@@ -1,3 +1,4 @@
+use ignore::Walk;
 use std::fs;
 use std::path;
 
@@ -34,7 +35,7 @@ impl<'a> Move<'a> {
     self
   }
 
-  /// Move source file to specified destination
+  /// Move source file to specified destination (replace whole directory)
   pub fn to_dest(&self, dest: &path::Path) -> crate::Result<()> {
     match self.temp {
       None => {
@@ -54,4 +55,54 @@ impl<'a> Move<'a> {
     };
     Ok(())
   }
+
+  // Walk in the source and copy all files and create directories if needed by
+  // replacing existing elements. (equivalent to a cp -R)
+  pub fn walk_to_dest(&self, dest: &path::Path) -> crate::Result<()> {
+    match self.temp {
+      None => {
+        // got no temp -- no need to backup
+        walkdir_and_copy(self.source, dest)?;
+      }
+      Some(temp) => {
+        if dest.exists() {
+          // we got temp and our dest exist, lets make a backup
+          // of current files
+          fs::rename(dest, temp)?;
+          if let Err(e) = walkdir_and_copy(self.source, dest) {
+            // if we got something wrong we reset the dest with our backup
+            fs::rename(temp, dest)?;
+            return Err(e.into());
+          }
+        } else {
+          // got temp but dest didnt exist
+          walkdir_and_copy(self.source, dest)?;
+        }
+      }
+    };
+    Ok(())
+  }
+}
+// Walk into the source and create directories, and copy files
+// Overwriting existing items but keeping untouched the files in the dest
+// not provided in the source.
+fn walkdir_and_copy(source: &path::Path, dest: &path::Path) -> crate::Result<()> {
+  for entry in Walk::new(source) {
+    // Check if it's a file
+    let element = entry?;
+    let metadata = element.metadata()?;
+    let destination = dest.join(element.path().strip_prefix(&source)?);
+    // we make sure it's a directory
+    if metadata.is_dir() {
+      if !&destination.exists() {
+        fs::create_dir_all(&destination)?;
+      }
+    }
+
+    // we make sure it's a file
+    if metadata.is_file() {
+      fs::copy(element.path(), destination)?;
+    }
+  }
+  Ok(())
 }

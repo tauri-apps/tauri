b68ce0e81fd5cb02c6c0885bec6543202d0a2226
Denjell <denjell@mailscript.com>
2020-06-19 14:21:53 +0200

    feat(api): add bundle.ts for UMD

---
+++ cli/tauri.js/api-src/bundle.ts
@@ -0,0 +1,20 @@
+
+import * as cli from './cli'
+import * as dialog from './dialog'
+import * as event from './event'
+import * as fs from './fs'
+import * as http from './http'
+import * as process from './process'
+import * as tauri from './tauri'
+import * as window from './window'
+
+export {
+  cli,
+  dialog,
+  event,
+  fs,
+  http,
+  process,
+  tauri,
+  window
+}

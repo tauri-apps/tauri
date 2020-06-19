389750a549102e2d309133485ba5f294ecb51940
Denjell <denjell@mailscript.com>
2020-06-19 14:22:31 +0200

    feat(api): integrate rollup with build targets

--- cli/tauri.js/rollup.config.js
+++ cli/tauri.js/rollup.config.js
@@ -2,13 +2,13 @@
 import { terser } from 'rollup-plugin-terser'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import typescript from 'rollup-plugin-typescript2'
+import sucrase from '@rollup/plugin-sucrase'
+import { getBabelOutputPlugin } from '@rollup/plugin-babel'
 import pkg from './package.json'
 
-export default {
+export default [{
   input: {
-    'api/fs/index': './api-src/fs/index.ts',
-    'api/fs/dir': './api-src/fs/dir.ts',
+    'api/fs': './api-src/fs.ts',
     'api/dialog': './api-src/dialog.ts',
     'api/event': './api-src/event.ts',
     'api/http': './api-src/http.ts',
@@ -17,40 +17,31 @@ export default {
     'api/tauri': './api-src/tauri.ts',
     'api/window': './api-src/window.ts',
   },
-  // treeshake:      true,
-  // perf:           true,
+  treeshake:      true,
+  perf:           true,
   output:         [
     {
-      dir:    'api-e5/', // if you want to consume in node but want it tiny
-      entryFileNames: '[name].cjs.min.js',
+      dir: 'api/cjs/', // if you want to consume in node but want it tiny
+      entryFileNames: '[name].min.js',
       format:  'cjs',
       plugins: [ terser() ],
       exports: 'named',
-      globals: {
-      }
+      globals: {}
     },
     {
-      dir:      'api-e5/',  // if you will be transpiling and minifying yourself
-      entryFileNames: '[name].esm.js',
+      dir: 'api/esm/',  // if you will be transpiling and minifying yourself
+      entryFileNames: '[name].js',
       format:    'esm',
       sourcemap: true,
-      globals:   {
-      }
-    },
-    {
-      dir:    'dist/', // if it needs to run in the browser
-      entryFileNames: '[name]/[name].umd.min.js',
-      format:  'umd',
-      plugins: [ terser() ],
-      name:    'filer',
-      globals: {
-      }
+      exports: 'named',
+      globals: {}
     }
   ],
   plugins: [
     commonjs({}),
-    typescript({
-      typescript: require('typescript'),
+    sucrase({
+      exclude: ['node_modules'],
+      transforms: ['typescript']
     }),
     resolve({
     // pass custom options to the resolve plugin
@@ -65,7 +56,43 @@ export default {
   ],
   watch:    {
     chokidar: true,
-    include:  'src/**',
-    exclude:  'node_modules/**'
+    include: 'api-src/**',
+    exclude: 'node_modules/**'
   }
-}
+},
+{
+  input: {
+    'api/bundle': './api-src/bundle.ts'
+  },
+  output: [{
+    name: 'tauri',
+    dir:    'api/umd/', // if it needs to run in the browser
+    entryFileNames: 'tauri.bundle.min.js',
+    format:  'umd',
+    plugins: [
+      getBabelOutputPlugin({
+        presets: [['@babel/preset-env', { modules: 'umd' }]],
+        allowAllFormats: true
+      }),
+      terser()
+    ],
+    globals: {
+    }
+  }],
+  plugins: [
+    sucrase({
+      exclude: ['node_modules'],
+      transforms: ['typescript']
+    }),
+    resolve({
+    // pass custom options to the resolve plugin
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    })
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ]
+}]

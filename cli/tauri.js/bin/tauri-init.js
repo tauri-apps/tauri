91ddbd1b07e4d594c7067dc8d59e4d2581bf4f29
Lucas Nogueira <lucas@tauri.studio>
2020-07-10 16:31:48 -0300

    feat(tauri.js) prompt for appName on init

--- cli/tauri.js/bin/tauri-init.js
+++ cli/tauri.js/bin/tauri-init.js
@@ -1,6 +1,16 @@
 const parseArgs = require('minimist')
 const inquirer = require('inquirer')
-const { merge } = require('lodash')
+const {
+  resolve
+} = require('path')
+const {
+  readFileSync,
+  writeFileSync
+} = require('fs')
+const {
+  merge
+} = require('lodash')
+const toml = require('@tauri-apps/toml')
 
 /**
  * @type {object}
@@ -20,9 +30,10 @@ const argv = parseArgs(process.argv.slice(2), {
     l: 'log',
     d: 'directory',
     t: 'tauri-path',
-    w: 'window-title',
-    D: 'distDir',
-    p: 'devPath'
+    A: 'app-name',
+    W: 'window-title',
+    D: 'dist-dir',
+    P: 'dev-path'
   },
   boolean: ['h', 'l']
 })
@@ -35,32 +46,44 @@ if (argv.help) {
   Usage
     $ tauri init
   Options
-    --help, -h          Displays this message
-    --force, -f         Force init to overwrite [conf|template|all]
-    --log, -l           Logging [boolean]
-    --directory, -d     Set target directory for init
-    --tauri-path, -t    Path of the Tauri project to use (relative to the cwd)
-    --window-title, -w  Window title of your Tauri application
-    --distDir, -D       Web assets location, relative to <project-dir>/src-tauri
-    --devPath, -p       Url of your dev server
+    --help, -h           Displays this message
+    --force, -f          Force init to overwrite [conf|template|all]
+    --log, -l            Logging [boolean]
+    --directory, -d      Set target directory for init
+    --tauri-path, -t     Path of the Tauri project to use (relative to the cwd)
+    --app-name, -A       Name of your Tauri application
+    --window-title, -W   Window title of your Tauri application
+    --dist-dir, -D       Web assets location, relative to <project-dir>/src-tauri
+    --dev-path, -P       Url of your dev server
     `)
   process.exit(0)
 }
 
+let appName = argv.A
+if (!appName) {
+  try {
+    const packageJson = JSON.parse(readFileSync(resolve(process.cwd(), 'package.json')).toString())
+    appName = packageJson.displayName || packageJson.name
+  } catch {}
+}
+
 inquirer
-  .prompt([
-    {
+  .prompt([{
+      type: 'input',
+      name: 'appName',
+      message: 'What is your app name?',
+      default: appName
+    }, {
       type: 'input',
       name: 'tauri.window.title',
       message: 'What should the window title be?',
       default: 'Tauri App',
-      when: () => !argv.w
+      when: () => !argv.W
     },
     {
       type: 'input',
       name: 'build.distDir',
-      message:
-        'Where are your web assets (HTML/CSS/JS) located, relative to the "<current dir>/src-tauri" folder that will be created?',
+      message: 'Where are your web assets (HTML/CSS/JS) located, relative to the "<current dir>/src-tauri" folder that will be created?',
       default: '../dist',
       when: () => !argv.D
     },
@@ -69,7 +92,7 @@ inquirer
       name: 'build.devPath',
       message: 'What is the url of your dev server?',
       default: 'http://localhost:4000',
-      when: () => !argv.p
+      when: () => !argv.P
     }
   ])
   .then(answers => {
@@ -89,14 +112,19 @@ inquirer
   })
 
 function runInit(config = {}) {
+  const {
+    appName,
+    ...configOptions
+  } = config
   const init = require('../dist/api/init')
 
+  const directory = argv.d || process.cwd()
   init({
-    directory: argv.d || process.cwd(),
+    directory,
     force: argv.f || null,
     logging: argv.l || null,
     tauriPath: argv.t || null,
-    customConfig: merge(config, {
+    customConfig: merge(configOptions, {
       build: {
         distDir: argv.D,
         devPath: argv.p
@@ -108,4 +136,16 @@ function runInit(config = {}) {
       }
     })
   })
+
+  if (appName) {
+    const manifestPath = resolve(directory, 'src-tauri/Cargo.toml')
+    const cargoManifest = toml.parse(readFileSync(manifestPath).toString())
+    let binName = appName.replace(/ /g, '-')
+    cargoManifest.package.name = binName
+    cargoManifest.package['default-run'] = binName
+    if (cargoManifest.bin && cargoManifest.bin.length) {
+      cargoManifest.bin[0].name = binName
+    }
+    writeFileSync(manifestPath, toml.stringify(cargoManifest))
+  }
 }

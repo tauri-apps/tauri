3953611f0955c8b8a5eecf6aaff6154fd85cae24
Lucas Nogueira <lucas@tauri.studio>
2020-07-16 13:33:27 -0300

    feat(cli) add mutation-observer template

--- cli/src/helpers/tauri_html.rs
+++ cli/src/helpers/tauri_html.rs
@@ -1,8 +1,27 @@
+use handlebars::Handlebars;
 use html5ever::{interface::QualName, namespace_url, ns, LocalName};
 use inline_assets::inline_html_string;
 use kuchiki::{traits::*, NodeRef};
+use once_cell::sync::Lazy;
 
+use std::collections::BTreeMap;
 use std::path::PathBuf;
+
+fn handlebars() -> &'static Handlebars<'static> {
+  static HANDLEBARS: Lazy<Handlebars> = Lazy::new(|| {
+    let mut handlebars = Handlebars::new();
+
+    handlebars
+      .register_template_string(
+        "mutation-observer.js",
+        include_str!("../templates/mutation-observer.js"),
+      )
+      .or_else(|e| Err(e.to_string()))
+      .expect("Failed to setup handlebar template");
+    handlebars
+  });
+  &HANDLEBARS
+}
 
 pub struct TauriHtml {
   original: String,
@@ -39,19 +58,36 @@ impl TauriHtml {
     };
 
     let document = kuchiki::parse_html().one(html);
-    let target = document.select_first("head").unwrap_or_else(|_| {
-      document
-        .select_first("body")
-        .expect("html must contain head or body")
-    });
+    let body = document.select_first("body");
+    let head = document.select_first("head");
 
-    let tauri_script = create_script_element(include_str!("../templates/tauri.js"));
-    target.as_node().prepend(tauri_script);
+    let handlebars = handlebars();
 
-    if self.global_tauri {
-      let global_api_script =
-        create_script_element(include_str!("../../tauri.js/api/tauri.bundle.umd.js"));
-      target.as_node().prepend(global_api_script);
+    if let Ok(ref head) = head {
+      let mut data = BTreeMap::new();
+      data.insert("target".to_string(), "head".to_string());
+      let head_mutation_observer_script =
+        create_script_element(&handlebars.render("mutation-observer.js", &data)?);
+      head.as_node().prepend(head_mutation_observer_script);
+    }
+
+    if let Ok(ref body) = body {
+      let mut data = BTreeMap::new();
+      data.insert("target".to_string(), "body".to_string());
+      let body_mutation_observer_script =
+        create_script_element(&handlebars.render("mutation-observer.js", &data)?);
+      body.as_node().prepend(body_mutation_observer_script);
+    }
+
+    if let Ok(target) = if head.is_ok() { head } else { body } {
+      let tauri_script = create_script_element(include_str!("../templates/tauri.js"));
+      target.as_node().prepend(tauri_script);
+
+      if self.global_tauri {
+        let global_api_script =
+          create_script_element(include_str!("../../tauri.js/api/tauri.bundle.umd.js"));
+        target.as_node().prepend(global_api_script);
+      }
     }
 
     let new_html = document.to_string();

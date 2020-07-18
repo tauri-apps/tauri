89e397eaf503084eb1eaa0ca3256d79b70d0f439
Lucas Nogueira <lucas@tauri.studio>
2020-07-18 11:53:35 -0300

    fix(workflow) build api before building cli

--- cli/build.rs
+++ cli/build.rs
@@ -1,19 +1,25 @@
+use std::env::current_dir;
 use std::error::Error;
 use std::process::Command;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-  println!("cargo:rerun-if-changed=./tauri.js/api-src");
-  let exit_status = Command::new("yarn").current_dir("./tauri.js").status()?;
-  if !exit_status.success() {
-    panic!("Failed to install yarn deps");
-  }
+  if !current_dir()?
+    .join("tauri.js/api/tauri.bundle.umd.js")
+    .exists()
+  {
+    println!("cargo:rerun-if-changed=./tauri.js/api-src");
+    let exit_status = Command::new("yarn").current_dir("./tauri.js").status()?;
+    if !exit_status.success() {
+      panic!("Failed to install yarn deps");
+    }
 
-  let exit_status = Command::new("yarn")
-    .arg("build:api")
-    .current_dir("./tauri.js")
-    .status()?;
-  if !exit_status.success() {
-    panic!("Failed to build api");
+    let exit_status = Command::new("yarn")
+      .arg("build:api")
+      .current_dir("./tauri.js")
+      .status()?;
+    if !exit_status.success() {
+      panic!("Failed to build api");
+    }
   }
 
   Ok(())

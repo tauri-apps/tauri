5532995d67a49b600380020f7756bf7cad189581
Noah Klayman <noahklayman@gmail.com>
2020-07-26 16:11:11 -0700

    feat(tauri.js/init): set appName in api instead of bin

--- cli/tauri.js/src/api/init.ts
+++ cli/tauri.js/src/api/init.ts
@@ -1,5 +1,10 @@
 import { TauriConfig } from 'types'
 import { inject } from '../template'
+import { resolve } from 'path'
+import toml, { JsonMap } from '@tauri-apps/toml'
+import { readFileSync, writeFileSync } from 'fs'
+import { kebabCase } from 'lodash'
+import { CargoManifest } from 'src/types/cargo'
 
 module.exports = (args: {
   directory: string
@@ -7,8 +12,9 @@ module.exports = (args: {
   logging: boolean
   tauriPath?: string
   customConfig?: Partial<TauriConfig>
+  appName?: string
 }): boolean => {
-  return inject(
+  const injectResult = inject(
     args.directory,
     'all',
     {
@@ -18,4 +24,16 @@ module.exports = (args: {
     },
     args.customConfig
   )
+  if (args.appName) {
+    const manifestPath = resolve(args.directory, 'src-tauri/Cargo.toml')
+    const cargoManifest = toml.parse(readFileSync(manifestPath).toString()) as unknown as CargoManifest
+    const binName = kebabCase(args.appName)
+    cargoManifest.package.name = binName
+    cargoManifest.package['default-run'] = binName
+    if (cargoManifest.bin?.length) {
+      cargoManifest.bin[0].name = binName
+    }
+    writeFileSync(manifestPath, toml.stringify(cargoManifest as unknown as JsonMap))
+  }
+  return injectResult
 }

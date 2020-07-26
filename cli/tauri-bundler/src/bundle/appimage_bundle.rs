941eb0069574af9bb2397e7845efbbcbb958da0d
Noah Klayman <noahklayman@gmail.com>
2020-07-25 17:09:59 -0700

    feat(bundler): add notice about --verbose on error

--- cli/tauri-bundler/src/bundle/appimage_bundle.rs
+++ cli/tauri-bundler/src/bundle/appimage_bundle.rs
@@ -91,8 +91,12 @@ pub fn bundle_project(settings: &Settings) -> crate::Result<Vec<PathBuf>> {
   let mut cmd = Command::new(&sh_file);
   cmd.current_dir(output_path);
 
-  common::execute_with_verbosity(&mut cmd, &settings)
-    .map_err(|_| crate::Error::ShellScriptError("error running build_appimage.sh".to_owned()))?;
+  common::execute_with_verbosity(&mut cmd, &settings).map_err(|_| {
+    crate::Error::ShellScriptError(
+      "error running build_appimage.sh, try running with --verbose to see command output"
+        .to_owned(),
+    )
+  })?;
 
   remove_dir_all(&package_dir)?;
   Ok(vec![appimage_path])
--- cli/tauri-bundler/src/bundle/dmg_bundle.rs
+++ cli/tauri-bundler/src/bundle/dmg_bundle.rs
@@ -116,8 +116,11 @@ pub fn bundle_project(settings: &Settings) -> crate::Result<Vec<PathBuf>> {
     .args(vec![dmg_name.as_str(), bundle_name.as_str()]);
 
   common::print_info("running bundle_dmg.sh")?;
-  common::execute_with_verbosity(&mut cmd, &settings)
-    .map_err(|_| crate::Error::ShellScriptError("error running bundle_dmg.sh".to_owned()))?;
+  common::execute_with_verbosity(&mut cmd, &settings).map_err(|_| {
+    crate::Error::ShellScriptError(
+      "error running bundle_dmg.sh, try running with --verbose to see command output".to_owned(),
+    )
+  })?;
 
   fs::rename(bundle_dir.join(dmg_name), dmg_path.clone())?;
   Ok(vec![bundle_path, dmg_path])

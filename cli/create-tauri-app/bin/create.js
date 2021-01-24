617fb9fc46c0f6be54610206115112f35acca618
Noah Klayman <noahklayman@gmail.com>
2021-01-24 13:23:20 -0800

    feat(create-tauri-app): use yarn if installed

--- cli/create-tauri-app/bin/create.js
+++ cli/create-tauri-app/bin/create.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const scaffe = require("scaffe");
 const init = require("tauri/dist/api/init");
+const { execSync } = require('child_process')
+const fs = require('fs')
 
 module.exports = (appName, args) => {
   const template_dir = path.join(__dirname, "../templates/vanilla")
@@ -26,6 +28,19 @@ module.exports = (appName, args) => {
     })
 
     process.chdir(appName)
+
+    let hasYarn = false
+    try {
+      execSync('yarn version', {stdio: "ignore"})
+      hasYarn = true
+    } catch (error) {
+      // Do nothing, the user doesn't have Yarn
+    }
+    // TODO add CLI arg to force npm usage
+    if (hasYarn) {
+      // Create empty yarn.lock so that installDependencies uses Yarn
+      fs.writeFileSync('yarn.lock', '')
+    }
 
     const { installDependencies } = require('tauri/dist/api/dependency-manager')
     await installDependencies()

d4d533a0f09f5fac1d7bd89075d3b38da2800091
Lucas Nogueira <lucas@tauri.studio>
2021-02-04 20:45:54 -0300

    chore: update rollup example

---
+++ tauri/examples/rollup/src/components/Window.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { setTitle, open } from "@tauri-apps/api/window";
+
+  let urlValue = "https://tauri.studio";
+
+  function openUrl() {
+    open(urlValue);
+  }
+
+  function setWindowTitle() {
+    setTitle(urlValue);
+  }
+</script>
+
+<form style="margin-top: 24px" on:submit|preventDefault={setWindowTitle}>
+  <input id="title" value="Awesome Tauri Example!" />
+  <button class="button" type="submit">Set title</button>
+</form>
+<form style="margin-top: 24px" on:submit|preventDefault={openUrl}>
+  <input id="url" bind:value={urlValue} />
+  <button class="button" id="open-url">
+    Open URL
+  </button>
+</form>

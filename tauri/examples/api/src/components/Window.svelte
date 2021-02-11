ec564d4f4f72355b8d8dea135b27b73a7002349c
Lucas Nogueira <lucas@tauri.studio>
2021-02-11 20:53:54 -0300

    fix: API example

--- tauri/examples/api/src/components/Window.svelte
+++ tauri/examples/api/src/components/Window.svelte
@@ -2,18 +2,19 @@
   import { setTitle, open } from "@tauri-apps/api/window";
 
   let urlValue = "https://tauri.studio";
+  let windowTitle = 'Awesome Tauri Example!';
 
   function openUrl() {
     open(urlValue);
   }
 
   function setWindowTitle() {
-    setTitle(urlValue);
+    setTitle(windowTitle);
   }
 </script>
 
 <form style="margin-top: 24px" on:submit|preventDefault={setWindowTitle}>
-  <input id="title" value="Awesome Tauri Example!" />
+  <input id="title" bind:value={windowTitle} />
   <button class="button" type="submit">Set title</button>
 </form>
 <form style="margin-top: 24px" on:submit|preventDefault={openUrl}>

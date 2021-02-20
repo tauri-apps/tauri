7290d53fd8abc10ff8a26b9de38d3ee507bf6e82
Noah Klayman <noahklayman@gmail.com>
2021-02-20 09:56:44 -0800

    chore: update install commands in setup scripts

--- .scripts/setup.ps1
+++ .scripts/setup.ps1
@@ -4,27 +4,22 @@ cd api
 yarn; yarn build
 cd ..
 
-echo "Building the Tauri CLI..."
+echo "Installing the Tauri Rust CLI..."
 cd cli\core
-cargo build --release
+cargo install --path .
 cd ..\..
-
-Set-Alias rtauri "$(pwd)\cli\core\target\release\cargo-tauri.exe"
-echo "Added alias 'rtauri' for '$(pwd)\cli\core\target\release\cargo-tauri.exe'"
-echo "Tauri CLI installed. Run it with '$ rtauri tauri [COMMAND]'."
+echo "Tauri Rust CLI installed. Run it with '$ cargo tauri [COMMAND]'."
 
 $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes"
 $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No"
 $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
 
-$result = $host.ui.PromptForChoice("Node.js CLI", "Do you want to use the Node.js CLI?", $options, 1)
+$result = $host.ui.PromptForChoice("Node.js CLI", "Do you want to install the Node.js CLI?", $options, 1)
 switch ($result) {
   0{
     cd cli\tauri.js
-    yarn; yarn build
+    yarn; yarn build; yarn link
     cd ..\..
-    Set-Alias stauri "$(pwd)\cli\tauri.js\bin\tauri.js"
-    echo "Added alias 'stauri' for '$(pwd)\cli\tauri.js\bin\tauri.js'"
     echo "Tauri Node.js CLI installed. Run it with '$ stauri [COMMAND]'"
   }
 }
--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -4,25 +4,20 @@ cd api
 yarn && yarn build
 cd ..
 
-echo "Building the Tauri CLI..."
+echo "Building the Tauri Rust CLI..."
 cd cli/core
-cargo build --release
+cargo install --path .
 cd ../..
+echo "Tauri Rust CLI installed. Run it with '$ cargo tauri [COMMAND]'."
 
-alias rtauri="$(pwd)/cli/core/target/release/cargo-tauri.exe tauri"
-echo "Added alias 'rtauri' for '$(pwd)/cli/core/target/release/cargo-tauri.exe tauri'"
-echo "Tauri CLI installed. Run it with '$ rtauri [COMMAND]'."
-
-echo "Do you want to use the Node.js CLI?"
+echo "Do you want to install the Node.js CLI?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
             cd cli/tauri.js
-            yarn && yarn build
+            yarn && yarn build && yarn link
             cd ../..
-            alias stauri="$(pwd)/cli/tauri.js/bin/tauri.js"
-            echo "Added alias 'stauri' for '$(pwd)/cli/tauri.js/bin/tauri.js'"
-            echo "Tauri Node.js CLI installed. Run it with '$ stauri [COMMAND]'"
+            echo "Tauri Node.js CLI installed. Run it with '$ tauri [COMMAND]'."
             break;;
         No ) break;;
     esac

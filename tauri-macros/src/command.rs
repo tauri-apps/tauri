cf5a7c2a94812f69b86ad514d3fd50940e7e0d9b
Noah Klayman <noahklayman@gmail.com>
2021-02-27 17:02:36 -0800

    chore(macros/command): clean up code and improve error messages also remove webview type inference

--- tauri-macros/src/command.rs
+++ tauri-macros/src/command.rs
@@ -1,14 +1,14 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use std::borrow::BorrowMut;
 use syn::{
-  parse::Parser, parse_quote, punctuated::Punctuated, FnArg, ItemFn, NestedMeta, Path, Token, Type,
+  parse::Parser, punctuated::Punctuated, FnArg, Ident, ItemFn, Meta, NestedMeta, Pat, Path, Token,
+  Type,
 };
 
-pub fn generate_command(attrs: Vec<NestedMeta>, mut function: ItemFn) -> TokenStream {
+pub fn generate_command(attrs: Vec<NestedMeta>, function: ItemFn) -> TokenStream {
   // Check if "webview" attr was passed to macro
   let uses_webview = attrs.iter().any(|a| {
-    if let syn::NestedMeta::Meta(syn::Meta::Path(path)) = a {
+    if let NestedMeta::Meta(Meta::Path(path)) = a {
       path
         .get_ident()
         .map(|i| *i == "with_webview")
@@ -18,84 +18,90 @@ pub fn generate_command(attrs: Vec<NestedMeta>, mut function: ItemFn) -> TokenSt
     }
   });
 
-  if uses_webview {
-    // Check if webview type should be automatically injected
-    if let FnArg::Typed(arg) = function.sig.inputs[0].borrow_mut() {
-      if let Type::Infer(_) = arg.ty.as_ref() {
-        let default_type: Box<syn::Type> = parse_quote!(tauri::WebviewManager<tauri::flavors::Wry>);
-        arg.ty = default_type;
-      }
-    }
-  }
+  let fn_name = function.sig.ident.clone();
+  let fn_wrapper = format_ident!("{}_wrapper", fn_name);
 
-  let ident = function.sig.ident.clone();
-  let params = function.sig.inputs.clone();
-  let (mut names, mut types): (Vec<syn::Ident>, Vec<syn::Path>) = params
+  // Split function args into names and types
+  let (mut names, mut types): (Vec<Ident>, Vec<Path>) = function
+    .sig
+    .inputs
+    .clone()
     .iter()
     .map(|param| {
       let mut arg_name = None;
       let mut arg_type = None;
-      if let syn::FnArg::Typed(arg) = param {
-        if let syn::Pat::Ident(ident) = arg.pat.as_ref() {
+      if let FnArg::Typed(arg) = param {
+        if let Pat::Ident(ident) = arg.pat.as_ref() {
           arg_name = Some(ident.ident.clone());
         }
-        if let syn::Type::Path(path) = arg.ty.as_ref() {
+        if let Type::Path(path) = arg.ty.as_ref() {
           arg_type = Some(path.path.clone());
         }
       }
-      (arg_name.unwrap(), arg_type.unwrap())
+      (
+        arg_name.clone().unwrap(),
+        arg_type.expect(&format!(
+          "#[command]: Invalid type for arg \"{}\"",
+          arg_name.unwrap()
+        )),
+      )
     })
     .unzip();
 
-  // Default generic for functions that don't take the webview
-  let mut application_ext_generic = quote!(<A: tauri::ApplicationExt>);
+  // If function doesn't take the webview, wrapper just takes webview generically and ignores it
+  // Otherwise the wrapper uses the specific type from the original function declaration
   let mut webview_arg_type = quote!(tauri::WebviewManager<A>);
-  let webview_arg = match types.first() {
-    Some(first_type) => {
-      if uses_webview {
-        // If the function takes the webview, give it a specific type
-        webview_arg_type = quote!(#first_type);
-        application_ext_generic = quote!();
-        types.drain(0..1);
-        names.drain(0..1);
-        quote!(_webview,)
-      } else {
-        quote!()
-      }
+  let mut application_ext_generic = quote!(<A: tauri::ApplicationExt>);
+  let webview_arg_maybe = match types.first() {
+    Some(first_type) if uses_webview => {
+      // Give wrapper specific type
+      webview_arg_type = quote!(#first_type);
+      // Generic is no longer needed
+      application_ext_generic = quote!();
+      // Remove webview arg from list so it isn't expected as arg from JS
+      types.drain(0..1);
+      names.drain(0..1);
+      // Tell wrapper to pass webview to original function
+      quote!(_webview,)
     }
-    None => quote!(),
+    // Tell wrapper not to pass webview to original function
+    _ => quote!(),
   };
 
-  let ident_wrapper = format_ident!("{}_wrapper", ident);
-  let gen = quote! {
+  quote! {
     #function
-    pub fn #ident_wrapper #application_ext_generic(_webview: #webview_arg_type, arg: serde_json::Value) -> Option<tauri::InvokeResponse> {
+    pub fn #fn_wrapper #application_ext_generic(_webview: #webview_arg_type, arg: serde_json::Value) -> Option<tauri::InvokeResponse> {
       #[derive(serde::Deserialize)]
       #[serde(rename_all = "camelCase")]
       struct ParsedArgs {
         #(#names: #types),*
       }
       let parsed_args: ParsedArgs = serde_json::from_value(arg).unwrap();
-      Some(#ident(#webview_arg #(parsed_args.#names),*).into())
+      Some(#fn_name(#webview_arg_maybe #(parsed_args.#names),*).into())
     }
-  };
-  gen
+  }
 }
 
 pub fn generate_handler(item: proc_macro::TokenStream) -> TokenStream {
+  // Get paths of functions passed to macro
   let paths = <Punctuated<Path, Token![,]>>::parse_terminated
     .parse(item)
-    .unwrap();
-  let funcs = paths
+    .expect("generate_handler!: Failed to parse list of command functions");
+
+  // Get names of functions, used for match statement
+  let fn_names = paths
     .iter()
     .map(|p| p.segments.last().unwrap().ident.clone());
-  let funcs_wrapper = paths.iter().map(|func| {
+
+  // Get paths to wrapper functions
+  let fn_wrappers = paths.iter().map(|func| {
     let mut func = func.clone();
-    let ident = format_ident!("{}_wrapper", func.segments.last().unwrap().ident);
-    func.segments.last_mut().unwrap().ident = ident;
+    let mut last_segment = func.segments.last_mut().unwrap();
+    last_segment.ident = format_ident!("{}_wrapper", last_segment.ident);
     func
   });
-  let gen = quote! {
+
+  quote! {
     |webview, arg| async move {
       let dispatch: Result<tauri::DispatchInstructions, serde_json::Error> =
       serde_json::from_str(&arg);
@@ -103,13 +109,12 @@ pub fn generate_handler(item: proc_macro::TokenStream) -> TokenStream {
         Err(e) => Err(e.into()),
         Ok(dispatch) => {
           let res = match dispatch.cmd.as_str() {
-            #(stringify!(#funcs) => #funcs_wrapper(webview, dispatch.args),)*
+            #(stringify!(#fn_names) => #fn_wrappers(webview, dispatch.args),)*
             _ => None,
           };
           Ok(res.unwrap_or(().into()))
         }
       }
     }
-  };
-  gen
+  }
 }

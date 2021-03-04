d4d242783f39057db7ab2c7299bad21af1480fc5
Jacob Bolda <me@jacobbolda.com>
2021-03-04 02:12:25 -0600

    shift remaining files that weren't removed

---
+++ cli/create-tauri-app/src/dependency-manager/npm-packages.ts
@@ -0,0 +1,99 @@
+import { ManagementType, Result } from "./types";
+import {
+  getNpmLatestVersion,
+  getNpmPackageVersion,
+  installNpmPackage,
+  installNpmDevPackage,
+  updateNpmPackage,
+  semverLt,
+} from "./util";
+import logger from "../../../tauri.js/src/helpers/logger";
+import { resolve } from "../../../tauri.js/src/helpers/app-paths";
+import inquirer from "inquirer";
+import { existsSync } from "fs";
+import { sync as crossSpawnSync } from "cross-spawn";
+
+const log = logger("dependency:npm-packages");
+
+async function manageDependencies(
+  managementType: ManagementType,
+  dependencies: string[]
+): Promise<Result> {
+  const installedDeps = [];
+  const updatedDeps = [];
+
+  const npmChild = crossSpawnSync("npm", ["--version"]);
+  const yarnChild = crossSpawnSync("yarn", ["--version"]);
+  if (
+    (npmChild.status ?? npmChild.error) &&
+    (yarnChild.status ?? yarnChild.error)
+  ) {
+    throw new Error(
+      "must have `npm` or `yarn` installed to manage dependenices"
+    );
+  }
+
+  if (existsSync(resolve.app("package.json"))) {
+    for (const dependency of dependencies) {
+      const currentVersion = await getNpmPackageVersion(dependency);
+      if (currentVersion === null) {
+        log(`Installing ${dependency}...`);
+        if (managementType === ManagementType.Install) {
+          await installNpmPackage(dependency);
+        } else if (managementType === ManagementType.InstallDev) {
+          await installNpmDevPackage(dependency);
+        }
+        installedDeps.push(dependency);
+      } else if (managementType === ManagementType.Update) {
+        const latestVersion = await getNpmLatestVersion(dependency);
+        if (semverLt(currentVersion, latestVersion)) {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-member-access
+          const inquired = await inquirer.prompt([
+            {
+              type: "confirm",
+              name: "answer",
+              message: `[NPM]: "${dependency}" latest version is ${latestVersion}. Do you want to update?`,
+              default: false,
+            },
+          ]);
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-member-access
+          if (inquired.answer) {
+            log(`Updating ${dependency}...`);
+            updateNpmPackage(dependency);
+            updatedDeps.push(dependency);
+          }
+        } else {
+          log(`"${dependency}" is up to date`);
+        }
+      } else {
+        log(`"${dependency}" is already installed`);
+      }
+    }
+  }
+
+  const result: Result = new Map<ManagementType, string[]>();
+  result.set(ManagementType.Install, installedDeps);
+  result.set(ManagementType.Update, updatedDeps);
+
+  return result;
+}
+
+const dependencies = ["tauri"];
+
+async function install(): Promise<Result> {
+  return await manageDependencies(ManagementType.Install, dependencies);
+}
+
+async function installThese(dependencies: string[]): Promise<Result> {
+  return await manageDependencies(ManagementType.Install, dependencies);
+}
+
+async function installTheseDev(dependencies: string[]): Promise<Result> {
+  return await manageDependencies(ManagementType.InstallDev, dependencies);
+}
+
+async function update(): Promise<Result> {
+  return await manageDependencies(ManagementType.Update, dependencies);
+}
+
+export { install, installThese, installTheseDev, update };

3a781026c95ffb3eea21df18006bf500a635290a
Rhydderchc <rhydderchc@gmail.com>
2021-03-26 21:36:44 -0600

    Update tauri.js

--- cli/tauri.js/bin/tauri.js
+++ cli/tauri.js/bin/tauri.js
@@ -5,80 +5,76 @@ const rustCliCmds = ['dev', 'build']
 const fs = require('fs');
 const cmd = process.argv[2]
 const chalk = require('chalk');
-const package = require('../package.json');
+const pkg = require('../package.json');
 const figlet = require('figlet');
+const updateNotifier = require('update-notifier')
 /**
  * @description This is the bootstrapper that in turn calls subsequent
  * Tauri Commands
  *
  * @param {string|array} command
  */
+let noUpdates;
 const tauri = function (command) {
-  if (typeof command === 'object') {
-    // technically we just care about an array
-    command = command[0]
-  }
-
-  if (rustCliCmds.includes(command)) {
-    const { runOnRustCli } = require('../dist/helpers/rust-cli')
-    if (process.argv && !process.env.test) {
-      process.argv.splice(0, 3)
+    if (typeof command === 'object') {
+        // technically we just care about an array
+        command = command[0]
     }
-    runOnRustCli(command, process.argv || [])
-    return
-  }
 
-  if (
-    !command ||
-    command === '-h' ||
-    command === '--help' ||
-    command === 'help'
-  ) {
-    
-    console.log(chalk.cyan(figlet.textSync('Tauri')));
-    console.log(chalk.green('Description'))
-    console.log('This is the Tauri CLI');
-    console.log(chalk.magenta('Usage'));
-    console.log('$ tauri ${cmds.join('|')}')
-    console.log(chalk.blue('Options'))
-    console.log('--help, -h     Displays this message \n  --version, -v  Displays the Tauri CLI version');
-    process.exit(0)
-    // eslint-disable-next-line no-unreachable
-    return false // do this for node consumers and tests
-  }
-
-  if (command === '-v' || command === '--version') {
-    console.log(chalk.green(`${package.version}`));
-    // also added colors to beautify the cli
-    return false // do this for node consumers and tests
-  }
-  if (command ==='-no-update-notifier' || command === '--no-update-notifier'){
-      const noUpdates = true;
-      }
-
-  if (cmds.includes(command)) {
-    if (process.argv && !process.env.test) {
-      process.argv.splice(2, 1)
+    if (rustCliCmds.includes(command)) {
+        const { runOnRustCli } = require('../dist/helpers/rust-cli')
+        if (process.argv && !process.env.test) {
+            process.argv.splice(0, 3)
+        }
+        runOnRustCli(command, process.argv || [])
+        return
     }
-    console.log(`[tauri]: running ${command}`)
-    // eslint-disable-next-line security/detect-non-literal-require
-    if (['init'].includes(command)) {
-      require(`./tauri-${command}`)(process.argv.slice(2))
+
+    if (
+        !command ||
+        command === '-h' ||
+        command === '--help' ||
+        command === 'help'
+    ) {
+
+        console.log(chalk.cyan(figlet.textSync('Tauri')));
+        console.log(`${chalk.cyan("Description")} \n This is the Tauri CLI \n ${chalk.magenta('Usage')} \n $ tauri ${cmds.join('|')} \n ${chalk.cyan('Options')} \n --help, -h     Displays this message \n  --version, -v  Displays the Tauri CLI version`)
+     
+        process.exit(0)
+        // eslint-disable-next-line no-unreachable
+        return false // do this for node consumers and tests
+    }
+
+    if (command === '-v' || command === '--version') {
+        console.log(`${pkg.version}`);
+        return false // do this for node consumers and tests
+    }
+    if (command ==='-no-update-notifier' || command === '--no-update-notifier'){
+       noUpdates = true;
+    }
+
+    if (cmds.includes(command)) {
+        if (process.argv && !process.env.test) {
+            process.argv.splice(2, 1)
+        }
+        console.log(`[tauri]: running ${command}`)
+        // eslint-disable-next-line security/detect-non-literal-require
+        if (['init'].includes(command)) {
+            require(`./tauri-${command}`)(process.argv.slice(2))
+        } else {
+            require(`./tauri-${command}`)
+        }
     } else {
-      require(`./tauri-${command}`)
+        console.log(`Invalid command ${command}. Use one of ${cmds.join(', ')}.`)
     }
-  } else {
-    console.log(`Invalid command ${command}. Use one of ${cmds.join(', ')}.`)
-  }
 }
 // notifying updates.
-const pkg = JSON.parse(fs.readFileSync(__dirname + '/../package.json')); 
 if (pkg.version.indexOf('0.0.0') !== 0 && noUpdates !== true) {
-  require('update-notifier')({ pkg }).notify();
+    updateNotifier({ pkg }).notify();
 }
 
 module.exports = {
-  tauri
+    tauri
 }
 
 tauri(cmd)

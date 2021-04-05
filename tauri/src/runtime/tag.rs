2ea56cf21d61ed2bc2039efe194a3145043c6871
chip <chip@chip.sh>
2021-04-05 09:33:07 -0700

    refactor(tauri): add some better docs for Tag (#1425)

--- tauri/src/runtime/tag.rs
+++ tauri/src/runtime/tag.rs
@@ -17,6 +17,61 @@ use std::{
 /// across thread boundaries, although some of those constraints may relax in the future.
 ///
 /// The simplest type that fits all these requirements is a [`String`](std::string::String).
+///
+/// # Handling Errors
+///
+/// Because we leave it up to the type to implement [`FromStr`], if an error is returned during
+/// parsing then Tauri will [`panic!`](std::panic) with the string it failed to parse.
+///
+/// To avoid Tauri panicking during the application runtime, have your type be able to handle
+/// unknown events and never return an error in [`FromStr`]. Then it will be up to your own code
+/// to handle the unknown event.
+///
+/// # Example
+///
+/// ```
+/// use std::fmt;
+/// use std::str::FromStr;
+///
+/// #[derive(Debug, Clone, Hash, Eq, PartialEq)]
+/// enum Event {
+///   Foo,
+///   Bar,
+///   Unknown(String),
+/// }
+///
+/// impl fmt::Display for Event {
+///   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+///     f.write_str(match self {
+///       Self::Foo => "foo",
+///       Self::Bar => "bar",
+///       Self::Unknown(s) => &s
+///     })
+///   }
+/// }
+///
+/// impl FromStr for Event {
+///   type Err = std::convert::Infallible;
+///
+///   fn from_str(s: &str) -> Result<Self, Self::Err> {
+///     Ok(match s {
+///       "foo" => Self::Foo,
+///       "bar" => Self::Bar,
+///       other => Self::Unknown(other.to_string())
+///     })
+///   }
+/// }
+///
+/// // safe to unwrap because we know it's infallible due to our FromStr implementation.
+/// let event: Event = "tauri://file-drop".parse().unwrap();
+///
+/// // show that this event type can be represented as a Tag, a requirement for using it in Tauri.
+/// fn is_file_drop(tag: impl tauri::runtime::Tag) {
+///   assert_eq!("tauri://file-drop", tag.to_string());
+/// }
+///
+/// is_file_drop(event);
+/// ```
 pub trait Tag: Hash + Eq + FromStr + Display + Debug + Clone + Send + Sync + 'static {}
 
 /// Automatically implement [`Tag`] for all types that fit the requirements.

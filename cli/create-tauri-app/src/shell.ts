a16f32ad3bc1e51463f44ac794beef9a820e09e4
Jacob Bolda <me@jacobbolda.com>
2021-04-08 01:00:29 -0500

    log shell commands

--- cli/create-tauri-app/src/shell.ts
+++ cli/create-tauri-app/src/shell.ts
@@ -3,17 +3,21 @@ import execa from "execa";
 export const shell = async (
   command: string,
   args?: string[],
-  options?: execa.Options
+  options?: execa.Options,
+  log: boolean = false
 ) => {
   try {
     if (options && options.shell === true) {
-      return await execa([command, ...(!args ? [] : args)].join(" "), {
+      const stringCommand = [command, ...(!args ? [] : args)].join(" ");
+      if (log) console.log(`[running]: ${stringCommand}`);
+      return await execa(stringCommand, {
         stdio: "inherit",
         cwd: process.cwd(),
         env: process.env,
         ...options,
       });
     } else {
+      if (log) console.log(`[running]: ${command}`);
       return await execa(command, args, {
         stdio: "inherit",
         cwd: process.cwd(),

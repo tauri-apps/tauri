8fad665a3d702d5571fcf3f9f079b8b46060ff0d
Jacob Bolda <me@jacobbolda.com>
2021-04-14 12:48:40 -0500

    add license header to vue-cli file

--- tooling/create-tauri-app/src/recipes/vue-cli.ts
+++ tooling/create-tauri-app/src/recipes/vue-cli.ts
@@ -1,6 +1,9 @@
+// Copyright 2019-2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 import { Recipe } from "..";
 import { join } from "path";
-//@ts-ignore
 import { shell } from "../shell";
 
 const completeLogMsg = `

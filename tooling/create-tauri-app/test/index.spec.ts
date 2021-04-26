2042f68a4a5ef341134a88c4f6714ff3197d2a61
Jacob Bolda <me@jacobbolda.com>
2021-04-26 00:44:19 -0500

    assert on package.json contents as first check

--- tooling/create-tauri-app/test/index.spec.ts
+++ tooling/create-tauri-app/test/index.spec.ts
@@ -6,86 +6,134 @@ import execa from 'execa'
 import fixtures from 'fixturez'
 const f = fixtures(__dirname)
 import path from 'path'
+import fs from 'fs'
 
 const ctaBinary = path.resolve('./bin/create-tauri-app.js')
 const clijs = path.resolve('../cli.js/')
 const api = path.resolve('../api/')
 
+const packageFile = (template: string): object => {
+  const packageFilesReturn: { [k: string]: object } = {
+    vanillajs: {
+      name: 'tauri-app',
+      scripts: {
+        tauri: 'tauri'
+      }
+    },
+    reactjs: {
+      name: 'tauri-app',
+      scripts: {
+        start: 'react-scripts start',
+        build: 'react-scripts build',
+        test: 'react-scripts test',
+        eject: 'react-scripts eject',
+        tauri: 'tauri'
+      }
+    },
+    reactts: {
+      name: 'tauri-app',
+      scripts: {
+        start: 'react-scripts start',
+        build: 'react-scripts build',
+        test: 'react-scripts test',
+        eject: 'react-scripts eject',
+        tauri: 'tauri'
+      }
+    }
+  }
+
+  return packageFilesReturn[template] ?? packageFilesReturn['vanillajs']
+}
+
 const manager = process.env.TAURI_RUN_MANAGER ?? 'npm'
 const recipes = process.env.TAURI_RECIPE
   ? [process.env.TAURI_RECIPE]
-  : ['vanillajs'] //, 'reactjs', 'reactts', 'vite', 'vuecli']
-const timeout15m = 900000
-const timeout16m = 960000
+  : ['vanillajs', 'reactjs', 'reactts', 'vite', 'vuecli']
+const timeout3m = 180000
+const timeout3plusm = 200000
 const logOut = false ? 'inherit' : 'pipe'
 
 beforeAll(async () => {
   const installCLI = await execa('yarn', [], {
     stdio: logOut,
     cwd: clijs,
-    timeout: timeout15m
+    timeout: timeout3m
   })
 
   const buildCLI = await execa('yarn', ['build-release'], {
     stdio: logOut,
     cwd: clijs,
-    timeout: timeout15m
+    timeout: timeout3m
   })
 
   const linkCLI = await execa('yarn', ['link'], {
     stdio: logOut,
     cwd: clijs,
-    timeout: timeout15m
+    timeout: timeout3m
   })
 
   const installAPI = await execa('yarn', [], {
     stdio: logOut,
     cwd: api,
-    timeout: timeout15m
+    timeout: timeout3m
   })
 
   const buildAPI = await execa('yarn', ['build'], {
     stdio: logOut,
     cwd: api,
-    timeout: timeout15m
+    timeout: timeout3m
   })
 
   const linkAPI = await execa('yarn', ['link'], {
     stdio: logOut,
     cwd: api,
-    timeout: timeout15m
+    timeout: timeout3m
   })
-}, timeout16m)
+}, timeout3plusm)
 
 describe('CTA', () => {
-  describe.each(recipes)(`%s recipe`, (recipe) => {
-    it(
-      'runs',
-      async () => {
-        const folder = f.temp()
+  describe.each(recipes.map((recipe) => [recipe, packageFile(recipe)]))(
+    `%s recipe`,
+    (recipe: any, packageFile: any) => {
+      it(
+        'runs',
+        async () => {
+          const folder = f.temp()
 
-        const child = await execa(
-          'node',
-          [
-            ctaBinary,
-            '--manager',
-            manager,
-            '--recipe',
-            recipe,
-            '--ci',
-            '--dev'
-          ],
-          {
-            all: true,
-            stdio: logOut,
-            cwd: folder,
-            timeout: timeout15m
-          }
-        )
+          const child = await execa(
+            'node',
+            [
+              ctaBinary,
+              '--manager',
+              manager,
+              '--recipe',
+              recipe,
+              '--ci',
+              '--dev'
+            ],
+            {
+              all: true,
+              stdio: logOut,
+              cwd: folder,
+              timeout: timeout3m
+            }
+          )
 
-        expect(child.all).toEqual('')
-      },
-      timeout16m
-    )
-  })
+          const packageFileOutput: {
+            [k: string]: string | object
+          } = JSON.parse(
+            await fs.promises.readFile(
+              path.join(folder, 'tauri-app', 'package.json'),
+              'utf-8'
+            )
+          )
+          expect(packageFileOutput['name']).toBe(packageFile['name']!)
+          expect(packageFileOutput['scripts']).toMatchObject(
+            packageFile['scripts']!
+          )
+        },
+        timeout3plusm
+      )
+    }
+  )
 })

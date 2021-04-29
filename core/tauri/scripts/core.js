4e285a33f9e39da23322bfaeb37257849e1b5578
Lucas Nogueira <lucas@tauri.studio>
2021-04-29 18:29:40 -0300

    refactor(core): change drag element detection to data attr, fixes #1656

--- core/tauri/scripts/core.js
+++ core/tauri/scripts/core.js
@@ -189,7 +189,8 @@ if (!String.prototype.startsWith) {
 
   // drag region
   document.addEventListener('mousedown', (e) => {
-    if (e.target.classList.contains('drag-region') && e.buttons === 1) {
+    // start dragging if the element has a `tauri-drag-region` data attribute
+    if (e.target.dataset.tauriDragRegion === '' && e.buttons === 1) {
       window.__TAURI__.invoke('tauri', {
         __tauriModule: "Window",
         message: {

6e881d99ea59bc9950606f105166c67be766d05a
Lucas Nogueira <lucas@tauri.studio>
2021-05-03 13:16:14 -0300

    feat(examples): add navigation example

---
+++ examples/navigation/public/index.js
@@ -0,0 +1,10 @@
+const routeSelect = document.querySelector('#route')
+const link = document.querySelector('#link')
+
+routeSelect.addEventListener('change', (event) => {
+  link.href = event.target.value
+})
+
+document.querySelector('#go').addEventListener('click', () => {
+  window.location.href = (window.location.origin + '/' + routeSelect.value)
+})
---
+++ examples/navigation/public/nested/index.js
@@ -0,0 +1,1 @@
+document.querySelector('h1').innerHTML = 'nested/index.html'

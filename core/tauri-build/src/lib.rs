771f58a5a6ec120f52d46d3d6fd324fb23695bb4
Lucas Nogueira <lucas@tauri.studio>
2021-05-03 09:45:45 -0300

    refactor(build): allow setting window icon path on `try_build`

--- core/tauri-build/src/lib.rs
+++ core/tauri-build/src/lib.rs
@@ -6,11 +6,41 @@
 
 pub use anyhow::Result;
 
+use std::path::{Path, PathBuf};
+
 #[cfg(feature = "codegen")]
 mod codegen;
 
 #[cfg(feature = "codegen")]
 pub use codegen::context::CodegenContext;
+
+/// The attributes used on the build.
+pub struct Attributes {
+  #[allow(dead_code)]
+  window_icon_path: PathBuf,
+}
+
+impl Default for Attributes {
+  fn default() -> Self {
+    Self {
+      window_icon_path: PathBuf::from("icons/icon.ico"),
+    }
+  }
+}
+
+impl Attributes {
+  /// Creates the default attribute set.
+  pub fn new() -> Self {
+    Self::default()
+  }
+
+  /// Sets the icon to use on the window. Currently only used on Windows.
+  /// It must be in `ico` format. Defaults to `icons/icon.ico`.
+  pub fn window_icon_path<P: AsRef<Path>>(mut self, window_icon_path: P) -> Self {
+    self.window_icon_path = window_icon_path.as_ref().into();
+    self
+  }
+}
 
 /// Run all build time helpers for your Tauri Application.
 ///
@@ -32,22 +62,25 @@ pub use codegen::context::CodegenContext;
 /// If any of the build time helpers fail, they will [`std::panic!`] with the related error message.
 /// This is typically desirable when running inside a build script; see [`try_build`] for no panics.
 pub fn build() {
-  if let Err(error) = try_build() {
+  if let Err(error) = try_build(Attributes::default()) {
     panic!("error found during tauri-build: {}", error);
   }
 }
 
 /// Non-panicking [`build()`].
-pub fn try_build() -> Result<()> {
+#[allow(unused_variables)]
+pub fn try_build(attributes: Attributes) -> Result<()> {
   #[cfg(windows)]
   {
     use anyhow::{anyhow, Context};
-    use std::path::Path;
     use winres::WindowsResource;
 
-    if Path::new("icons/icon.ico").exists() {
+    if attibutes.window_icon_path.exists() {
       let mut res = WindowsResource::new();
-      res.set_icon_with_id("icons/icon.ico", "32512");
+      res.set_icon_with_id(
+        attributes.window_icon_path.into_os_string().into_string(),
+        "32512",
+      );
       res.compile().with_context(|| {
         "failed to compile icons/icon.ico into a Windows Resource file during tauri-build"
       })?;

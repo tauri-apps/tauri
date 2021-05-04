6ee0c1db185e01408f87971fe26413a6c93d27f2
chip reed <chip@chip.sh>
2021-05-04 16:17:41 -0700

    add wildcard, struct, and tuple struct commands to examples

--- examples/commands/src-tauri/src/main.rs
+++ examples/commands/src-tauri/src/main.rs
@@ -7,37 +7,40 @@
   windows_subsystem = "windows"
 )]
 
+use serde::Deserialize;
+use tauri::{command, Params, State, Window};
+
 #[derive(Debug)]
 struct MyState {
   value: u64,
   label: String,
 }
 
-#[tauri::command]
+#[command]
 fn simple_command(argument: String) {
   println!("{}", argument);
 }
 
-#[tauri::command]
-fn stateful_command(argument: Option<String>, state: tauri::State<'_, MyState>) {
+#[command]
+fn stateful_command(argument: Option<String>, state: State<'_, MyState>) {
   println!("{:?} {:?}", argument, state.inner());
 }
 
 // ------------------------ Commands using Window ------------------------
-#[tauri::command]
-fn window_label(window: tauri::Window<impl tauri::Params<Label = String>>) {
+#[command]
+fn window_label(window: Window<impl Params<Label = String>>) {
   println!("window label: {}", window.label());
 }
 
 // Async commands
 
-#[tauri::command]
+#[command]
 async fn async_simple_command(argument: String) {
   println!("{}", argument);
 }
 
-#[tauri::command]
-async fn async_stateful_command(argument: Option<String>, state: tauri::State<'_, MyState>) {
+#[command]
+async fn async_stateful_command(argument: Option<String>, state: State<'_, MyState>) {
   println!("{:?} {:?}", argument, state.inner());
 }
 
@@ -45,16 +48,16 @@ async fn async_stateful_command(argument: Option<String>, state: tauri::State<'_
 
 type Result<T> = std::result::Result<T, ()>;
 
-#[tauri::command]
+#[command]
 fn simple_command_with_result(argument: String) -> Result<String> {
   println!("{}", argument);
   (!argument.is_empty()).then(|| argument).ok_or(())
 }
 
-#[tauri::command]
+#[command]
 fn stateful_command_with_result(
   argument: Option<String>,
-  state: tauri::State<'_, MyState>,
+  state: State<'_, MyState>,
 ) -> Result<String> {
   println!("{:?} {:?}", argument, state.inner());
   argument.ok_or(())
@@ -62,19 +65,45 @@ fn stateful_command_with_result(
 
 // Async commands
 
-#[tauri::command]
+#[command]
 async fn async_simple_command_with_result(argument: String) -> Result<String> {
   println!("{}", argument);
   Ok(argument)
 }
 
-#[tauri::command]
+#[command]
 async fn async_stateful_command_with_result(
   argument: Option<String>,
-  state: tauri::State<'_, MyState>,
+  state: State<'_, MyState>,
 ) -> Result<String> {
   println!("{:?} {:?}", argument, state.inner());
   Ok(argument.unwrap_or_else(|| "".to_string()))
+}
+
+// Non-Ident command function arguments
+
+#[command]
+fn command_arguments_wild<P: Params>(_: Window<P>) {
+  println!("we saw the wildcard!")
+}
+
+#[derive(Deserialize)]
+struct Person<'a> {
+  name: &'a str,
+  age: u8,
+}
+
+#[command]
+fn command_arguments_struct(Person { name, age }: Person) {
+  println!("received person struct with name: {} | age: {}", name, age)
+}
+
+#[derive(Deserialize)]
+struct InlinePerson<'a>(&'a str, u8);
+
+#[command]
+fn command_arguments_tuple_struct(InlinePerson(name, age): InlinePerson) {
+  println!("received person tuple with name: {} | age: {}", name, age)
 }
 
 fn main() {
@@ -89,10 +118,13 @@ fn main() {
       stateful_command,
       async_simple_command,
       async_stateful_command,
+      command_arguments_wild,
+      command_arguments_struct,
       simple_command_with_result,
       stateful_command_with_result,
+      command_arguments_tuple_struct,
       async_simple_command_with_result,
-      async_stateful_command_with_result
+      async_stateful_command_with_result,
     ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");

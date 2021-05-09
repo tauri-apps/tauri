6659255930eb7f65fe38e180d8b1fb3da742ea1c
Lucas Nogueira <lucas@tauri.studio>
2021-05-09 11:33:32 -0300

    feat(ci): also rewrite changelog version

--- .scripts/sync-prerelease.js
+++ .scripts/sync-prerelease.js
@@ -12,12 +12,17 @@ const { readFileSync, writeFileSync } = require("fs")
 
 const runtimeManifestPath = '../../core/tauri-runtime/Cargo.toml'
 const dependencyManifestPaths = ['../../core/tauri/Cargo.toml']
+const changelogPath = '../../core/tauri-runtime/CHANGELOG.md'
 
 const bump = process.argv[2]
 
 let runtimeManifest = readFileSync(runtimeManifestPath, "utf-8")
 runtimeManifest = runtimeManifest.replace(/version = "(\d+\.\d+\.\d+)-[^0-9\.]+\.0"/, 'version = "$1"')
 writeFileSync(runtimeManifestPath, runtimeManifest)
+
+let changelog = readFileSync(changelogPath, "utf-8")
+changelog = changelog.replace(/(\d+\.\d+\.\d+)-[^0-9\.]+\.0/, '$1')
+writeFileSync(changelogPath, changelog)
 
 for (const dependencyManifestPath of dependencyManifestPaths) {
   let dependencyManifest = readFileSync(dependencyManifestPath, "utf-8")

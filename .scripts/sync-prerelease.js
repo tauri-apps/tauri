e764704a85afe1922471298df9c9aee32495143b
Lucas Nogueira <lucas@tauri.studio>
2021-05-09 09:53:50 -0300

    feat(ci): force tauri-runtime to be an alpha package

---
+++ .scripts/sync-prerelease.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+// Copyright 2019-2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+/*
+This script is solely intended to be run as part of the `covector version` step to
+keep the `tauri-release` crate version without the `beta` or `beta-rc` suffix.
+*/
+
+const { readFileSync, writeFileSync } = require("fs")
+
+const runtimeManifestPath = '../../core/tauri-runtime/Cargo.toml'
+const dependencyManifestPaths = ['../../core/tauri/Cargo.toml']
+
+const bump = process.argv[2]
+
+let runtimeManifest = readFileSync(runtimeManifestPath, "utf-8")
+runtimeManifest = runtimeManifest.replace(/version = "(\d+\.\d+\.\d+)-[^0-9\.]+\.0"/, 'version = "$1"')
+writeFileSync(runtimeManifestPath, runtimeManifest)
+
+for (const dependencyManifestPath of dependencyManifestPaths) {
+  let dependencyManifest = readFileSync(dependencyManifestPath, "utf-8")
+  dependencyManifest = dependencyManifest.replace(/tauri-runtime = { version = "(\d+\.\d+\.\d+)-[^0-9\.]+\.0"/, 'tauri-runtime = { version = "$1"')
+  writeFileSync(dependencyManifestPath, dependencyManifest)
+}

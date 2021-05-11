3557386663ec1854cd90f4d8769a117e0f78c0a3
Chip Reed <chip@chip.sh>
2021-05-11 10:31:05 -0700

    feat(examples): barebones custom param type example

---
+++ examples/params/src-tauri/build.rs
@@ -0,0 +1,7 @@
+// Copyright 2019-2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+fn main() {
+  tauri_build::build();
+}

47fc1d1e59fa64a8e63f0f8a86e73a48194ef33b
David Lemarier <david@lemarier.ca>
2021-05-19 13:18:43 -0400

    add `tauri_cpu_intensive` benchmark

--- core/tauri/bench/main.rs
+++ core/tauri/bench/main.rs
@@ -26,8 +26,14 @@ fn write_json(filename: &str, value: &Value) -> Result<()> {
 }
 
 /// The list of the examples of the benchmark name, arguments and return code
-const EXEC_TIME_BENCHMARKS: &[(&str, &str, Option<i32>)] =
-  &[("tauri_hello_world", "target/release/bench_helloworld", None)];
+const EXEC_TIME_BENCHMARKS: &[(&str, &str, Option<i32>)] = &[
+  ("tauri_hello_world", "target/release/bench_helloworld", None),
+  (
+    "tauri_cpu_intensive",
+    "target/release/bench_cpu_intensive",
+    None,
+  ),
+];
 
 fn run_strace_benchmarks(new_data: &mut BenchResult) -> Result<()> {
   use std::io::Read;

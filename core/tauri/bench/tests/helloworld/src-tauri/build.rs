448fa1c9d457a524095afa3866e69e7b12460ee7
David Lemarier <david@lemarier.ca>
2021-05-20 15:42:20 -0400

    fix windows build

--- core/tauri/bench/tests/helloworld/src-tauri/build.rs
+++ core/tauri/bench/tests/helloworld/src-tauri/build.rs
@@ -7,7 +7,7 @@ use tauri_build::{try_build, Attributes, WindowsAttributes};
 fn main() {
   if let Err(error) = try_build(
     Attributes::new()
-      .windows_attributes(WindowsAttributes::new().window_icon_path("../../.icons/icon.ico")),
+      .windows_attributes(WindowsAttributes::new().window_icon_path("../../../../../../examples/.icons/icon.ico")),
   ) {
     panic!("error found during tauri-build: {}", error);
   }

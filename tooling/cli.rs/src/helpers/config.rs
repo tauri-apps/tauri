47919619815900fc3af47ec5873e31afb778b0ad
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2021-06-15 21:12:34 -0300

    feat(bundler): allow setting wix language, closes #1976 (#1988)

--- tooling/cli.rs/src/helpers/config.rs
+++ tooling/cli.rs/src/helpers/config.rs
@@ -16,6 +16,7 @@ pub use config_definition::*;
 impl From<WixConfig> for tauri_bundler::WixSettings {
   fn from(config: WixConfig) -> tauri_bundler::WixSettings {
     tauri_bundler::WixSettings {
+      language: config.language,
       template: config.template,
       fragment_paths: config.fragment_paths,
       component_group_refs: config.component_group_refs,

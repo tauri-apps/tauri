66916b739ffbbb5d96850f1a098e6de22a229f6d
Amr Bashir <48618675+amrbashir@users.noreply.github.com>
2021-07-04 00:48:04 +0200

    chore: add `.prettierrc` in repo root (#2145)

--- tooling/bench/tests/cpu_intensive/public/worker.js
+++ tooling/bench/tests/cpu_intensive/public/worker.js
@@ -1,45 +1,45 @@
 const isPrime = (number) => {
   if (number % 2 === 0 && number > 2) {
-    return false;
+    return false
   }
 
-  let start = 2;
-  const limit = Math.sqrt(number);
+  let start = 2
+  const limit = Math.sqrt(number)
   while (start <= limit) {
     if (number % start++ < 1) {
-      return false;
+      return false
     }
   }
-  return number > 1;
-};
+  return number > 1
+}
 
-addEventListener("message", (e) => {
-  const { startTime } = e.data;
+addEventListener('message', (e) => {
+  const { startTime } = e.data
 
-  let n = 0;
-  let total = 0;
-  const THRESHOLD = e.data.value;
-  const primes = [];
+  let n = 0
+  let total = 0
+  const THRESHOLD = e.data.value
+  const primes = []
 
-  let previous = startTime;
+  let previous = startTime
 
   while (++n <= THRESHOLD) {
     if (isPrime(n)) {
-      primes.push(n);
-      total++;
+      primes.push(n)
+      total++
 
-      const now = Date.now();
+      const now = Date.now()
 
       if (now - previous > 250) {
-        previous = now;
+        previous = now
         postMessage({
-          status: "calculating",
+          status: 'calculating',
           count: total,
-          time: Date.now() - startTime,
-        });
+          time: Date.now() - startTime
+        })
       }
     }
   }
 
-  postMessage({ status: "done", count: total, time: Date.now() - startTime });
-});
+  postMessage({ status: 'done', count: total, time: Date.now() - startTime })
+})

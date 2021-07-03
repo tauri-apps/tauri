1110142bd36496092bb6d9440dd95917e1d11a9a
amrbashir <48618675+amrbashir@users.noreply.github.com>
2021-07-03 23:47:44 +0200

    chore: add `.prettierrc` in repo root

--- tooling/bench/tests/cpu_intensive/public/site.js
+++ tooling/bench/tests/cpu_intensive/public/site.js
@@ -1,59 +1,59 @@
 // Create web worker
-const THRESHOLD = 10000000;
-const worker = new Worker("worker.js");
+const THRESHOLD = 10000000
+const worker = new Worker('worker.js')
 /** @type {HTMLButtonElement} */
-const start = document.getElementById("start");
+const start = document.getElementById('start')
 /** @type {HTMLParagraphElement} */
-const status = document.getElementById("status");
-const results = document.getElementById("results");
+const status = document.getElementById('status')
+const results = document.getElementById('results')
 
-const ITERATIONS = 1;
+const ITERATIONS = 1
 
-let resolver;
+let resolver
 
 const onMessage = (message) => {
   // Update the UI
-  let prefix = "[Calculating]";
+  let prefix = '[Calculating]'
 
-  if (message.data.status === "done") {
+  if (message.data.status === 'done') {
     // tell tauri that we are done
-    window.__TAURI__.invoke("tauri", {
-      __tauriModule: "Process",
+    window.__TAURI__.invoke('tauri', {
+      __tauriModule: 'Process',
       message: {
-        cmd: "exit",
-        exitCode: 0,
-      },
-    });
+        cmd: 'exit',
+        exitCode: 0
+      }
+    })
   }
 
-  status.innerHTML = `${prefix} Found <code>${message.data.count}</code> prime numbers in <code>${message.data.time}ms</code>`;
+  status.innerHTML = `${prefix} Found <code>${message.data.count}</code> prime numbers in <code>${message.data.time}ms</code>`
 
-  if (message.data.status === "done") {
-    resolver(message.data.time);
+  if (message.data.status === 'done') {
+    resolver(message.data.time)
   }
-};
+}
 
-worker.addEventListener("message", onMessage);
+worker.addEventListener('message', onMessage)
 
 const benchmark = () => {
   return new Promise((resolve) => {
-    const startTime = Date.now();
-    resolver = resolve;
-    worker.postMessage({ value: THRESHOLD, startTime });
-  });
-};
+    const startTime = Date.now()
+    resolver = resolve
+    worker.postMessage({ value: THRESHOLD, startTime })
+  })
+}
 
 const calculate = async () => {
-  let total = 0;
+  let total = 0
 
   for (let i = 0; i < ITERATIONS; i++) {
-    const result = await benchmark();
-    total += result;
+    const result = await benchmark()
+    total += result
   }
 
-  const average = total / ITERATIONS;
+  const average = total / ITERATIONS
 
-  results.innerText = `Average time: ${average}ms`;
-};
+  results.innerText = `Average time: ${average}ms`
+}
 
-window.addEventListener("DOMContentLoaded", calculate);
+window.addEventListener('DOMContentLoaded', calculate)

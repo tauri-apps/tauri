0bdf01144f141979238f36502fa520535a687965
Ben Briggs <euph.briggs@gmail.com>
2021-07-11 08:10:36 -0500

    Removed debug output

--- tooling/bundler/src/bundle/windows/templates/install-task.ps1
+++ tooling/bundler/src/bundle/windows/templates/install-task.ps1
@@ -1,6 +1,6 @@
 # Adapted from https://superuser.com/a/532109
+param([string]$ChangeDir, [switch]$Elevated)
 
-param([switch]$Elevated)
 
 function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
@@ -10,16 +10,17 @@ function Test-Admin {
 if ((Test-Admin) -eq $false) {
     if ($elevated) {
         # tried to elevate, did not work, aborting
-        Write-Host "Failed to elevate"
     }
     else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-        Write-Host "Attempted to elevate"
+        $InstallDirectory = Get-Location
+        $ArgList = ('-File "{0}" -ChangeDir "{1}" -Elevated' -f ($myinvocation.MyCommand.Definition, $InstallDirectory))
+        Start-Process powershell.exe -Verb RunAs -ArgumentList $ArgList
     }
-    PAUSE
     exit
 }
 
-Write-Host "Elevated"
-SCHTASKS.EXE /CREATE /XML "C:\Program Files\updater-example\update.xml" /TN "Update {{{product_name}}} - Skip UAC" /F
-PAUSE
+if ($ChangeDir -ne "") {
+    # Change directories to the install path
+    Set-Location -Path $ChangeDir
+}
+SCHTASKS.EXE /CREATE /XML update.xml /TN "Update {{{product_name}}} - Skip UAC" /F

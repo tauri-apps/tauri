8362f75de6415c6a87e1266d0a69e4def5926516
Ben Briggs <euph.briggs@gmail.com>
2021-07-11 07:17:38 -0500

    Successful with hard-coded install path

--- tooling/bundler/src/bundle/windows/templates/install-task.ps1
+++ tooling/bundler/src/bundle/windows/templates/install-task.ps1
@@ -10,13 +10,16 @@ function Test-Admin {
 if ((Test-Admin) -eq $false) {
     if ($elevated) {
         # tried to elevate, did not work, aborting
+        Write-Host "Failed to elevate"
     }
     else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)) -WorkingDirectory Get-Location
-        pause
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+        Write-Host "Attempted to elevate"
     }
+    PAUSE
     exit
 }
 
-SCHTASKS.EXE /CREATE /XML ".\update.xml" /TN "Update {{{product_name}}} - Skip UAC" /F
-pause
+Write-Host "Elevated"
+SCHTASKS.EXE /CREATE /XML "C:\Program Files\updater-example\update.xml" /TN "Update {{{product_name}}} - Skip UAC" /F
+PAUSE

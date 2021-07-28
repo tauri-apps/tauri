11547159ff7ced4a097646749eeb2c6383f1897f
amrbashir <48618675+amrbashir@users.noreply.github.com>
2021-07-28 08:37:48 +0200

    cleanup ?!

--- core/tauri/src/endpoints/operating_system.rs
+++ core/tauri/src/endpoints/operating_system.rs
@@ -20,18 +20,20 @@ impl Cmd {
   #[allow(unused_variables)]
   pub fn run(self) -> crate::Result<InvokeResponse> {
     #[cfg(os_all)]
-    return Ok(match self {
-      Self::Platform => std::env::consts::OS.into(),
-      Self::Version => os_info::get().version().to_string().into(),
-      #[cfg(target_os = "linux")]
-      Self::Type => "Linux".into(),
-      #[cfg(target_os = "windows")]
-      Self::Type => "Windows_NT".into(),
-      #[cfg(target_os = "macos")]
-      Self::Type => "Darwin".into(),
-      Self::Arch => std::env::consts::ARCH.into(),
-      Self::Tempdir => std::env::temp_dir().into(),
-    });
+    return match self {
+      Self::Platform => Ok(std::env::consts::OS.into()),
+      Self::Version => Ok(os_info::get().version().to_string().into()),
+      Self::Type => {
+        #[cfg(target_os = "linux")]
+        return Ok("Linux".into());
+        #[cfg(target_os = "windows")]
+        return Ok("Windows_NT".into());
+        #[cfg(target_os = "macos")]
+        return Ok("Darwing".into());
+      }
+      Self::Arch => Ok(std::env::consts::ARCH.into()),
+      Self::Tempdir => Ok(std::env::temp_dir().into()),
+    };
     #[cfg(not(os_all))]
     Err(crate::Error::ApiNotAllowlisted("os".into()))
   }

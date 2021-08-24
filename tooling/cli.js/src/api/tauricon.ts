8e177627d984e769cc27ccb8a0c3ab9b7b38f429
Lucas Nogueira <lucas@tauri.studio>
2021-08-24 11:20:33 -0300

    fmt

--- tooling/cli.js/src/api/tauricon.ts
+++ tooling/cli.js/src/api/tauricon.ts
@@ -149,10 +149,10 @@ const hexToRgb = (
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   return result
     ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    }
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      }
     : undefined
 }
 

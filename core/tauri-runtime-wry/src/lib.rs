7b629954c14892922a6e6ff6d4f833f7055bad0e
Bill Avery <wravery@gmail.com>
2021-09-19 22:05:02 -0700

    Pick up latest branch of TAO and WRY

--- core/tauri-runtime-wry/src/lib.rs
+++ core/tauri-runtime-wry/src/lib.rs
@@ -2036,7 +2036,7 @@ fn handle_event_loop(
             #[cfg(target_os = "macos")]
             WindowMessage::NSWindow(tx) => tx.send(NSWindow(window.ns_window())).unwrap(),
             #[cfg(windows)]
-            WindowMessage::Hwnd(tx) => tx.send(Hwnd(window.hwnd() as HWND)).unwrap(),
+            WindowMessage::Hwnd(tx) => tx.send(Hwnd(HWND(window.hwnd() as _))).unwrap(),
             #[cfg(any(
               target_os = "linux",
               target_os = "dragonfly",

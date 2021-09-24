4f4baef2d2053fd9caa5957adc309bc93863bb2e
Lucas Nogueira <lucas@tauri.studio>
2021-09-24 10:08:15 -0300

    feat(cli): do not enforce `src-tauri` folder structure, closes #2643

--- tooling/cli.rs/src/helpers/app_paths.rs
+++ tooling/cli.rs/src/helpers/app_paths.rs
@@ -6,34 +6,42 @@ use std::{env::current_dir, path::PathBuf};
 
 use once_cell::sync::Lazy;
 
-fn get_app_dir() -> PathBuf {
-  let mut dir = current_dir().expect("failed to read cwd");
+fn get_tauri_dir() -> PathBuf {
+  glob::glob(
+    &current_dir()
+      .expect("failed to read cwd")
+      .join("**/tauri.conf.json")
+      .to_string_lossy()
+      .into_owned(),
+  )
+  .unwrap()
+  .filter_map(Result::ok)
+  .last()
+  .map(|p| p.parent().unwrap().to_path_buf())
+  .expect("Couldn't recognize the current folder as a Tauri project. It must contain a `tauri.conf.json` file in any subfolder.")
+}
 
-  let mut count = 0;
-
-  // only go up three folders max
-  while count <= 2 {
-    let test_path = dir.join("src-tauri/tauri.conf.json");
-    if test_path.exists() {
-      return dir;
-    }
-    count += 1;
-    match dir.parent() {
-      Some(parent) => {
-        dir = parent.to_path_buf();
-      }
-      None => break,
-    }
-  }
-
-  panic!("Couldn't recognize the current folder as a Tauri project.")
+fn get_app_dir() -> Option<PathBuf> {
+  glob::glob(
+    &current_dir()
+      .expect("failed to read cwd")
+      .join("**/package.json")
+      .to_string_lossy()
+      .into_owned(),
+  )
+  .unwrap()
+  .filter_map(Result::ok)
+  .filter(|p| !p.to_string_lossy().into_owned().contains("node_modules"))
+  .last()
+  .map(|p| p.parent().unwrap().to_path_buf())
 }
 
 pub fn app_dir() -> &'static PathBuf {
-  static APP_DIR: Lazy<PathBuf> = Lazy::new(get_app_dir);
+  static APP_DIR: Lazy<PathBuf> =
+    Lazy::new(|| get_app_dir().unwrap_or_else(|| get_tauri_dir().parent().unwrap().to_path_buf()));
   &APP_DIR
 }
 
 pub fn tauri_dir() -> PathBuf {
-  app_dir().join("src-tauri")
+  get_tauri_dir()
 }

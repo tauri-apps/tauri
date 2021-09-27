6d6870503006cc675e92051fa7af064fd6b31bc4
Lucas Nogueira <lucas@tauri.studio>
2021-09-27 19:11:01 -0300

    feat(ci): cache cli.js's template test

--- tooling/cli.js/test/jest/__tests__/template.spec.js
+++ tooling/cli.js/test/jest/__tests__/template.spec.js
@@ -1,22 +1,33 @@
 import * as fixtureSetup from '../fixtures/app-test-setup.js'
 import { resolve, dirname } from 'path'
-import { writeFileSync, readFileSync } from 'fs'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
+import { move } from 'fs-extra'
 import { init, build } from 'dist/api/cli'
 import { fileURLToPath } from 'url'
 
-const currentDirName = dirname(fileURLToPath(import.meta.url))
+const currentDirName = dirname(fileURLToPath(
+  import.meta.url))
 
 describe('[CLI] cli.js template', () => {
   it('init a project and builds it', async () => {
     const cwd = process.cwd()
     const fixturePath = resolve(currentDirName, '../fixtures/empty')
     const tauriFixturePath = resolve(fixturePath, 'src-tauri')
+    const outPath = resolve(tauriFixturePath, 'target')
+    const cacheOutPath = resolve(fixturePath, 'target')
 
     fixtureSetup.initJest('empty')
 
     process.chdir(fixturePath)
 
-    const { promise } = await init({
+    const outExists = existsSync(outPath)
+    if (outExists) {
+      await move(outPath, cacheOutPath)
+    }
+
+    const {
+      promise
+    } = await init({
       directory: process.cwd(),
       force: true,
       tauriPath: resolve(currentDirName, '../../../../..'),
@@ -24,13 +35,19 @@ describe('[CLI] cli.js template', () => {
     })
     await promise
 
+    if (outExists) {
+      await move(cacheOutPath, outPath)
+    }
+
     process.chdir(tauriFixturePath)
 
     const manifestPath = resolve(tauriFixturePath, 'Cargo.toml')
     const manifestFile = readFileSync(manifestPath).toString()
     writeFileSync(manifestPath, `workspace = { }\n${manifestFile}`)
 
-    const { promise: buildPromise } = await build()
+    const {
+      promise: buildPromise
+    } = await build()
     await buildPromise
     process.chdir(cwd)
   })

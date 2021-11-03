5f3487d0c4c8456d584d7e6c1d2ac2ab1cfe4aab
chip <chip@chip.sh>
2021-11-02 19:13:14 -0700

    Resolve symlinks during `tauri::api::process::current_binary` [TRI-047] (#29)
    * create integration test for tauri process restart
    
    most notably, this integration test is entirely self-contained and
    should successfully run with a regular `cargo test`.
    
    * cargo fmt
    
    * canonicalize the current_binary path to resolve links
    
    * add licensing metadata to restart test cargo project
    
    * remove use statements for simpler restart test binary code
    
    * allow windows to fail creating the symlink due to permissions
    
    * provide an overload of information on current_binary
    
    * add nested symlinking to the restart test
    
    * user -> attacker
    
    * grammar (i think)

--- core/tauri/src/api/process.rs
+++ core/tauri/src/api/process.rs
@@ -6,11 +6,7 @@
 
 use crate::Env;
 
-use std::{
-  env,
-  path::PathBuf,
-  process::{exit, Command as StdCommand},
-};
+use std::path::PathBuf;
 
 #[cfg(feature = "command")]
 #[cfg_attr(doc_cfg, doc(cfg(feature = "command")))]
@@ -19,32 +15,93 @@ mod command;
 #[cfg_attr(doc_cfg, doc(cfg(feature = "command")))]
 pub use command::*;
 
-/// Gets the current binary.
+/// Finds the current running binary's path.
+///
+/// # Platform-specific behavior
+///
+/// On the `Linux` platform, this function will also **attempt** to detect if
+/// it's currently running from a valid [AppImage] and use that path instead.
+///
+/// # Security
+///
+/// If the above Platform-specific behavior does not take place, this function
+/// uses [`std::env::current_exe`]. Notably, it also has a security section
+/// that goes over a theoretical attack using hard links. Let's cover some
+/// specific topics that relate to different ways an attacker might try to
+/// trick this function into returning the wrong binary path.
+///
+/// ## Symlinks ("Soft Links")
+///
+/// [`std::path::Path::canonicalize`] is used to resolve symbolic links to the
+/// original path, including nested symbolic links (`link2 -> link1 -> bin`).
+///
+/// ## Hard Links
+///
+/// A [Hard Link] is a named entry that points to a file in the file system.
+/// On most systems, this is what you would think of as a "file". The term is
+/// used on filesystems that allow multiple entries to point to the same file.
+/// The linked [Hard Link] Wikipedia page provides a decent overview.
+///
+/// In short, unless the attacker was able to create the link with elevated
+/// permissions, it should generally not be possible for an them to hard link
+/// to a file they do not have permissions to - with exception to possible
+/// operating system exploits.
+///
+/// There are also some platform-specific information about this below.
+///
+/// ### Windows
+///
+/// Windows requires a permission to be set for the user to create a symlink
+/// or a hard link, regardless of ownership status of the target. Elevated
+/// permissions users have the ability to create them.
+///
+/// ### macOS
+///
+/// macOS allows for the creation of symlinks and hard links to any file.
+/// Accessing through those links will fail if the user who owns the links
+/// does not have the proper permissions on the original file.
+///
+/// ### Linux
+///
+/// Linux allows for the creation of symlinks to any file. Accessing the
+/// symlink will fail if the user who owns the symlink does not have the
+/// proper permissions on the original file.
+///
+/// Linux additionally provides a kernel hardening feature since version
+/// 3.6 (30 September 2012). Most distributions since then have enabled
+/// the protection (setting `fs.protected_hardlinks = 1`) by default, which
+/// means that a vast majority of desktop Linux users should have it enabled.
+/// **The feature prevents the creation of hardlinks that the user does not own
+/// or have read/write access to.** [See the patch that enabled this.](https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=800179c9b8a1e796e441674776d11cd4c05d61d7)
+///
+/// [AppImage]: https://appimage.org/
+/// [Hard Link]: https://en.wikipedia.org/wiki/Hard_link
 #[allow(unused_variables)]
 pub fn current_binary(env: &Env) -> Option<PathBuf> {
-  let mut current_binary = None;
-
-  // if we are running with an APP Image, we should return the app image path
+  // if we are running from an AppImage, we ONLY want the set AppImage path
   #[cfg(target_os = "linux")]
   if let Some(app_image_path) = &env.appimage {
-    current_binary = Some(PathBuf::from(app_image_path));
+    return Some(PathBuf::from(app_image_path));
   }
 
-  // if we didn't extracted binary in previous step,
-  // let use the current_exe from current environment
-  if current_binary.is_none() {
-    if let Ok(current_process) = env::current_exe() {
-      current_binary = Some(current_process);
-    }
-  }
-
-  current_binary
+  // see https://doc.rust-lang.org/std/env/fn.current_exe.html#security for
+  // an example of what to be careful of when using `current_exe` output.
+  std::env::current_exe()
+    .ok()
+    // we canonicalize the path we received from `current_exe` to resolve any
+    // soft links. it avoids the usual issue of needing the file to exist at
+    // the passed path because a valid `current_exe` result should always exist
+    .and_then(|path| path.canonicalize().ok())
 }
 
 /// Restarts the process.
+///
+/// See [`current_binary`] for the possible security implications.
 pub fn restart(env: &Env) {
+  use std::process::{exit, Command};
+
   if let Some(path) = current_binary(env) {
-    StdCommand::new(path)
+    Command::new(path)
       .spawn()
       .expect("application failed to start");
   }

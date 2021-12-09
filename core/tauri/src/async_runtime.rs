156cdbc9f8b9b8e79a7da4d042e9b06800cf4b6d
Chaoqian Xu <chaoranxu@gmail.com>
2021-12-09 23:22:32 +0800

    fix: extract an entire source archive into a specified path is not working if the archive includes dirs. (#2997)
    Co-authored-by: Lucas Nogueira <lucas@tauri.studio>

--- core/tauri/src/async_runtime.rs
+++ core/tauri/src/async_runtime.rs
@@ -326,7 +326,11 @@ mod tests {
   #[tokio::test]
   async fn handle_abort() {
     let handle = handle();
-    let join = handle.spawn(async { 5 });
+    let join = handle.spawn(async {
+      // Here we sleep 1 second to ensure this task to be uncompleted when abort() invoked.
+      tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+      5
+    });
     join.abort();
     if let crate::Error::JoinError(raw_box) = join.await.unwrap_err() {
       let raw_error = raw_box.downcast::<tokio::task::JoinError>().unwrap();

d5e6046756f7ab0badf9351ac79c5f61362ef35b
FabianLars <fabianlars@fabianlars.de>
2022-01-13 17:34:31 +0100

    Fix broken links to tauri.studio docs (#3212)

--- examples/api/src/App.svelte
+++ examples/api/src/App.svelte
@@ -115,7 +115,7 @@
   <div class="flex row noselect just-around" style="margin=1em;" data-tauri-drag-region>
     <img class="logo" src="tauri logo.png" height="60" on:click={onLogoClick} alt="logo" />
     <div>
-      <a class="dark-link" target="_blank" href="https://tauri.studio/en/docs/getting-started/intro">
+      <a class="dark-link" target="_blank" href="https://tauri.studio/en/docs/get-started/intro">
         Documentation
       </a>
       <a class="dark-link" target="_blank" href="https://github.com/tauri-apps/tauri">

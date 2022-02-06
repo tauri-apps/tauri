6a542b2b465546e228843135a3a5ed8e0242842e
FabianLars <fabianlars@fabianlars.de>
2022-02-06 13:45:05 +0100

    fix missing regex import if shell-open is enabled

--- core/tauri/src/lib.rs
+++ core/tauri/src/lib.rs
@@ -133,7 +133,7 @@
 pub use embed_plist;
 /// The Tauri error enum.
 pub use error::Error;
-#[cfg(feature = "shell-execute")]
+#[cfg(any(feature = "shell-execute", feature = "shell-open-api"))]
 #[doc(hidden)]
 pub use regex;
 pub use tauri_macros::{command, generate_handler};

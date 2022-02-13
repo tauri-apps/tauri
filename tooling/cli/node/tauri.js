b94d06f5e5f38fef2710154488fb745ad16a2327
Lucas Nogueira <lucas@tauri.studio>
2022-02-13 10:50:56 -0300

    fmt

--- tooling/cli/node/tauri.js
+++ tooling/cli/node/tauri.js
@@ -43,7 +43,7 @@ if (binStem === 'node' || binStem === 'nodejs') {
   arguments.unshift(bin)
 }
 
-cli.run(arguments, binName).catch(err => {
+cli.run(arguments, binName).catch((err) => {
   console.log(`Error running CLI: ${err.message}`)
   process.exit(1)
 })

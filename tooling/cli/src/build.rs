4562e671e4795e9386429348bf738f7078706945
Alex Rock <pierstoval@gmail.com>
2022-05-03 16:57:11 +0000

    fix(build): append .exe binary based on target triple instead of running OS, closes #3870 (#4032)
    Co-authored-by: Lucas Nogueira <lucas@tauri.studio>

--- tooling/cli/src/build.rs
+++ tooling/cli/src/build.rs
@@ -168,10 +168,20 @@ pub fn command(options: Options) -> Result<()> {
     .name
     .clone()
     .expect("Cargo manifest must have the `package.name` field");
-  #[cfg(windows)]
-  let bin_path = out_dir.join(format!("{}.exe", bin_name));
-  #[cfg(not(windows))]
-  let bin_path = out_dir.join(&bin_name);
+
+  let target: String = if let Some(target) = options.target.clone() {
+    target
+  } else {
+    tauri_utils::platform::target_triple()?
+  };
+  let binary_extension: String = if target.contains("windows") {
+    ".exe"
+  } else {
+    ""
+  }
+  .into();
+
+  let bin_path = out_dir.join(&bin_name).with_extension(&binary_extension);
 
   let no_default_features = args.contains(&"--no-default-features".into());
 
@@ -211,12 +221,13 @@ pub fn command(options: Options) -> Result<()> {
   }
 
   if let Some(product_name) = config_.package.product_name.clone() {
-    #[cfg(windows)]
-    let product_path = out_dir.join(format!("{}.exe", product_name));
-    #[cfg(target_os = "macos")]
-    let product_path = out_dir.join(product_name);
     #[cfg(target_os = "linux")]
-    let product_path = out_dir.join(product_name.to_kebab_case());
+    let product_name = product_name.to_kebab_case();
+
+    let product_path = out_dir
+      .join(&product_name)
+      .with_extension(&binary_extension);
+
     rename(&bin_path, &product_path).with_context(|| {
       format!(
         "failed to rename `{}` to `{}`",
@@ -314,7 +325,7 @@ pub fn command(options: Options) -> Result<()> {
     }
     let settings = crate::interface::get_bundler_settings(
       app_settings,
-      options.target.clone(),
+      target,
       &enabled_features,
       &manifest,
       config_,

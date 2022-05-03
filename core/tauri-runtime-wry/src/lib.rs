4c06171f2354bcf58ff93c704033e688782cb7e3
Lucas Nogueira <lucas@tauri.studio>
2022-05-03 12:24:46 -0300

    chore: only decode on Linux

--- core/tauri-runtime-wry/src/lib.rs
+++ core/tauri-runtime-wry/src/lib.rs
@@ -923,21 +923,41 @@ impl WindowBuilder for WindowBuilderWrapper {
 
 pub struct FileDropEventWrapper(WryFileDropEvent);
 
-fn percent_decode_path(path: PathBuf) -> PathBuf {
+// on Linux, the paths are percent-encoded
+#[cfg(any(
+  target_os = "linux",
+  target_os = "dragonfly",
+  target_os = "freebsd",
+  target_os = "netbsd",
+  target_os = "openbsd"
+))]
+fn decode_path(path: PathBuf) -> PathBuf {
   percent_encoding::percent_decode(path.display().to_string().as_bytes())
     .decode_utf8_lossy()
     .into_owned()
     .into()
 }
 
+// on Windows and macOS, we do not need to decode the path
+#[cfg(not(any(
+  target_os = "linux",
+  target_os = "dragonfly",
+  target_os = "freebsd",
+  target_os = "netbsd",
+  target_os = "openbsd"
+)))]
+fn decode_path(path: PathBuf) -> PathBuf {
+  path
+}
+
 impl From<FileDropEventWrapper> for FileDropEvent {
   fn from(event: FileDropEventWrapper) -> Self {
     match event.0 {
       WryFileDropEvent::Hovered(paths) => {
-        FileDropEvent::Hovered(paths.into_iter().map(percent_decode_path).collect())
+        FileDropEvent::Hovered(paths.into_iter().map(decode_path).collect())
       }
       WryFileDropEvent::Dropped(paths) => {
-        FileDropEvent::Dropped(paths.into_iter().map(percent_decode_path).collect())
+        FileDropEvent::Dropped(paths.into_iter().map(decode_path).collect())
       }
       // default to cancelled
       // FIXME(maybe): Add `FileDropEvent::Unknown` event?

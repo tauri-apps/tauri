deeadfc0facb90956ec0706b268096d23e7e54a7
Lucas Nogueira <lucas@tauri.studio>
2022-05-13 09:32:43 -0700

    feat(build): find .ico in config instead of default `icons/icon.ico`

--- examples/updater/src-tauri/build.rs
+++ examples/updater/src-tauri/build.rs
@@ -2,13 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-use tauri_build::{try_build, Attributes, WindowsAttributes};
-
 fn main() {
-  if let Err(error) = try_build(
-    Attributes::new()
-      .windows_attributes(WindowsAttributes::new().window_icon_path("../../.icons/icon.ico")),
-  ) {
-    panic!("error found during tauri-build: {:#?}", error);
-  }
+  tauri_build::build()
 }

8eb67247ada8f39f8c41d29ce6817aa6dcc25af9
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-05-17 23:45:46 +0100

    Fix config links

--- core/tauri-utils/src/config.rs
+++ core/tauri-utils/src/config.rs
@@ -2248,10 +2248,10 @@ impl PackageConfig {
 ///
 /// `tauri.conf.json` is composed of the following objects:
 ///
-/// - [`build`](BuildConfig): The build configuration
-/// - [`package`](PackageConfig): Package settings
-/// - [`plugins`](PluginConfig): The plugins config
-/// - [`tauri`](TauriConfig): The Tauri config
+/// - [`build`](#BuildConfig): The build configuration
+/// - [`package`](#PackageConfig): Package settings
+/// - [`plugins`](#PluginConfig): The plugins config
+/// - [`tauri`](#TauriConfig): The Tauri config
 ///
 /// ```json title="Example tauri.config.json file"
 /// {

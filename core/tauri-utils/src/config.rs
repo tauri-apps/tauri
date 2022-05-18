e010b517b9a59f0ea106fd76cbf85f96f4d67c9f
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-05-18 12:25:55 +0100

    Revise `HTTPAllowlistScope` description [skip ci]

--- core/tauri-utils/src/config.rs
+++ core/tauri-utils/src/config.rs
@@ -1378,10 +1378,10 @@ impl Allowlist for DialogAllowlistConfig {
 /// It is a list of URLs that can be accessed by the webview when using the HTTP APIs.
 /// The scoped URL is matched against the request URL using a glob pattern.
 ///
-/// # Examples
-/// - "https://**": allows all HTTPS urls
-/// - "https://*.github.com/tauri-apps/tauri": allows any subdomain of "github.com" with the "tauri-apps/api" path
-/// - "https://myapi.service.com/users/*": allows access to any URLs that begins with "https://myapi.service.com/users/"
+/// Examples:
+///  - "https://**": allows all HTTPS urls
+///  - "https://*.github.com/tauri-apps/tauri": allows any subdomain of "github.com" with the "tauri-apps/api" path
+///  - "https://myapi.service.com/users/*": allows access to any URLs that begins with "https://myapi.service.com/users/"
 #[allow(rustdoc::bare_urls)]
 #[derive(Debug, Default, PartialEq, Clone, Deserialize, Serialize)]
 #[cfg_attr(feature = "schema", derive(JsonSchema))]

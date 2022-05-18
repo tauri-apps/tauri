f8659fcc3ac8c01b207ccb70c84bc4784ec11124
Lucas Nogueira <lucas@tauri.studio>
2022-05-18 09:55:17 -0300

    remove empty space [skip ci]

--- core/tauri-utils/src/config.rs
+++ core/tauri-utils/src/config.rs
@@ -1379,9 +1379,9 @@ impl Allowlist for DialogAllowlistConfig {
 /// The scoped URL is matched against the request URL using a glob pattern.
 ///
 /// Examples:
-///  - "https://**": allows all HTTPS urls
-///  - "https://*.github.com/tauri-apps/tauri": allows any subdomain of "github.com" with the "tauri-apps/api" path
-///  - "https://myapi.service.com/users/*": allows access to any URLs that begins with "https://myapi.service.com/users/"
+/// - "https://**": allows all HTTPS urls
+/// - "https://*.github.com/tauri-apps/tauri": allows any subdomain of "github.com" with the "tauri-apps/api" path
+/// - "https://myapi.service.com/users/*": allows access to any URLs that begins with "https://myapi.service.com/users/"
 #[allow(rustdoc::bare_urls)]
 #[derive(Debug, Default, PartialEq, Clone, Deserialize, Serialize)]
 #[cfg_attr(feature = "schema", derive(JsonSchema))]

5088f15bb7123a0c714404b98b7744deeeb62910
Lucas Nogueira <lucas@tauri.studio>
2022-05-27 11:50:17 -0300

    add license to new file

--- core/tauri-build/src/static_vcruntime.rs
+++ core/tauri-build/src/static_vcruntime.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 // taken from https://github.com/ChrisDenton/static_vcruntime/
 // we're not using static_vcruntime directly because we want this for debug builds too
 

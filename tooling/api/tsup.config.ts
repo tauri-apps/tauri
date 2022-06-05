d4a2289e4c07bfa4f5fb2a4d760c009de91fbd73
amrbashir <amr.bashir2015@gmail.com>
2022-06-05 14:15:51 +0200

    fix withGlobalTauri

--- tooling/api/tsup.config.ts
+++ tooling/api/tsup.config.ts
@@ -16,10 +16,15 @@ export default defineConfig(() => [
     entry: { bundle: 'src/index.ts' },
     outDir: '../../core/tauri/scripts',
     format: ['iife'],
-    globalName: '__TAURI__',
+    globalName: '__TAURI_IIFE__',
     clean: false,
     minify: true,
     platform: 'browser',
-    dts: false
+    dts: false,
+    // esbuild `globalName` option generates `var __TAURI_IIFE__ = (() => {})()`
+    // but we want to assign it to the `window` object, this footer takes care of that
+    footer: {
+      js: 'window.__TAURI__ = __TAURI_IIFE__'
+    }
   }
 ])

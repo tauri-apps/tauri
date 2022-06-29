5105b428c4726b2179cd4b3244350d1a1ee73734
Lucas Fernandes Nogueira <lucas@tauri.studio>
2022-06-29 14:03:42 -0300

    feat(cli): change app template to only set default menu on macOS (#4518)

--- examples/resources/src-tauri/src/main.rs
+++ examples/resources/src-tauri/src/main.rs
@@ -16,7 +16,11 @@ fn main() {
   let context = tauri::generate_context!();
 
   tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
+    .menu(if cfg!(target_os = "macos") {
+      tauri::Menu::os_default(&context.package_info().name)
+    } else {
+      tauri::Menu::default()
+    })
     .setup(move |app| {
       let window = app.get_window("main").unwrap();
       let script_path = app

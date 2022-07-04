3fbaee454de1608a4d18eb72781aa545f8892463
Amr Bashir <amr.bashir2015@gmail.com>
2022-07-04 03:15:23 +0200

    refactor(examples/api): new look (#4465)
    Co-authored-by: Lucas Nogueira <lucas@tauri.studio>

---
+++ examples/api/src/views/Communication.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { listen, emit } from '@tauri-apps/api/event'
+  import { invoke } from '@tauri-apps/api/tauri'
+  import { onMount, onDestroy } from 'svelte'
+
+  export let onMessage
+  let unlisten
+
+  onMount(async () => {
+    unlisten = await listen('rust-event', onMessage)
+  })
+  onDestroy(() => {
+    if (unlisten) {
+      unlisten()
+    }
+  })
+
+  function log() {
+    invoke('log_operation', {
+      event: 'tauri-click',
+      payload: 'this payload is optional because we used Option in Rust'
+    })
+  }
+
+  function performRequest() {
+    invoke('perform_request', {
+      endpoint: 'dummy endpoint arg',
+      body: {
+        id: 5,
+        name: 'test'
+      }
+    })
+      .then(onMessage)
+      .catch(onMessage)
+  }
+
+  function emitEvent() {
+    emit('js-event', 'this is the payload string')
+  }
+</script>
+
+<div>
+  <button class="btn" id="log" on:click={log}>Call Log API</button>
+  <button class="btn" id="request" on:click={performRequest}>
+    Call Request (async) API
+  </button>
+  <button class="btn" id="event" on:click={emitEvent}>
+    Send event to Rust
+  </button>
+</div>
--- examples/api/vite.config.js
+++ examples/api/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import Unocss from 'unocss/vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [Unocss(), svelte()],
   build: {
     rollupOptions: {
       output: {

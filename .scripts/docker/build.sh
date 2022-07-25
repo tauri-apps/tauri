c9f745b4897c00bb431056ea22b9ebc9d5836e9e
Lucas Nogueira <lucas@tauri.studio>
2022-07-25 12:49:28 -0300

    append org name to tag

--- .scripts/docker/build.sh
+++ .scripts/docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t ghcr.io/tauri/aarch64-unknown-linux-gnu:latest --file .docker/cross/aarch64.Dockerfile .docker/cross
+docker build -t ghcr.io/tauri-apps/tauri/aarch64-unknown-linux-gnu:latest --file .docker/cross/aarch64.Dockerfile .docker/cross

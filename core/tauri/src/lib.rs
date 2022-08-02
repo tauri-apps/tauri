5ec7c34845a4174dace5909ea6bde21ebaeaac76
Lucas Nogueira <lucas@tauri.studio>
2022-08-02 11:09:38 -0300

    feat: proxy for attohttpc tls-vendored

--- core/tauri/src/lib.rs
+++ core/tauri/src/lib.rs
@@ -22,7 +22,8 @@
 //! - **http-api**: Enables the [`api::http`] module.
 //! - **http-multipart**: Adds support to `multipart/form-data` requests.
 //! - **reqwest-client**: Uses `reqwest` as HTTP client on the `http` APIs. Improves performance, but increases the bundle size.
-//! - **reqwest-native-tls-vendored**: Compile and statically link to a vendored copy of OpenSSL.
+//! - **native-tls-vendored**: Compile and statically link to a vendored copy of OpenSSL (applies to the default HTTP client).
+//! - **reqwest-native-tls-vendored**: Compile and statically link to a vendored copy of OpenSSL (applies to the `reqwest` HTTP client).
 //! - **process-command-api**: Enables the [`api::process::Command`] APIs.
 //! - **global-shortcut**: Enables the global shortcut APIs.
 //! - **clipboard**: Enables the clipboard APIs.

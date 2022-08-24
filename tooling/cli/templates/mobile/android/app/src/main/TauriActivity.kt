6006a600ded68b7ea83ceb854b0383f8b50a614a
Lucas Nogueira <lucas@tauri.studio>
2022-08-24 14:04:46 -0300

    add dummy TauriActivity.kt

---
+++ tooling/cli/templates/mobile/android/app/src/main/TauriActivity.kt
@@ -0,0 +1,6 @@
+package {{app-domain-reversed}}.{{app-name-snake-case}}
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class TauriActivity : AppCompatActivity()

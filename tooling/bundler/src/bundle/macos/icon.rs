4e7c1d76dae5250ccaae5ff47a7251479bbe9ba3
Lucas Nogueira <lucas@tauri.studio>
2022-08-27 10:10:34 -0300

    feat: add cargo-bundle to the bundler license headers

--- tooling/bundler/src/bundle/macos/icon.rs
+++ tooling/bundler/src/bundle/macos/icon.rs
@@ -1,3 +1,4 @@
+// Copyright 2016-2019 Cargo-Bundle developers <https://github.com/burtonageo/cargo-bundle>
 // Copyright 2019-2021 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT

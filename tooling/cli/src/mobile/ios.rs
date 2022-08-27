dee19784f9ab6731aae2ae376c828d61427f20a2
Lucas Fernandes Nogueira <lucas@tauri.studio>
2022-08-27 16:35:57 -0300

    fix(cli): only require development team when running iOS commands (#5071)

--- tooling/cli/src/mobile/ios.rs
+++ tooling/cli/src/mobile/ios.rs
@@ -115,7 +115,7 @@ fn with_config<T>(
     let tauri_config_guard = tauri_config.lock().unwrap();
     let tauri_config_ = tauri_config_guard.as_ref().unwrap();
     let cli_options = cli_options.unwrap_or_else(|| read_options(tauri_config_, MobileTarget::Ios));
-    let (config, metadata) = get_config(tauri_config_, &cli_options);
+    let (config, metadata) = get_config(tauri_config_, &cli_options, MobileTarget::Ios);
     (config, metadata, cli_options)
   };
   f(&config, config.apple(), metadata.apple(), cli_options)

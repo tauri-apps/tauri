63c6bb754386f68c42a93fd164d7ac2e3f9a98b0
Lucas Nogueira <lucas@tauri.studio>
2022-08-31 10:51:05 -0300

    render template in prefix-path helper

--- tooling/cli/src/mobile/init.rs
+++ tooling/cli/src/mobile/init.rs
@@ -352,20 +352,21 @@ fn app_root(ctx: &Context) -> Result<&str, RenderError> {
 
 fn prefix_path(
   helper: &Helper,
-  _: &Handlebars,
+  handlebars: &Handlebars,
   ctx: &Context,
   _: &mut RenderContext,
   out: &mut dyn Output,
 ) -> HelperResult {
   out
     .write(
-      util::prefix_path(app_root(ctx)?, get_str(helper))
-        .to_str()
-        .ok_or_else(|| {
-          RenderError::new(
-            "Either the `app.root-dir` or the specified path contained invalid UTF-8.",
-          )
-        })?,
+      util::prefix_path(
+        app_root(ctx)?,
+        handlebars.render_template(get_str(helper), ctx.data())?,
+      )
+      .to_str()
+      .ok_or_else(|| {
+        RenderError::new("Either the `app.root-dir` or the specified path contained invalid UTF-8.")
+      })?,
     )
     .map_err(Into::into)
 }

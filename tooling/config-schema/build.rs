69f7d1774b5b7370533b3aa0d7f3ab2996c56fa0
amrbashir <amr.bashir2015@gmail.com>
2022-09-15 01:45:54 +0200

    feat: config schema generator

--- tooling/config-schema/build.rs
+++ tooling/config-schema/build.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 use std::{
-  env::current_dir,
+  path::PathBuf,
   error::Error,
   fs::File,
   io::{BufWriter, Write},
@@ -11,7 +11,7 @@ use std::{
 
 pub fn main() -> Result<(), Box<dyn Error>> {
   let schema = schemars::schema_for!(tauri_utils::config::Config);
-  let schema_file_path = current_dir()?.join("schema.json");
+  let schema_file_path = PathBuf::from( std::env::var("CARGO_MANIFEST_DIR")?).join("schema.json");
   let mut schema_file = BufWriter::new(File::create(&schema_file_path)?);
   write!(
     schema_file,

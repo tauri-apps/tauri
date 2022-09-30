f8dfda06de0a622a333ed459eb1e81c78db9eb1a
amrbashir <amr.bashir2015@gmail.com>
2022-09-30 15:20:08 +0200

    fmt

--- tooling/api/src/window.ts
+++ tooling/api/src/window.ts
@@ -1159,12 +1159,12 @@ class WindowManager extends WebviewWindowHandle {
             type: 'setMinSize',
             payload: size
               ? {
-                type: size.type,
-                data: {
-                  width: size.width,
-                  height: size.height
+                  type: size.type,
+                  data: {
+                    width: size.width,
+                    height: size.height
+                  }
                 }
-              }
               : null
           }
         }
@@ -1201,12 +1201,12 @@ class WindowManager extends WebviewWindowHandle {
             type: 'setMaxSize',
             payload: size
               ? {
-                type: size.type,
-                data: {
-                  width: size.width,
-                  height: size.height
+                  type: size.type,
+                  data: {
+                    width: size.width,
+                    height: size.height
+                  }
                 }
-              }
               : null
           }
         }

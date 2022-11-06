7af5edc6473e02fe3e6cf2c4d93b7804082e62c4
Orvar Segerström <orvarsegerstrom@gmail.com>
2022-11-06 21:28:39 +0100

    Fix error on events emitted in externally urled windows

--- core/tauri/src/manager.rs
+++ core/tauri/src/manager.rs
@@ -1408,7 +1408,7 @@ fn on_window_event<R: Runtime>(
       let windows = windows_map.values();
       for window in windows {
         window.eval(&format!(
-          r#"window.__TAURI_METADATA__.__windows = window.__TAURI_METADATA__.__windows.filter(w => w.label !== "{}");"#,
+          r#"var metadata = window.__TAURI_METADATA__; if (metadata != null) {{ metadata.__windows = window.__TAURI_METADATA__.__windows.filter(w => w.label !== "{}"); }}"#,
           label
         ))?;
       }

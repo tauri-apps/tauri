ce0dea53a33ce13c5ba70361fe72bf500d174493
amrbashir <amr.bashir2015@gmail.com>
2022-12-08 17:28:59 +0200

    strip unc on windows only

--- tooling/cli/src/build.rs
+++ tooling/cli/src/build.rs
@@ -373,10 +373,12 @@ fn print_signed_updater_archive(output_paths: &[PathBuf]) -> crate::Result<()> {
   let msg = format!("{} {} at:", output_paths.len(), pluralised);
   info!("{}", msg);
   for path in output_paths {
-    info!(
-      "        {}",
+    info!("        {}", {
+      #[cfg(unix)]
+      return path.display();
+      #[cfg(windows)]
       path.display().to_string().replacen(r"\\?\", "", 1)
-    );
+    });
   }
   Ok(())
 }

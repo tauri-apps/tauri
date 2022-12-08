9413ee66d3797e0daccf9c89fe17c6c0cc948c5e
amrbashir <amr.bashir2015@gmail.com>
2022-12-08 17:48:00 +0200

    fix cli build on unix

--- tooling/cli/src/build.rs
+++ tooling/cli/src/build.rs
@@ -373,12 +373,13 @@ fn print_signed_updater_archive(output_paths: &[PathBuf]) -> crate::Result<()> {
   let msg = format!("{} {} at:", output_paths.len(), pluralised);
   info!("{}", msg);
   for path in output_paths {
-    info!("        {}", {
-      #[cfg(unix)]
-      return path.display();
-      #[cfg(windows)]
+    #[cfg(unix)]
+    info!("        {}", path.display());
+    #[cfg(windows)]
+    info!(
+      "        {}",
       path.display().to_string().replacen(r"\\?\", "", 1)
-    });
+    );
   }
   Ok(())
 }

f5aaed7b72bec527278a57c4c51f559b4dd9c68a
Lucas Nogueira <lucas@tauri.studio>
2022-12-14 20:45:53 -0300

    add missing license header [skip ci]

--- tooling/bundler/src/bundle/windows/util.rs
+++ tooling/bundler/src/bundle/windows/util.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2022 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use std::{
   fs::{create_dir_all, File},
   io::{Cursor, Read, Write},

04391bda85e97f3fabab709806b3b2d096a1cae1
Lucas Nogueira <lucas@tauri.studio>
2022-12-15 00:09:12 -0300

    optimize test

--- core/tests/app-updater/tests/update.rs
+++ core/tests/app-updater/tests/update.rs
@@ -54,7 +54,13 @@ fn get_cli_bin_path(cli_dir: &Path, debug: bool) -> Option<PathBuf> {
   }
 }
 
-fn build_app(cli_bin_path: &Path, cwd: &Path, config: &Config, bundle_updater: bool) {
+fn build_app(
+  cli_bin_path: &Path,
+  cwd: &Path,
+  config: &Config,
+  bundle_updater: bool,
+  target: BundleTarget,
+) {
   let mut command = Command::new(&cli_bin_path);
   command
     .args(["build", "--debug", "--verbose"])
@@ -62,18 +68,22 @@ fn build_app(cli_bin_path: &Path, cwd: &Path, config: &Config, bundle_updater: b
     .arg(serde_json::to_string(config).unwrap())
     .current_dir(&cwd);
 
-  #[cfg(windows)]
-  command.args(["--bundles", "msi", "nsis"]);
   #[cfg(target_os = "linux")]
-  command.args(["--bundles", "appimage"]);
+  command.args(["--bundles", target.name()]);
   #[cfg(target_os = "macos")]
-  command.args(["--bundles", "app"]);
+  command.args(["--bundles", target.name()]);
 
   if bundle_updater {
+    #[cfg(windows)]
+    command.args(["--bundles", "msi", "nsis"]);
+
     command
       .env("TAURI_PRIVATE_KEY", UPDATER_PRIVATE_KEY)
       .env("TAURI_KEY_PASSWORD", "")
       .args(["--bundles", "updater"]);
+  } else {
+    #[cfg(windows)]
+    command.args(["--bundles", target.name()]);
   }
 
   let status = command
@@ -85,35 +95,81 @@ fn build_app(cli_bin_path: &Path, cwd: &Path, config: &Config, bundle_updater: b
   }
 }
 
+enum BundleTarget {
+  AppImage,
+
+  App,
+
+  Msi,
+  Nsis,
+}
+
+impl BundleTarget {
+  fn name(self) -> &'static str {
+    match self {
+      Self::AppImage => "appimage",
+      Self::App => "app",
+      Self::Msi => "msi",
+      Self::Nsis => "nsis",
+    }
+  }
+}
+
+impl Default for BundleTarget {
+  fn default() -> Self {
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    return Self::App;
+    #[cfg(target_os = "linux")]
+    return Self::App;
+    #[cfg(windows)]
+    return Self::Nsis;
+  }
+}
+
 #[cfg(target_os = "linux")]
-fn bundle_paths(root_dir: &Path, version: &str) -> Vec<PathBuf> {
-  root_dir.join(format!(
-    "target/debug/bundle/appimage/app-updater_{}_amd64.AppImage",
-    version
-  ))
+fn bundle_paths(root_dir: &Path, version: &str) -> Vec<(BundleTarget, PathBuf)> {
+  vec![(
+    BundleTarget::AppImage,
+    root_dir.join(format!(
+      "target/debug/bundle/appimage/app-updater_{}_amd64.AppImage",
+      version
+    )),
+  )]
 }
 
 #[cfg(target_os = "macos")]
-fn bundle_paths(root_dir: &Path, _version: &str) -> Vec<PathBuf> {
-  vec![root_dir.join(format!("target/debug/bundle/macos/app-updater.app"))]
+fn bundle_paths(root_dir: &Path, _version: &str) -> Vec<(BundleTarget, PathBuf)> {
+  vec![(
+    BundleTarget::App,
+    root_dir.join(format!("target/debug/bundle/macos/app-updater.app")),
+  )]
 }
 
 #[cfg(target_os = "ios")]
-fn bundle_paths(root_dir: &Path, _version: &str) -> Vec<PathBuf> {
-  vec![root_dir.join(format!("target/debug/bundle/ios/app-updater.app"))]
+fn bundle_paths(root_dir: &Path, _version: &str) -> Vec<(BundleTarget, PathBuf)> {
+  vec![(
+    BundleTarget::App,
+    root_dir.join(format!("target/debug/bundle/ios/app-updater.app")),
+  )]
 }
 
 #[cfg(windows)]
-fn bundle_paths(root_dir: &Path, version: &str) -> Vec<PathBuf> {
+fn bundle_paths(root_dir: &Path, version: &str) -> Vec<(BundleTarget, PathBuf)> {
   vec![
-    root_dir.join(format!(
-      "target/debug/bundle/nsis/app-updater_{}_x64-setup.exe",
-      version
-    )),
-    root_dir.join(format!(
-      "target/debug/bundle/msi/app-updater_{}_x64_en-US.msi",
-      version
-    )),
+    (
+      BundleTarget::Msi,
+      root_dir.join(format!(
+        "target/debug/bundle/msi/app-updater_{}_x64_en-US.msi",
+        version
+      )),
+    ),
+    (
+      BundleTarget::Nsis,
+      root_dir.join(format!(
+        "target/debug/bundle/nsis/app-updater_{}_x64-setup.exe",
+        version
+      )),
+    ),
   ]
 }
 
@@ -146,11 +202,17 @@ fn update_app() {
   };
 
   // bundle app update
-  build_app(&cli_bin_path, &manifest_dir, &config, true);
+  build_app(
+    &cli_bin_path,
+    &manifest_dir,
+    &config,
+    true,
+    Default::default(),
+  );
 
   let updater_zip_ext = if cfg!(windows) { "zip" } else { "tar.gz" };
 
-  for out_bundle_path in bundle_paths(&root_dir, "1.0.0") {
+  for (bundle_target, out_bundle_path) in bundle_paths(&root_dir, "1.0.0") {
     let bundle_updater_ext = out_bundle_path
       .extension()
       .unwrap()
@@ -225,7 +287,7 @@ fn update_app() {
     config.package.version = "0.1.0";
 
     // bundle initial app version
-    build_app(&cli_bin_path, &manifest_dir, &config, false);
+    build_app(&cli_bin_path, &manifest_dir, &config, false, bundle_target);
 
     let mut binary_cmd = if cfg!(windows) {
       Command::new(root_dir.join("target/debug/app-updater.exe"))
@@ -234,15 +296,16 @@ fn update_app() {
         bundle_paths(&root_dir, "0.1.0")
           .first()
           .unwrap()
+          .1
           .join("Contents/MacOS/app-updater"),
       )
     } else if std::env::var("CI").map(|v| v == "true").unwrap_or_default() {
       let mut c = Command::new("xvfb-run");
       c.arg("--auto-servernum")
-        .arg(bundle_paths(&root_dir, "0.1.0").first().unwrap());
+        .arg(&bundle_paths(&root_dir, "0.1.0").first().unwrap().1);
       c
     } else {
-      Command::new(bundle_paths(&root_dir, "0.1.0").first().unwrap())
+      Command::new(&bundle_paths(&root_dir, "0.1.0").first().unwrap().1)
     };
 
     let status = binary_cmd.status().expect("failed to run app");

b207f0b064bc67247e361c923797b7340d283d19
FabianLars <fabianlars@fabianlars.de>
2022-12-21 17:46:35 +0100

    clippy

--- tooling/bundler/src/bundle/windows/nsis.rs
+++ tooling/bundler/src/bundle/windows/nsis.rs
@@ -394,7 +394,7 @@ fn build_nsis_app_installer(
     .output_ok()
     .context("error running makensis.exe")?;
 
-  rename(&nsis_output_path, &nsis_installer_path)?;
+  rename(nsis_output_path, &nsis_installer_path)?;
 
   // Code signing is currently only supported on Windows hosts
   #[cfg(target_os = "windows")]

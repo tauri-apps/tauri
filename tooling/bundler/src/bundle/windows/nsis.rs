2fe1ad102468ef09cbb9ec7877a397651ef462fe
Fabian-Lars <fabianlars@fabianlars.de>
2023-01-04 16:00:14 +0100

    remove dbg macro [skip ci]

--- tooling/bundler/src/bundle/windows/nsis.rs
+++ tooling/bundler/src/bundle/windows/nsis.rs
@@ -187,7 +187,7 @@ fn build_nsis_app_installer(
   {
     let mut dir = dirs_next::cache_dir().unwrap();
     dir.extend(["tauri", "NSIS", "Plugins", "x86-unicode"]);
-    data.insert("additional_plugins_path", to_json(dbg!(dir)));
+    data.insert("additional_plugins_path", to_json(dir));
   }
 
   data.insert("arch", to_json(arch));

51607760ef35bf38aad84df970ac646c293004ca
Jonas Kruckenberg <iterpre@protonmail.com>
2023-01-14 11:09:23 +0100

    fix(cli): re-add example command in template

--- tooling/cli/templates/app/src-tauri/src/lib.rs
+++ tooling/cli/templates/app/src-tauri/src/lib.rs
@@ -1,3 +1,9 @@
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()

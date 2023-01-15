a2999cdc6c07a9128ab82c3e8b9a81a2585c09fa
Simon Hyll <hyllsimon@gmail.com>
2023-01-15 04:28:46 +0100

    fix: removed greet from template

--- tooling/cli/templates/app/src-tauri/src/lib.rs
+++ tooling/cli/templates/app/src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }

07eb7287ae671719ecd97e2889cec39e55c49b09
Lucas Nogueira <lucas@tauri.studio>
2023-02-18 16:54:29 -0300

    add header on missing files

--- examples/api/src-tauri/src/main.rs
+++ examples/api/src-tauri/src/main.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 

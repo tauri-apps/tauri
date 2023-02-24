34c8232f66f14e1e414429bd5d7defec4da6f533
Lucas Nogueira <lucas@tauri.studio>
2023-02-24 15:02:43 -0300

    fix cgrect usage

--- core/tauri/mobile/ios-api/Sources/Tauri/UiUtils.swift
+++ core/tauri/mobile/ios-api/Sources/Tauri/UiUtils.swift
@@ -7,7 +7,7 @@ import UIKit
 public class UIUtils {
     public static func centerPopover(rootViewController: UIViewController?, popoverController: UIViewController) {
         if let viewController = rootViewController {
-            popoverController.popoverPresentationController?.sourceRect = CGRectMake(viewController.view.center.x, viewController.view.center.y, 0, 0)
+            popoverController.popoverPresentationController?.sourceRect = CGRect(x: viewController.view.center.x, y: viewController.view.center.y, width: 0, height: 0)
             popoverController.popoverPresentationController?.sourceView = viewController.view
             popoverController.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.up
         }

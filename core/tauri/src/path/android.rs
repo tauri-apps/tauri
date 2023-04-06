a3b27b2d554541093a7f6b3345e11d514602ce54
Lucas Nogueira <lucas@tauri.app>
2023-04-06 10:53:52 -0300

    fix android conflict

--- core/tauri/src/path/android.rs
+++ core/tauri/src/path/android.rs
@@ -15,7 +15,7 @@ struct PathResponse {
 }
 
 impl<R: Runtime> PathResolver<R> {
-  fn resolve(&self, dir: &str) -> Result<PathBuf> {
+  fn call_resolve(&self, dir: &str) -> Result<PathBuf> {
     self
       .0
       .run_mobile_plugin::<PathResponse>(dir, ())
@@ -25,89 +25,91 @@ impl<R: Runtime> PathResolver<R> {
 
   /// Returns the path to the user's audio directory.
   pub fn audio_dir(&self) -> Result<PathBuf> {
-    self.resolve("getAudioDir")
+    self.call_resolve("getAudioDir")
   }
 
   /// Returns the path to the user's cache directory.
   pub fn cache_dir(&self) -> Result<PathBuf> {
-    self.resolve("getExternalCacheDir")
+    self.call_resolve("getExternalCacheDir")
   }
 
   /// Returns the path to the user's config directory.
   pub fn config_dir(&self) -> Result<PathBuf> {
-    self.resolve("getConfigDir")
+    self.call_resolve("getConfigDir")
   }
 
   /// Returns the path to the user's data directory.
   pub fn data_dir(&self) -> Result<PathBuf> {
-    self.resolve("getDataDir")
+    self.call_resolve("getDataDir")
   }
 
   /// Returns the path to the user's local data directory.
   pub fn local_data_dir(&self) -> Result<PathBuf> {
-    self.resolve("getDataDir")
+    self.call_resolve("getDataDir")
   }
 
   /// Returns the path to the user's document directory.
   pub fn document_dir(&self) -> Result<PathBuf> {
-    self.resolve("getDocumentDir")
+    self.call_resolve("getDocumentDir")
   }
 
   /// Returns the path to the user's download directory.
   pub fn download_dir(&self) -> Result<PathBuf> {
-    self.resolve("getDownloadDir")
+    self.call_resolve("getDownloadDir")
   }
 
   /// Returns the path to the user's picture directory.
   pub fn picture_dir(&self) -> Result<PathBuf> {
-    self.resolve("getPictureDir")
+    self.call_resolve("getPictureDir")
   }
 
   /// Returns the path to the user's public directory.
   pub fn public_dir(&self) -> Result<PathBuf> {
-    self.resolve("getPublicDir")
+    self.call_resolve("getPublicDir")
   }
 
   /// Returns the path to the user's video dir
   pub fn video_dir(&self) -> Result<PathBuf> {
-    self.resolve("getVideoDir")
+    self.call_resolve("getVideoDir")
   }
 
   /// Returns the path to the resource directory of this app.
   pub fn resource_dir(&self) -> Result<PathBuf> {
-    self.resolve("getResourcesDir")
+    self.call_resolve("getResourcesDir")
   }
 
   /// Returns the path to the suggested directory for your app's config files.
   ///
   /// Resolves to [`config_dir`]`/${bundle_identifier}`.
   pub fn app_config_dir(&self) -> Result<PathBuf> {
-    self.resolve("getConfigDir")
+    self.call_resolve("getConfigDir")
   }
 
   /// Returns the path to the suggested directory for your app's data files.
   ///
   /// Resolves to [`data_dir`]`/${bundle_identifier}`.
   pub fn app_data_dir(&self) -> Result<PathBuf> {
-    self.resolve("getDataDir")
+    self.call_resolve("getDataDir")
   }
 
   /// Returns the path to the suggested directory for your app's local data files.
   ///
   /// Resolves to [`local_data_dir`]`/${bundle_identifier}`.
   pub fn app_local_data_dir(&self) -> Result<PathBuf> {
-    self.resolve("getDataDir")
+    self.call_resolve("getDataDir")
   }
 
   /// Returns the path to the suggested directory for your app's cache files.
   ///
   /// Resolves to [`cache_dir`]`/${bundle_identifier}`.
   pub fn app_cache_dir(&self) -> Result<PathBuf> {
-    self.resolve("getCacheDir")
+    self.call_resolve("getCacheDir")
   }
 
   /// Returns the path to the suggested directory for your app's log files.
   pub fn app_log_dir(&self) -> Result<PathBuf> {
-    self.resolve("getConfigDir").map(|dir| dir.join("logs"))
+    self
+      .call_resolve("getConfigDir")
+      .map(|dir| dir.join("logs"))
   }
 }

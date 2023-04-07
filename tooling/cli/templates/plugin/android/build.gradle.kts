db4c9dc655e07ee2184fe04571f500f7910890cd
Lucas Fernandes Nogueira <lucas@tauri.studio>
2023-04-07 07:21:42 -0700

    feat(core): add option to configure Android's minimum SDK version (#6651)

--- tooling/cli/templates/plugin/android/build.gradle.kts
+++ tooling/cli/templates/plugin/android/build.gradle.kts
@@ -8,8 +8,8 @@ android {
     compileSdk = 32
 
     defaultConfig {
-        minSdk = 24
-        targetSdk = 32
+        minSdk = 21
+        targetSdk = 33
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")

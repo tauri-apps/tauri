8c45842e2863357a48304a396275d3e4084fdb42
Tomáš Diblík <dibla.tomas@post.cz>
2023-04-14 22:38:53 +0200

    chore(api): formatting
    Co-authored-by: Amr Bashir <amr.bashir2015@gmail.com>

--- tooling/api/src/window.ts
+++ tooling/api/src/window.ts
@@ -750,6 +750,7 @@ class WindowManager extends WebviewWindowHandle {
    * #### Platform-specific
    *
    * - **Linux / iOS / Android:** Unsupported.
+   *
    * @example
    * ```typescript
    * import { appWindow } from '@tauri-apps/api/window';

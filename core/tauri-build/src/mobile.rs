b8f47557f7a1c5351497452e9670c60c3a9d35f5
Lucas Nogueira <lucas@tauri.app>
2023-04-22 18:05:12 -0300

    tauri-mobile 0.4.0

--- core/tauri-build/src/mobile.rs
+++ core/tauri-build/src/mobile.rs
@@ -101,15 +101,19 @@ impl PluginBuilder {
 pub fn link_swift_library(name: &str, source: impl AsRef<Path>) {
   let source = source.as_ref();
 
-  let curr_dir = std::env::current_dir().unwrap();
-  std::env::set_current_dir(source).unwrap();
+  let sdk_root = std::env::var_os("SDKROOT");
+  std::env::remove_var("SDKROOT");
+
   swift_rs::SwiftLinker::new(
     &std::env::var("MACOSX_DEPLOYMENT_TARGET").unwrap_or_else(|_| "10.13".into()),
   )
   .with_ios(&std::env::var("IPHONEOS_DEPLOYMENT_TARGET").unwrap_or_else(|_| "13.0".into()))
   .with_package(name, source)
   .link();
-  std::env::set_current_dir(curr_dir).unwrap();
+
+  if let Some(root) = sdk_root {
+    std::env::set_var("SDKROOT", root);
+  }
 }
 
 #[doc(hidden)]

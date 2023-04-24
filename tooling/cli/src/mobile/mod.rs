tauri-apps/tauri

commit 2b22f649724816edf1f2745b33cbbea5507fb1cb

fix(ios): correctly use lib-name field

tooling/cli/src/mobile/mod.rs
@@ -275,12 +275,18 @@ fn get_app(config: &TauriConfig) -> App {
   )
   .expect("failed to load interface");
 
-  let app_name = interface.app_settings().app_name().unwrap_or(app_name);
-  let lib_name = interface.app_settings().lib_name();
+  let app_name = interface
+    .app_settings()
+    .app_name()
+    .unwrap_or(app_name.clone());
+  let lib_name = interface
+    .app_settings()
+    .lib_name()
+    .unwrap_or(app_name.clone());
 
   let raw = RawAppConfig {
     name: app_name,
-    lib_name,
+    lib_name: Some(lib_name),
     stylized_name: config.package.product_name.clone(),
     domain,
     asset_dir: None,

tooling/cli/templates/mobile/ios/project.yml
@@ -78,7 +78,7 @@ targets:
         ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES: true
       groups: [app]
     dependencies:
-      - framework: lib{{app.lib_name}}.a
+      - framework: lib{{app.lib-name}}.a
         embed: false
       {{~#each ios-libraries}}
       - framework: {{this}}
@@ -120,8 +120,8 @@ targets:
         name: Build Rust Code
         basedOnDependencyAnalysis: false
         outputFiles:
-          - $(SRCROOT)/target/aarch64-apple-ios/${CONFIGURATION}/deps/lib{{app.lib_name}}.a
-          - $(SRCROOT)/target/x86_64-apple-ios/${CONFIGURATION}/deps/lib{{app.lib_name}}.a
+          - $(SRCROOT)/target/aarch64-apple-ios/${CONFIGURATION}/deps/lib{{app.lib-name}}.a
+          - $(SRCROOT)/target/x86_64-apple-ios/${CONFIGURATION}/deps/lib{{app.lib-name}}.a
     {{~#if ios-post-compile-scripts}}
     postCompileScripts:
       {{~#each ios-post-compile-scripts}}{{#if this.path}}

commit 2424b33a0520f5a4e29e9e8cc6543da7955f8bf1

fix(cli): use snake_case fallback of app_name
fix regression introduced in https://github.com/tauri-apps/tauri/commit/2b22f649724816edf1f2745b33cbbea5507fb1cb

tooling/cli/src/mobile/mod.rs
@@ -12,6 +12,7 @@ use crate::{
   interface::{AppInterface, AppSettings, DevProcess, Interface, Options as InterfaceOptions},
 };
 use anyhow::{bail, Result};
+use heck::ToSnekCase;
 use jsonrpsee::core::client::{Client, ClientBuilder, ClientT};
 use jsonrpsee::server::{RpcModule, ServerBuilder, ServerHandle};
 use jsonrpsee_client_transport::ws::WsTransportClientBuilder;
@@ -281,7 +282,7 @@ fn get_app(config: &TauriConfig) -> App {
   let lib_name = interface
     .app_settings()
     .lib_name()
-    .unwrap_or(app_name.clone());
+    .unwrap_or_else(|| app_name.to_snek_case());
 
   let raw = RawAppConfig {
     name: app_name,
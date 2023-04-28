2a19f016d27dbc0b15ff8845d812b5dbe9fe8b81
Lucas Nogueira <lucas@tauri.app>
2023-04-28 10:44:25 -0300

    revert callback/error cleanup removal

--- core/tauri/scripts/core.js
+++ core/tauri/scripts/core.js
@@ -52,9 +52,11 @@
     return new Promise(function (resolve, reject) {
       var callback = window.__TAURI__.transformCallback(function (r) {
         resolve(r)
+        delete window[`_${error}`]
       }, true)
       var error = window.__TAURI__.transformCallback(function (e) {
         reject(e)
+        delete window[`_${callback}`]
       }, true)
 
       if (typeof cmd === 'string') {
--- tooling/api/src/tauri.ts
+++ tooling/api/src/tauri.ts
@@ -91,9 +91,11 @@ async function invoke<T>(cmd: string, args: InvokeArgs = {}): Promise<T> {
   return new Promise((resolve, reject) => {
     const callback = transformCallback((e: T) => {
       resolve(e)
+      Reflect.deleteProperty(window, `_${error}`)
     }, true)
     const error = transformCallback((e) => {
       reject(e)
+      Reflect.deleteProperty(window, `_${callback}`)
     }, true)
 
     window.__TAURI_IPC__({

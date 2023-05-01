414e4d02c7554ecadfe27317bee87389ce1e3370
Lucas Nogueira <lucas@tauri.app>
2023-05-01 20:17:48 -0300

    remove notification from example

--- examples/api/src-tauri/src/lib.rs
+++ examples/api/src-tauri/src/lib.rs
@@ -33,7 +33,6 @@ pub fn run() {
         .build(),
     )
     .plugin(tauri_plugin_sample::init())
-    .plugin(tauri_plugin_notification::init())
     .setup(move |app| {
       #[cfg(desktop)]
       {

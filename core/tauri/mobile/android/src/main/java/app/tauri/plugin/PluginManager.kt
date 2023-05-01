5492b18272ba37b67ac88f720acdfb039fd18cc1
Lucas Nogueira <lucas@tauri.app>
2023-05-01 12:54:27 -0300

    fix(android): adjust Android plugin command exception to use e.message

--- core/tauri/mobile/android/src/main/java/app/tauri/plugin/PluginManager.kt
+++ core/tauri/mobile/android/src/main/java/app/tauri/plugin/PluginManager.kt
@@ -119,7 +119,7 @@ class PluginManager(val activity: AppCompatActivity) {
         plugins[pluginId]?.invoke(invoke)
       }
     } catch (e: Exception) {
-      invoke.reject(e.toString())
+      invoke.reject(e.message)
     }
   }
 

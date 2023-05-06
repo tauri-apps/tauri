370e22e319784ff4ebe66b3f31636c10b7d7a5c7
Simon Hyll <hyllsimon@gmail.com>
2023-05-06 03:12:44 +0200

    fix: added license headers

--- tooling/cli/src/completions.rs
+++ tooling/cli/src/completions.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use crate::Result;
 use clap::{Command, Parser};
 use clap_complete::{generate, Shell};

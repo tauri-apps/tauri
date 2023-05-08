304f1fed444c85834a0f2c4eeca924bf0128aa2c
Chip Reed <chip@chip.sh>
2023-05-08 13:04:10 +0200

    use handlebars no_escape helper fn instead of closures

--- tooling/cli/src/plugin/init.rs
+++ tooling/cli/src/plugin/init.rs
@@ -90,7 +90,7 @@ pub fn command(mut options: Options) -> Result<()> {
 
     let _ = remove_dir_all(&template_target_path);
     let mut handlebars = Handlebars::new();
-    handlebars.register_escape_fn(|s| s.into());
+    handlebars.register_escape_fn(handlebars::no_escape);
 
     let mut data = BTreeMap::new();
     data.insert("plugin_name_original", to_json(&options.plugin_name));

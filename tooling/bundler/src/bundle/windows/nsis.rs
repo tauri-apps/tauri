b257bebf9e478d616690ba1c357dc7f071ea7b31
nathan-fall <39990940+nathan-fall@users.noreply.github.com>
2023-05-11 16:50:35 +0900

    feat(bundler): Add Japanese to supported NSIS languages, ref #6906 (#6940)

--- tooling/bundler/src/bundle/windows/nsis.rs
+++ tooling/bundler/src/bundle/windows/nsis.rs
@@ -514,6 +514,10 @@ fn get_lang_data(lang: &str) -> Option<(&'static str, &'static encoding_rs::Enco
       include_str!("./templates/nsis-languages/English.nsh"),
       UTF_8,
     )),
+    "japanese" => Some((
+      include_str!("./templates/nsis-languages/Japanese.nsh"),
+      UTF_8,
+    )),
     "portuguesebr" => Some((
       include_str!("./templates/nsis-languages/PortugueseBR.nsh"),
       UTF_8,

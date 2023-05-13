47f16b63dc93f0b75d99fa2e09f6e8cfbba7d2c7
Lucas Nogueira <lucas@tauri.app>
2023-05-13 00:50:14 -0300

    Revert "simplify allow_file impl"
    This reverts commit b8882f2fd021917abce9338018d64652c65f33f9.

--- core/tauri/src/scope/mod.rs
+++ core/tauri/src/scope/mod.rs
@@ -8,9 +8,26 @@ pub mod ipc;
 
 pub use self::ipc::Scope as IpcScope;
 pub use fs::{Event as FsScopeEvent, Pattern as GlobPattern, Scope as FsScope};
+use std::path::Path;
 
 pub(crate) struct Scopes {
   pub ipc: IpcScope,
   #[cfg(feature = "protocol-asset")]
   pub asset_protocol: FsScope,
+}
+
+impl Scopes {
+  #[allow(dead_code, unused)]
+  pub(crate) fn allow_directory(&self, path: &Path, recursive: bool) -> crate::Result<()> {
+    #[cfg(feature = "protocol-asset")]
+    self.asset_protocol.allow_directory(path, recursive)?;
+    Ok(())
+  }
+
+  #[allow(dead_code, unused)]
+  pub(crate) fn allow_file(&self, path: &Path) -> crate::Result<()> {
+    #[cfg(feature = "protocol-asset")]
+    self.asset_protocol.allow_file(path)?;
+    Ok(())
+  }
 }

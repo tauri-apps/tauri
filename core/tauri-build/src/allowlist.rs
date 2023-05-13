90b1db0f33ff65e5dc5cc3254326bda71fa72346
Lucas Nogueira <lucas@tauri.studio>
2023-05-13 14:38:11 -0300

    header

--- core/tauri-build/src/allowlist.rs
+++ core/tauri-build/src/allowlist.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use anyhow::{anyhow, Result};
 use cargo_toml::{Dependency, Manifest};
 use heck::AsKebabCase;

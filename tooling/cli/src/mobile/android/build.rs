tauri-apps/tauri

commit d03e47d141c3917520975be9081775dbc4e9d4fd

fix: only build specified rust targets for aab/apk build (#6625)
Co-authored-by: Lucas Nogueira <lucas@tauri.studio>

tooling/cli/src/mobile/android/build.rs
@@ -181,7 +181,7 @@ fn run_build(
       env,
       noise_level,
       profile,
-      get_targets_or_all(Vec::new())?,
+      get_targets(options.targets.clone().unwrap_or_default())?,
       options.split_per_abi,
     )?
   } else {
@@ -194,7 +194,7 @@ fn run_build(
       env,
       noise_level,
       profile,
-      get_targets_or_all(Vec::new())?,
+      get_targets(options.targets.unwrap_or_default())?,
       options.split_per_abi,
     )?
   } else {
@@ -207,28 +207,24 @@ fn run_build(
   Ok(())
 }
 
-fn get_targets_or_all<'a>(targets: Vec<String>) -> Result<Vec<&'a Target<'a>>> {
-  if targets.is_empty() {
-    Ok(Target::all().iter().map(|t| t.1).collect())
-  } else {
-    let mut outs = Vec::new();
+fn get_targets<'a>(targets: Vec<String>) -> Result<Vec<&'a Target<'a>>> {
+  let mut outs = Vec::new();
 
-    let possible_targets = Target::all()
-      .keys()
-      .map(|key| key.to_string())
-      .collect::<Vec<String>>()
-      .join(",");
+  let possible_targets = Target::all()
+    .keys()
+    .map(|key| key.to_string())
+    .collect::<Vec<String>>()
+    .join(",");
 
-    for t in targets {
-      let target = Target::for_name(&t).ok_or_else(|| {
-        anyhow::anyhow!(
-          "Target {} is invalid; the possible targets are {}",
-          t,
-          possible_targets
-        )
-      })?;
-      outs.push(target);
-    }
-    Ok(outs)
+  for t in targets {
+    let target = Target::for_name(&t).ok_or_else(|| {
+      anyhow::anyhow!(
+        "Target {} is invalid; the possible targets are {}",
+        t,
+        possible_targets
+      )
+    })?;
+    outs.push(target);
   }
+  Ok(outs)
 }

tooling/cli/src/build.rs
@@ -240,20 +240,15 @@ pub fn command(mut options: Options, verbosity: u8) -> Result<()> {
 }
 
 pub fn setup(options: &mut Options, mobile: bool) -> Result<AppInterface> {
-  let (merge_config, merge_config_path) = if let Some(config) = &options.config {
-    if config.starts_with('{') {
-      (Some(config.to_string()), None)
-    } else {
-      (
-        Some(
-          std::fs::read_to_string(config).with_context(|| "failed to read custom configuration")?,
-        ),
-        Some(config.clone()),
-      )
-    }
-  } else {
-    (None, None)
+  let (merge_config, merge_config_path) = match &options.config {
+    Some(config) if config.starts_with('{') => (Some(config.to_string()), None),
+    Some(config) => (
+      Some(std::fs::read_to_string(config).with_context(|| "failed to read custom configuration")?),
+      Some(config.clone()),
+    ),
+    None => (None, None),
   };
+
   options.config = merge_config;
 
   let tauri_path = tauri_dir();

tooling/cli/Cargo.toml
@@ -42,7 +42,7 @@ path = "src/main.rs"
 openssl-vendored = [ "tauri-mobile/openssl-vendored" ]
 
 [dependencies]
-tauri-mobile = { version = "0.2.5", default-features = false }
+tauri-mobile = { version = "0.3", default-features = false }
 textwrap = { version = "0.11.0", features = [ "term_size" ] }
 jsonrpsee = { version = "0.16", features = [ "server" ] }
 jsonrpsee-core = "0.16"

tooling/cli/src/mobile/mod.rs
@@ -316,6 +316,7 @@ fn ensure_init(project_dir: PathBuf, target: Target) -> Result<()> {
       target.command_name(),
     )
   } else {
+    #[allow(irrefutable_let_patterns)]
     if let Target::Android = target {
       create_dir_all(project_dir.join(".tauri").join("plugins"))?;
     }

tooling/cli/templates/mobile/android/app/build.gradle.kts
@@ -30,7 +30,6 @@ android {
             isMinifyEnabled = false
             packagingOptions {
                 {{~#each targets}}
-
                 jniLibs.keepDebugSymbols.add("*/{{this.abi}}/*.so")
                 {{/each}}
             }
@@ -46,17 +45,14 @@ android {
     flavorDimensions.add("abi")
     productFlavors {
         create("universal") {
-            val abiList = findProperty("abiList") as? String
-
             dimension = "abi"
             ndk {
-                abiFilters += abiList?.split(",")?.map { it.trim() } ?: listOf(
+                abiFilters += (findProperty("abiList") as? String)?.split(",") ?: listOf(
                     {{~#each targets}}
                     "{{this.abi}}",{{/each}}
                 )
             }
         }
-
         {{~#each targets}}
 
         create("{{this.arch}}") {
@@ -74,8 +70,8 @@ android {
 
 rust {
     rootDirRel = "{{root-dir-rel}}"
-    targets = listOf({{quote-and-join target-names}})
-    arches = listOf({{quote-and-join arches}})
+    targets = (findProperty("targetList") as? String)?.split(",") ?: listOf({{quote-and-join target-names}})
+    arches = (findProperty("archList") as? String)?.split(",") ?: listOf({{quote-and-join arches}})
 }
 
 dependencies {
@@ -98,9 +94,11 @@ afterEvaluate {
     android.applicationVariants.all {
         tasks["mergeUniversalReleaseJniLibFolders"].dependsOn(tasks["rustBuildRelease"])
         tasks["mergeUniversalDebugJniLibFolders"].dependsOn(tasks["rustBuildDebug"])
-        productFlavors.filter{ it.name != "universal" }.forEach { _ ->
-            val archAndBuildType = name.capitalize()
-            tasks["merge${archAndBuildType}JniLibFolders"].dependsOn(tasks["rustBuild${archAndBuildType}"])
+        if (findProperty("targetList") == null) {
+            productFlavors.filter{ it.name != "universal" }.forEach { _ ->
+                val archAndBuildType = name.capitalize()
+                tasks["merge${archAndBuildType}JniLibFolders"].dependsOn(tasks["rustBuild${archAndBuildType}"])
+            }
         }
     }
 }

commit f1c4138aca7ec0596b1ecbb73f47620e45aa9256

fix(cli/android): fallback to all targets
fix regression introduced in https://github.com/tauri-apps/tauri/commit/d03e47d141c3917520975be9081775dbc4e9d4fd

tooling/cli/src/mobile/android/build.rs
@@ -186,7 +186,7 @@ fn run_build(
       env,
       noise_level,
       profile,
-      get_targets(options.targets.clone().unwrap_or_default())?,
+      get_targets_or_all(options.targets.clone().unwrap_or_default())?,
       options.split_per_abi,
     )?
   } else {
@@ -199,7 +199,7 @@ fn run_build(
       env,
       noise_level,
       profile,
-      get_targets(options.targets.unwrap_or_default())?,
+      get_targets_or_all(options.targets.unwrap_or_default())?,
       options.split_per_abi,
     )?
   } else {
@@ -212,24 +212,28 @@ fn run_build(
   Ok(())
 }
 
-fn get_targets<'a>(targets: Vec<String>) -> Result<Vec<&'a Target<'a>>> {
-  let mut outs = Vec::new();
+fn get_targets_or_all<'a>(targets: Vec<String>) -> Result<Vec<&'a Target<'a>>> {
+  if targets.is_empty() {
+    Ok(Target::all().iter().map(|t| t.1).collect())
+  } else {
+    let mut outs = Vec::new();
 
-  let possible_targets = Target::all()
-    .keys()
-    .map(|key| key.to_string())
-    .collect::<Vec<String>>()
-    .join(",");
+    let possible_targets = Target::all()
+      .keys()
+      .map(|key| key.to_string())
+      .collect::<Vec<String>>()
+      .join(",");
 
-  for t in targets {
-    let target = Target::for_name(&t).ok_or_else(|| {
-      anyhow::anyhow!(
-        "Target {} is invalid; the possible targets are {}",
-        t,
-        possible_targets
-      )
-    })?;
-    outs.push(target);
+    for t in targets {
+      let target = Target::for_name(&t).ok_or_else(|| {
+        anyhow::anyhow!(
+          "Target {} is invalid; the possible targets are {}",
+          t,
+          possible_targets
+        )
+      })?;
+      outs.push(target);
+    }
+    Ok(outs)
   }
-  Ok(outs)
 }
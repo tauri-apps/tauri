74e84b5260107e091fd74447e35e9ac9d7ccb9fa
Lucas Nogueira <lucas@tauri.studio>
2023-05-25 23:39:22 -0300

    feat(cli/mobile/init): add `--skip-targets-install`, ref #7044 #7058

---
+++ .changes/skip-target-install-arg.md
@@ -0,0 +1,6 @@
+---
+'cli.rs': 'patch:enhance'
+'cli.js': 'patch:enhance'
+---
+
+Add `--skip-targets-install` flag for `tauri android init` and `tauri ios init` to skip installing needed rust targets vie rustup.
--- tooling/cli/src/mobile/init.rs
+++ tooling/cli/src/mobile/init.rs
@@ -24,13 +24,19 @@ use std::{
   path::PathBuf,
 };
 
-pub fn command(target: Target, ci: bool, reinstall_deps: bool) -> Result<()> {
+pub fn command(
+  target: Target,
+  ci: bool,
+  reinstall_deps: bool,
+  skip_targets_install: bool,
+) -> Result<()> {
   let wrapper = TextWrapper::with_splitter(textwrap::termwidth(), textwrap::NoHyphenation);
   exec(
     target,
     &wrapper,
     ci || var_os("CI").is_some(),
     reinstall_deps,
+    skip_targets_install,
   )
   .map_err(|e| anyhow::anyhow!("{:#}", e))?;
   Ok(())
@@ -78,6 +84,7 @@ pub fn exec(
   wrapper: &TextWrapper,
   #[allow(unused_variables)] non_interactive: bool,
   #[allow(unused_variables)] reinstall_deps: bool,
+  skip_targets_install: bool,
 ) -> Result<App> {
   let tauri_config = get_tauri_config(None)?;
   let tauri_config_guard = tauri_config.lock().unwrap();
@@ -154,7 +161,13 @@ pub fn exec(
         let (app, config, metadata) =
           super::android::get_config(Some(app), tauri_config_, &Default::default());
         map.insert("android", &config);
-        super::android::project::gen(&config, &metadata, (handlebars, map), wrapper)?;
+        super::android::project::gen(
+          &config,
+          &metadata,
+          (handlebars, map),
+          wrapper,
+          skip_targets_install,
+        )?;
         app
       }
       Err(err) => {
@@ -183,6 +196,7 @@ pub fn exec(
         wrapper,
         non_interactive,
         reinstall_deps,
+        skip_targets_install,
       )?;
       app
     }

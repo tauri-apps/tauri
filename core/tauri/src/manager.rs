ca90c801404e504c3cad0b1b9af0c828f3a2820e
amrbashir <amr.bashir2015@gmail.com>
2023-06-05 15:02:30 +0300

    use `null` origin for `data:`

--- core/tauri/src/manager.rs
+++ core/tauri/src/manager.rs
@@ -471,20 +471,21 @@ impl<R: Runtime> WindowManager<R> {
     }
 
     let window_url = Url::parse(&pending.url).unwrap();
-    let window_origin =
-      if cfg!(windows) && window_url.scheme() != "http" && window_url.scheme() != "https" {
-        format!("https://{}.localhost", window_url.scheme())
-      } else {
-        format!(
-          "{}://{}{}",
-          window_url.scheme(),
-          window_url.host().map(|h| h.to_string()).unwrap_or_default(),
-          window_url
-            .port()
-            .map(|p| format!(":{p}"))
-            .unwrap_or_default()
-        )
-      };
+    let window_origin = if window_url.scheme() == "data" {
+      "null".into()
+    } else if cfg!(windows) && window_url.scheme() != "http" && window_url.scheme() != "https" {
+      format!("https://{}.localhost", window_url.scheme())
+    } else {
+      format!(
+        "{}://{}{}",
+        window_url.scheme(),
+        window_url.host().unwrap(),
+        window_url
+          .port()
+          .map(|p| format!(":{p}"))
+          .unwrap_or_default()
+      )
+    };
 
     if !registered_scheme_protocols.contains(&"tauri".into()) {
       let web_resource_request_handler = pending.web_resource_request_handler.take();

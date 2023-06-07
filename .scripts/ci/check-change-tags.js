66c4a68f773c08b8aed4e72547e105c202170f2a
Lucas Nogueira <lucas@tauri.studio>
2023-06-07 10:21:23 -0300

    only check changed files

--- .scripts/ci/check-change-tags.js
+++ .scripts/ci/check-change-tags.js
@@ -22,53 +22,58 @@ const tags = Object.keys(covectorConfig.changeTags)
 const missingTagsFiles = {}
 const unknownTagsFiles = {}
 
-const changeFiles = fs
-  .readdirSync('.changes')
-  .filter((f) => f.endsWith('.md') && f.toLowerCase() !== 'readme.md')
-for (const file of changeFiles) {
-  const content = fs.readFileSync(path.join('.changes', file), 'utf8')
-  const [frontMatter] = /^---[\s\S.]*---\n/i.exec(content)
-  const packages = frontMatter
-    .split('\n')
-    .filter((l) => !(l === '---' || !l))
-    .map((l) => l.replace(/('|")/g, '').split(':'))
+function checkChangeFiles(changeFiles) {
+  for (const file of changeFiles) {
+    const content = fs.readFileSync(file, 'utf8')
+    const [frontMatter] = /^---[\s\S.]*---\n/i.exec(content)
+    const packages = frontMatter
+      .split('\n')
+      .filter((l) => !(l === '---' || !l))
+      .map((l) => l.replace(/('|")/g, '').split(':'))
 
-  for (const [package, _, tag] of packages) {
-    if (!tag) {
-      if (ignorePackages.includes(package)) continue
+    for (const [package, _, tag] of packages) {
+      if (!tag) {
+        if (ignorePackages.includes(package)) continue
 
-      if (!missingTagsFiles[file]) missingTagsFiles[file] = []
-      missingTagsFiles[file].push(package)
-    } else if (!tags.includes(tag)) {
-      if (!unknownTagsFiles[file]) unknownTagsFiles[file] = []
-      unknownTagsFiles[file].push({ package, tag })
+        if (!missingTagsFiles[file]) missingTagsFiles[file] = []
+        missingTagsFiles[file].push(package)
+      } else if (!tags.includes(tag)) {
+        if (!unknownTagsFiles[file]) unknownTagsFiles[file] = []
+        unknownTagsFiles[file].push({ package, tag })
+      }
     }
+  }
+  const missingTagsEntries = Object.entries(missingTagsFiles)
+  const unknownTagsEntries = Object.entries(unknownTagsFiles)
+  if (missingTagsEntries.length > 0 || unknownTagsEntries.length > 0) {
+    for (const [file, packages] of missingTagsEntries) {
+      for (const package of packages) {
+        console.error(
+          `Package \`${package}\` is missing a change tag in ${file} `
+        )
+      }
+    }
+
+    for (const [file, packages] of unknownTagsEntries) {
+      for (const { package, tag } of packages) {
+        console.error(
+          `Package \`${package}\` has an uknown change tag ${tag} in ${file} `
+        )
+      }
+    }
+
+    process.exit(1)
   }
 }
-const missingTagsEntries = Object.entries(missingTagsFiles)
-const unknownTagsEntries = Object.entries(unknownTagsFiles)
-if (missingTagsEntries.length > 0 || unknownTagsEntries.length > 0) {
-  for (const [file, packages] of missingTagsEntries) {
-    for (const package of packages) {
-      console.error(
-        `Package \`${package}\` is missing a change tag in ${path.join(
-          '.changes',
-          file
-        )} `
-      )
-    }
-  }
 
-  for (const [file, packages] of unknownTagsEntries) {
-    for (const { package, tag } of packages) {
-      console.error(
-        `Package \`${package}\` has an uknown change tag ${tag} in ${path.join(
-          '.changes',
-          file
-        )} `
-      )
-    }
-  }
+const [_bin, _script, ...files] = process.argv
 
-  process.exit(1)
+if (files.length > 0) {
+  checkChangeFiles(files.filter((f) => f.toLowerCase() !== 'readme.md'))
+} else {
+  const changeFiles = fs
+    .readdirSync('.changes')
+    .filter((f) => f.endsWith('.md') && f.toLowerCase() !== 'readme.md')
+    .map((p) => path.join('.changes', p))
+  checkChangeFiles(changeFiles)
 }

675bdaaa3e8ef5bf9e0751e3449b130ab9dc6bfc
Lucas Nogueira <lucas@tauri.studio>
2023-06-12 14:51:29 -0300

    rename feature to linux-ipc-protocol

--- core/tauri/build.rs
+++ core/tauri/build.rs
@@ -52,7 +52,7 @@ fn main() {
 
   alias(
     "ipc_custom_protocol",
-    target_os != "linux" || has_feature("linux-protocol-body"),
+    target_os != "linux" || has_feature("linux-ipc-protocol"),
   );
 
   let checked_features_out_path = Path::new(&var("OUT_DIR").unwrap()).join("checked_features");

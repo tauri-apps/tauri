41402b1ee3350495fd9493b9cf9894add22fe85a
Simon Hyll <hyllsimon@gmail.com>
2023-06-19 15:06:45 +0200

    fix: added links

--- tooling/webdriver/src/main.rs
+++ tooling/webdriver/src/main.rs
@@ -6,7 +6,7 @@
 //!
 //! Cross-platform WebDriver server for Tauri applications.
 //!
-//! This is a [WebDriver Intermediary Node] that wraps the native WebDriver server for platforms that [Tauri] supports. Your WebDriver client will connect to the running `tauri-driver` server, and `tauri-driver` will handle starting the native WebDriver server for you behind the scenes. It requires two separate ports to be used since two distinct [WebDriver Remote Ends] run.
+//! This is a [WebDriver Intermediary Node](https://www.w3.org/TR/webdriver/#dfn-intermediary-nodes) that wraps the native WebDriver server for platforms that [Tauri](https://github.com/tauri-apps/tauri) supports. Your WebDriver client will connect to the running `tauri-driver` server, and `tauri-driver` will handle starting the native WebDriver server for you behind the scenes. It requires two separate ports to be used since two distinct [WebDriver Remote Ends](https://www.w3.org/TR/webdriver/#dfn-remote-ends) run.
 
 #![doc(
   html_logo_url = "https://github.com/tauri-apps/tauri/raw/dev/app-icon.png",

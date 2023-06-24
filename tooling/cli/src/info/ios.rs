5eb85543313eaf7ca2289898652b1b11dc776608
i-c-b <133848861+i-c-b@users.noreply.github.com>
2023-06-24 06:30:34 -0500

    fix(cli): Apple Dev Teams format for info::Section::Display (#7282)
    Co-authored-by: Lucas Fernandes Nogueira <lucas@tauri.app>

--- tooling/cli/src/info/ios.rs
+++ tooling/cli/src/info/ios.rs
@@ -12,13 +12,16 @@ pub fn items() -> Vec<SectionItem> {
       let teams = tauri_mobile::apple::teams::find_development_teams().unwrap_or_default();
       Some((
         if teams.is_empty() {
-          "None".red().to_string()
+          "Developer Teams: None".red().to_string()
         } else {
-          teams
-            .iter()
-            .map(|t| format!("{} (ID: {})", t.name, t.id))
-            .collect::<Vec<String>>()
-            .join(", ")
+          format!(
+            "Developer Teams: {}",
+            teams
+              .iter()
+              .map(|t| format!("{} (ID: {})", t.name, t.id))
+              .collect::<Vec<String>>()
+              .join(", ")
+          )
         },
         Status::Neutral,
       ))

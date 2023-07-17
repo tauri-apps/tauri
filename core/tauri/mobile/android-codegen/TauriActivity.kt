a5752db9852bb852e61f19dfb48a9435c1fdc79c
Fabian-Lars <fabianlars@fabianlars.de>
2023-07-17 15:20:16 +0200

    feat(core): forward `onNewIntent` event to android plugins (#7436)

--- core/tauri/mobile/android-codegen/TauriActivity.kt
+++ core/tauri/mobile/android-codegen/TauriActivity.kt
@@ -7,6 +7,7 @@
 package {{package}}
 
 import android.os.Bundle
+import android.content.Intent
 import app.tauri.plugin.PluginManager
 
 abstract class TauriActivity : WryActivity() {
@@ -17,5 +18,10 @@ abstract class TauriActivity : WryActivity() {
     if (intent != null) {
       pluginManager.onNewIntent(intent)
     }
+  }
+
+  override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+    pluginManager.onNewIntent(intent)
   }
 }

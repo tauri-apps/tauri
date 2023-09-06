dbaf98da2c8ab5b91801379fbbd42230a1735fde
Lucas Nogueira <lucas@tauri.studio>
2023-09-06 15:35:13 -0300

    license header

--- core/tauri/src/protocol/isolation.rs
+++ core/tauri/src/protocol/isolation.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use http::header::CONTENT_TYPE;
 use serialize_to_javascript::Template;
 use tauri_utils::assets::{Assets, EmbeddedAssets};

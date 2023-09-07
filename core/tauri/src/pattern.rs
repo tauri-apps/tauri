c3aa0086ab3e9065c038410c7c2c95877f2b2405
Lucas Nogueira <lucas@tauri.studio>
2023-09-07 16:44:07 -0300

    fix format_real_schema

--- core/tauri/src/pattern.rs
+++ core/tauri/src/pattern.rs
@@ -110,7 +110,7 @@ pub(crate) struct PatternJavascript {
 #[allow(dead_code)]
 pub(crate) fn format_real_schema(schema: &str) -> String {
   if cfg!(windows) || cfg!(target_os = "android") {
-    format!("https://{schema}.{ISOLATION_IFRAME_SRC_DOMAIN}")
+    format!("http://{schema}.{ISOLATION_IFRAME_SRC_DOMAIN}")
   } else {
     format!("{schema}://{ISOLATION_IFRAME_SRC_DOMAIN}")
   }

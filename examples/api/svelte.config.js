27bf9e482a86f48829b5d7adad96faf44de24c98
amrbashir <amr.bashir2015@gmail.com>
2023-10-03 15:20:56 +0300

    license header

--- examples/api/svelte.config.js
+++ examples/api/svelte.config.js
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {

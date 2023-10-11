148efbde5c12bbd539028a7d2f1e21c903077339
Lucas Nogueira <lucas@tauri.app>
2023-10-11 12:46:05 -0300

    license headers

--- .scripts/covector/parse-output.js
+++ .scripts/covector/parse-output.js
@@ -1,3 +1,9 @@
+#!/usr/bin/env node
+
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 const json = process.argv[2]
 const field = process.argv[3]
 

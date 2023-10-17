af0e50bbd1ef500c7526f3266247a9a9e7559677
Lucas Nogueira <lucas@tauri.studio>
2023-10-17 14:44:00 -0300

    expand api example

--- examples/api/src/views/Welcome.svelte
+++ examples/api/src/views/Welcome.svelte
@@ -1,5 +1,20 @@
 <script>
   import { invoke } from '@tauri-apps/api/primitives'
+  import { getName, getVersion, getTauriVersion } from '@tauri-apps/api/app'
+
+  let version = '1.0.0'
+  let tauriVersion = '1.0.0'
+  let appName = 'Unknown'
+
+  getName().then((n) => {
+    appName = n
+  })
+  getVersion().then((v) => {
+    version = v
+  })
+  getTauriVersion().then((v) => {
+    tauriVersion = v
+  })
 
   function contextMenu() {
     invoke('popup_context_menu')
@@ -14,6 +29,14 @@
     development process. In the future, this app will be used on Tauri's integration
     tests.
   </p>
+  <br />
+  <br />
+  <pre>
+    App name: <code>{appName}</code>
+    App version: <code>{version}</code>
+    Tauri version: <code>{tauriVersion}</code>
+  </pre>
+  <br />
 
   <button class="btn" on:click={contextMenu}>Context menu</button>
 </div>

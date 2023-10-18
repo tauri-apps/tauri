bc516abfeb18bf08fa0f005bb0203379803fcd1b
Lucas Nogueira <lucas@tauri.studio>
2023-10-18 09:33:53 -0300

    fix(cli): duplicated newlines on child process output

---
+++ .changes/fix-cmd-output.md
@@ -0,0 +1,7 @@
+---
+"@tauri-apps/cli": patch:bug
+"tauri-cli": patch:bug
+"tauri-bundler": patch:bug
+---
+
+Fixes duplicated newlines on command outputs.
--- tooling/bundler/src/bundle/common.rs
+++ tooling/bundler/src/bundle/common.rs
@@ -172,6 +172,9 @@ impl CommandExt for Command {
         if let Ok(0) = tauri_utils::io::read_line(&mut stdout, &mut buf) {
           break;
         }
+        if buf.ends_with(&[b'\n']) {
+          buf.pop();
+        }
         debug!(action = "stdout"; "{}", String::from_utf8_lossy(&buf));
         lines.extend(buf.clone());
         lines.push(b'\n');
@@ -188,6 +191,9 @@ impl CommandExt for Command {
         buf.clear();
         if let Ok(0) = tauri_utils::io::read_line(&mut stderr, &mut buf) {
           break;
+        }
+        if buf.ends_with(&[b'\n']) {
+          buf.pop();
         }
         debug!(action = "stderr"; "{}", String::from_utf8_lossy(&buf));
         lines.extend(buf.clone());

4f92ec4b2812922b6620a6327b91f254701b2dca
amrbashir <amr.bashir2015@gmail.com>
2023-11-15 02:59:46 +0200

    lciense headers and fix lint

--- tooling/api/src/menu/base.ts
+++ tooling/api/src/menu/base.ts
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 import { Resource } from '../internal'
 import { Channel, invoke } from '../primitives'
 import { CheckMenuItem } from './checkMenuItem'

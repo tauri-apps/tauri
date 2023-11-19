46b7012490b9f4f919eef0e5b16c04d8af647915
Lucas Nogueira <lucas@tauri.studio>
2023-11-19 10:45:56 -0300

    use as_ref

--- core/tauri/src/menu/submenu.rs
+++ core/tauri/src/menu/submenu.rs
@@ -85,10 +85,9 @@ impl<R: Runtime> ContextMenuBase for Submenu<R> {
         target_os = "openbsd"
       ))]
       if let Ok(w) = window.gtk_window() {
-        use gtk::prelude::Cast;
         self_
           .inner()
-          .show_context_menu_for_gtk_window(&w.upcast(), position);
+          .show_context_menu_for_gtk_window(w.as_ref(), position);
       }
 
       #[cfg(windows)]

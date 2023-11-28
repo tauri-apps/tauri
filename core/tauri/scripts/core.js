73401153e8441dfae4b1022b8108304c171df1db
amrbashir <amr.bashir2015@gmail.com>
2023-11-29 01:28:27 +0200

    cancel if mouse moves

--- core/tauri/scripts/core.js
+++ core/tauri/scripts/core.js
@@ -144,10 +144,13 @@
 
   //-----------------------//
   // data-tauri-drag-region
+  //
+  // drag on mousedown and maximize on double click on Windows and Linux
+  // while macOS macos maximization should be on mouseup and if the mouse
+  // moves after the double click, it should be cancelled
   //-----------------------//
   const TAURI_DRAG_REGION_ATTR = 'data-tauri-drag-region';
-  //drag on mousedown and maximize on double click on Windows and Linux
-  // while macOS macos maximization is on mouseup below
+  let x = 0, y = 0;
   document.addEventListener('mousedown', (e) => {
     if (
       // element has the magic data attribute
@@ -155,15 +158,24 @@
       // and was left mouse button
       e.button === 0 &&
       // and was normal click to drag or double click to maximize
-      (e.detail === 1 || e.detail === 2) &&
-      // and was on Windows or Linux or was on macOS and we drag only (macos maximization is on mouseup below)
-      (osName !== 'macos' || (osName === 'macos' && e.detail === 1))
-    ) {
+      (e.detail === 1 || e.detail === 2)
+      ) {
+
+      // macOS maximization happens on `mouseup`,
+      // so we save needed state and early return
+      if (osName === 'macos' && e.detail == 2) {
+        x = e.clientX
+        y = e.clientY
+        return
+      }
+
       // prevents text cursor
       e.preventDefault()
+
       // fix #2549: double click on drag region edge causes content to maximize without window sizing change
       // https://github.com/tauri-apps/tauri/issues/2549#issuecomment-1250036908
       e.stopImmediatePropagation()
+
       window.__TAURI_INVOKE__('tauri', {
         __tauriModule: 'Window',
         message: {
@@ -187,7 +199,9 @@
         // and was left mouse button
         e.button === 0 &&
         // and was double click
-        e.detail === 2
+        e.detail === 2 &&
+        // and the cursor hasn't moved from initial mousedown
+        e.clientX === x && e.clientY === y
       ) {
         window.__TAURI_INVOKE__('tauri', {
           __tauriModule: 'Window',

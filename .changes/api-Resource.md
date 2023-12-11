c825a3ad51935d1a0bdb3d356cb281d30710c53d
amrbashir <amr.bashir2015@gmail.com>
2023-12-11 22:35:34 +0200

    feat(api): expose `Resource` class
    continuation of https://github.com/tauri-apps/tauri/pull/8276

---
+++ .changes/api-Resource.md
@@ -0,0 +1,5 @@
+---
+'@tauri-apps/api': 'patch:feat'
+---
+
+Exposed `Resource` class which can be extended Rust-backed resources created through `tauri::Manager::resources_table`.
--- tooling/api/src/core.ts
+++ tooling/api/src/core.ts
@@ -160,6 +160,35 @@ function convertFileSrc(filePath: string, protocol = 'asset'): string {
   return window.__TAURI_INTERNALS__.convertFileSrc(filePath, protocol)
 }
 
+/**
+ * A rust-backed resource stored through `tauri::Manager::resources_table` API.
+ *
+ * The resource lives in the main process and does not exist
+ * in the Javascript world, and thus will not be cleaned up automatiacally
+ * except on application exit. If you want to clean it up early, call {@linkcode Resource.close}
+ */
+export class Resource {
+  readonly #rid: number
+
+  get rid(): number {
+    return this.#rid
+  }
+
+  constructor(rid: number) {
+    this.#rid = rid
+  }
+
+  /**
+   * Destroys and cleans up this resource from memory.
+   * **You should not call any method on this object anymore and should drop any reference to it.**
+   */
+  async close(): Promise<void> {
+    return invoke('plugin:resources|close', {
+      rid: this.rid
+    })
+  }
+}
+
 export type { InvokeArgs, InvokeOptions }
 
 export {

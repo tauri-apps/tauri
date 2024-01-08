bad126cca811b16539cc402d1fbc22738967c062
Lucas Nogueira <lucas@tauri.app>
2024-01-08 15:30:36 -0300

    license header

--- core/tauri/src/webview/webview_window.rs
+++ core/tauri/src/webview/webview_window.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 //! [`Window`] that hosts a single [`Webview`].
 
 use std::{borrow::Cow, path::PathBuf, sync::Arc};

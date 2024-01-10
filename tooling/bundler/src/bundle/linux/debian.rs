784a337f4bf53c92f9ed019bfe71e45f41bf2c11
Naman Garg <naman@crabnebula.dev>
2024-01-10 14:19:44 +0530

    Set correct permission for binaries

--- tooling/bundler/src/bundle/linux/debian.rs
+++ tooling/bundler/src/bundle/linux/debian.rs
@@ -40,6 +40,7 @@ use std::{
   fs::{self, read_to_string, File},
   io::{self, Write},
   path::{Path, PathBuf},
+  os::unix::fs::MetadataExt,
 };
 
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
@@ -369,19 +370,28 @@ fn create_tar_from_dir<P: AsRef<Path>, W: Write>(src_dir: P, dest_file: W) -> cr
     if entry.file_type().is_dir() {
       let stat = fs::metadata(src_path)?;
       let mut header = tar::Header::new_gnu();
-      header.set_mode(0o755);
       header.set_metadata(&stat);
       header.set_uid(0);
       header.set_gid(0);
+      header.set_mode(0o755);
       tar_builder.append_data(&mut header, dest_path, &mut io::empty())?;
     } else {
       let mut src_file = fs::File::open(src_path)?;
       let stat = src_file.metadata()?;
       let mut header = tar::Header::new_gnu();
-      header.set_mode(0o644);
       header.set_metadata(&stat);
       header.set_uid(0);
       header.set_gid(0);
+
+      // It extracts the MSB in octal permissions.
+      let user_permission = (stat.mode()%512)/64;
+      // Check if file is executable, and set permissions accordingly.
+      if user_permission%2 == 1{
+        header.set_mode(0o755);
+      } else {
+        header.set_mode(0o644);
+      }
+
       tar_builder.append_data(&mut header, dest_path, &mut src_file)?;
     }
   }

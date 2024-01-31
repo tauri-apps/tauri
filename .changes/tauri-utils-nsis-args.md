fcafd7dcc1f0650ae8a792a2a01bcc6236bbf6e6
Lucas Nogueira <lucas@tauri.studio>
2024-01-31 15:23:59 -0300

    move /args

--- .changes/tauri-utils-nsis-args.md
+++
@@ -1,5 +0,0 @@
----
-'tauri-utils': 'minor:enhance'
----
-
-Changed `WindowsUpdateInstallMode::nsis_args` return to include `/Args` flag at the end so it can be used to pass arguments when launching the app after the updater is done.
--- core/tauri-utils/src/config.rs
+++ core/tauri-utils/src/config.rs
@@ -2587,12 +2587,12 @@ impl WindowsUpdateInstallMode {
 
   /// Returns the associated nsis arguments.
   ///
-  /// [WindowsUpdateInstallMode::Passive] will return `["/P", "/R", "/ARGS"]`
-  /// [WindowsUpdateInstallMode::Quiet] will return `["/S", "/R", "/ARGS"]`
+  /// [WindowsUpdateInstallMode::Passive] will return `["/P", "/R"]`
+  /// [WindowsUpdateInstallMode::Quiet] will return `["/S", "/R"]`
   pub fn nsis_args(&self) -> &'static [&'static str] {
     match self {
-      Self::Passive => &["/P", "/R", "/ARGS"],
-      Self::Quiet => &["/S", "/R", "/ARGS"],
+      Self::Passive => &["/P", "/R"],
+      Self::Quiet => &["/S", "/R"],
       _ => &[],
     }
   }

3484d0a184ff3994abb21368a55131ae9f407e8c
amrbashir <amr.bashir2015@gmail.com>
2024-02-09 02:38:08 +0200

    license headers

--- tooling/cli/src/acl/mod.rs
+++ tooling/cli/src/acl/mod.rs
@@ -1,2 +1,6 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 pub mod capability;
 pub mod permission;

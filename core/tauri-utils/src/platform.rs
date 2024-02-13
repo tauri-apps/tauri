1e7f03d23df7886f3cc70ff5965f00ef531c1895
Sam Willis <sam.willis@gmail.com>
2024-02-13 18:47:02 +0000

    Fix resource_dir when app run from new target/someting/(debug|release) dir

--- core/tauri-utils/src/platform.rs
+++ core/tauri-utils/src/platform.rs
@@ -239,9 +239,14 @@ pub fn resource_dir(package_info: &PackageInfo, env: &Env) -> crate::Result<Path
   let exe_dir = exe.parent().expect("failed to get exe directory");
   let curr_dir = exe_dir.display().to_string();
 
-  if curr_dir.ends_with(format!("{MAIN_SEPARATOR}target{MAIN_SEPARATOR}debug").as_str())
-    || curr_dir.ends_with(format!("{MAIN_SEPARATOR}target{MAIN_SEPARATOR}release").as_str())
-    || cfg!(target_os = "windows")
+  let parts: Vec<&str> = curr_dir.split(MAIN_SEPARATOR).collect();
+
+  if (cfg!(target_os = "windows") || parts.contains(&"target"))
+    && (parts[parts.len() - 1] == "debug" || parts[parts.len() - 1] == "release")
+    && (parts[parts.len() - 2] == "target"
+      || parts
+        .get(parts.len() - 3)
+        .map_or(false, |&dir| dir == "target"))
   {
     // running from the out dir or windows
     return Ok(exe_dir.to_path_buf());

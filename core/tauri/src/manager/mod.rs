6251645acfe2df2da726f38a09373d7370bfcc86
Fabian-Lars <fabianlars@fabianlars.de>
2024-04-03 17:18:13 +0200

    fix(core): Update webview metadata on window close (#9360)
    * fix(core): Update webview metadata on window close
    
    * make it multiwebview friendlier
    
    * support webview.close() too -> THIS IS STILL MISSING AN EVENT LIKE tauri://destroyed !!!

--- core/tauri/src/manager/mod.rs
+++ core/tauri/src/manager/mod.rs
@@ -544,6 +544,12 @@ impl<R: Runtime> AppManager<R> {
 
   pub(crate) fn on_webview_close(&self, label: &str) {
     self.webview.webviews_lock().remove(label);
+
+    if let Ok(webview_labels_array) = serde_json::to_string(&self.webview.labels()) {
+      let _ = self.webview.eval_script_all(format!(
+          r#"(function () {{ const metadata = window.__TAURI_INTERNALS__.metadata; if (metadata != null) {{ metadata.webviews = {webview_labels_array}.map(function (label) {{ return {{ label: label }} }}) }} }})()"#,
+        ));
+    }
   }
 
   pub fn windows(&self) -> HashMap<String, Window<R>> {

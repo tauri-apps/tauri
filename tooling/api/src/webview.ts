4973d73a237dc5c60618c1011e202278e7a29b5c
Tony <68118705+Legend-Master@users.noreply.github.com>
2024-04-09 04:42:51 +0800

    feat: Add zoom hotkey polyfill for non windows platforms (#9386)

--- tooling/api/src/webview.ts
+++ tooling/api/src/webview.ts
@@ -666,7 +666,15 @@ interface WebviewOptions {
    * */
   proxyUrl?: string
   /**
-   * Whether page zooming by hotkeys or gestures is enabled **Windows Only**
+   * Whether page zooming by hotkeys is enabled
+   *
+   * ## Platform-specific:
+   *
+   * - **Windows**: Controls WebView2's [`IsZoomControlEnabled`](https://learn.microsoft.com/en-us/microsoft-edge/webview2/reference/winrt/microsoft_web_webview2_core/corewebview2settings?view=webview2-winrt-1.0.2420.47#iszoomcontrolenabled) setting.
+   * - **MacOS / Linux**: Injects a polyfill that zooms in and out with `ctrl/command` + `-/=`,
+   * 20% in each step, ranging from 20% to 1000%. Requires `webview:allow-set-webview-zoom` permission
+   *
+   * - **Android / iOS**: Unsupported.
    */
   zoomHotkeysEnabled?: boolean
 }

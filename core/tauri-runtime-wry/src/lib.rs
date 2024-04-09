tauri-apps/tauri

commit 5bd47b44673f74b1b4e8d704b7a95539915ede76

fix(windows): changing WebView visibility on hide/show/minimize (#9246)
* Fix not changing visibilty for windows webview

* Add change file

* Move is_visible calculation to the caller

* Rename update to set

core/tauri-runtime-wry/src/lib.rs
@@ -2632,8 +2632,16 @@ fn handle_user_message<T: UserEvent>(
           WindowMessage::Unmaximize => window.set_maximized(false),
           WindowMessage::Minimize => window.set_minimized(true),
           WindowMessage::Unminimize => window.set_minimized(false),
-          WindowMessage::Show => window.set_visible(true),
-          WindowMessage::Hide => window.set_visible(false),
+          WindowMessage::Show => {
+            window.set_visible(true);
+            #[cfg(windows)]
+            let _ = set_webview_visibility(&webviews, !window.is_minimized());
+          }
+          WindowMessage::Hide => {
+            window.set_visible(false);
+            #[cfg(windows)]
+            let _ = set_webview_visibility(&webviews, false);
+          }
           WindowMessage::Close => {
             panic!("cannot handle `WindowMessage::Close` on the main thread")
           }
@@ -3206,7 +3214,7 @@ fn handle_event_loop<T: UserEvent>(
               .map(|w| (w.inner.clone(), w.webviews.clone()))
             {
               let size = size.to_logical::<f32>(window.scale_factor());
-              for webview in webviews {
+              for webview in &webviews {
                 if let Some(b) = &*webview.bounds.lock().unwrap() {
                   webview.set_bounds(wry::Rect {
                     x: (size.width * b.x_rate) as i32,
@@ -3216,6 +3224,9 @@ fn handle_event_loop<T: UserEvent>(
                   });
                 }
               }
+              #[cfg(windows)]
+              let _ =
+                set_webview_visibility(&webviews, window.is_visible() && !window.is_minimized());
             }
           }
           _ => {}
@@ -3949,4 +3960,16 @@ fn clear_window_surface(
     buffer.fill(0);
     let _ = buffer.present();
   }
+}
+
+#[cfg(windows)]
+fn set_webview_visibility(
+  webviews: &[WebviewWrapper],
+  is_visible: bool,
+) -> windows::core::Result<()> {
+  for webview in webviews {
+    let controller = webview.controller();
+    unsafe { controller.SetIsVisible(is_visible) }?;
+  }
+  Ok(())
 }

commit 45d351db0432d157a8c8e2c00fb4edc11dea483e

fix(runtime-wry): don't hide/show webview when showing/hiding parent
This fixes a regression introduced in #9246 where previously users would wait for `DOMContentLoaded` before showing the window to avoid having a white flash, also introduce a regression for apps that is hidden by default and is shown on global shortcuts or some other action.

Also accoding to the MSDN docs for webview2's `IsVisible` they only recommend to change the webview visibility on when un/minimizing.

closes #9393
ref: https://learn.microsoft.com/en-us/microsoft-edge/webview2/reference/winrt/microsoft_web_webview2_core/corewebview2controller?view=webview2-winrt-1.0.2365.46#isvisible

core/tauri-runtime-wry/src/lib.rs
@@ -2699,16 +2699,8 @@ fn handle_user_message<T: UserEvent>(
           WindowMessage::Unmaximize => window.set_maximized(false),
           WindowMessage::Minimize => window.set_minimized(true),
           WindowMessage::Unminimize => window.set_minimized(false),
-          WindowMessage::Show => {
-            window.set_visible(true);
-            #[cfg(windows)]
-            let _ = set_webview_visibility(&webviews, !window.is_minimized());
-          }
-          WindowMessage::Hide => {
-            window.set_visible(false);
-            #[cfg(windows)]
-            let _ = set_webview_visibility(&webviews, false);
-          }
+          WindowMessage::Show => window.set_visible(true),
+          WindowMessage::Hide => window.set_visible(false),
           WindowMessage::Close => {
             panic!("cannot handle `WindowMessage::Close` on the main thread")
           }
@@ -3380,10 +3372,10 @@ fn handle_event_loop<T: UserEvent>(
                     log::error!("failed to autoresize webview: {e}");
                   }
                 }
+
+                #[cfg(windows)]
+                let _ = unsafe { webview.controller().SetIsVisible(!window.is_minimized()) };
               }
-              #[cfg(windows)]
-              let _ =
-                set_webview_visibility(&webviews, window.is_visible() && !window.is_minimized());
             }
           }
           _ => {}
@@ -4155,16 +4147,4 @@ fn clear_window_surface(
     buffer.fill(0);
     let _ = buffer.present();
   }
-}
-
-#[cfg(windows)]
-fn set_webview_visibility(
-  webviews: &[WebviewWrapper],
-  is_visible: bool,
-) -> windows::core::Result<()> {
-  for webview in webviews {
-    let controller = webview.controller();
-    unsafe { controller.SetIsVisible(is_visible) }?;
-  }
-  Ok(())
 }
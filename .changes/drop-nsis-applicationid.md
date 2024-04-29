afe41e607f3dc9b1a6d8d8ea640afbd3c7bb4a6f
amrbashir <amr.bashir2015@gmail.com>
2024-04-30 00:34:27 +0300

    chore(cli/deps): update `nsis-tauri-utils` to `0.3`

--- .changes/drop-nsis-applicationid.md
+++ .changes/drop-nsis-applicationid.md
@@ -2,4 +2,4 @@
 'tauri-bundler': 'patch:enhance'
 ---
 
-Use nsis's built-in com plugin instead of ApplicationID plugin, this reduces the installer size by 150-200 KB, and also fixes pinned shortcut not getting cleaned up on uninstall
+Use nsis's built-in COM plugin instead of `ApplicationID` plugin, this reduces the installer size by 100 KB, and also fixes pinned shortcut not getting cleaned up on uninstall.
--- tooling/bundler/src/bundle/windows/nsis.rs
+++ tooling/bundler/src/bundle/windows/nsis.rs
@@ -34,8 +34,8 @@ const NSIS_URL: &str =
 #[cfg(target_os = "windows")]
 const NSIS_SHA1: &str = "057e83c7d82462ec394af76c87d06733605543d4";
 const NSIS_TAURI_UTILS_URL: &str =
-  "https://github.com/tauri-apps/nsis-tauri-utils/releases/download/nsis_tauri_utils-v0.2.2/nsis_tauri_utils.dll";
-const NSIS_TAURI_UTILS_SHA1: &str = "16DF1D1A5B4D5DF3859447279C55BE36D4109DFB";
+  "https://github.com/tauri-apps/nsis-tauri-utils/releases/download/nsis_tauri_utils-v0.3.0/nsis_tauri_utils.dll";
+const NSIS_TAURI_UTILS_SHA1: &str = "01E48D6429B48B640230C6CE8F257C84758943AA";
 
 #[cfg(target_os = "windows")]
 const NSIS_REQUIRED_FILES: &[&str] = &[

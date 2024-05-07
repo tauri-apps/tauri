9a8c4ab39087ee3068304fa79c462312318d111c
Tony <legendmastertony@gmail.com>
2024-05-07 15:40:26 +0800

    Fix crashing when margin is bigger than screen

--- core/tauri-runtime-wry/src/lib.rs
+++ core/tauri-runtime-wry/src/lib.rs
@@ -3617,11 +3617,17 @@ fn create_window<T: UserEvent, F: Fn(RawWindow) + Send + 'static>(
         let constraint = PhysicalSize::new(size.width - margin.width, size.height - margin.height);
         if window_size.width > constraint.width || window_size.height > constraint.height {
           if window_size.width > constraint.width {
-            inner_size.width -= window_size.width - constraint.width;
+            inner_size.width = inner_size
+              .width
+              .checked_sub(window_size.width - constraint.width)
+              .unwrap_or_default();
             window_size.width = constraint.width;
           }
           if window_size.height > constraint.height {
-            inner_size.height -= window_size.height - constraint.height;
+            inner_size.height = inner_size
+              .height
+              .checked_sub(window_size.height - constraint.height)
+              .unwrap_or_default();
             window_size.height = constraint.height;
           }
           window_builder.inner.window.inner_size = Some(inner_size.into());

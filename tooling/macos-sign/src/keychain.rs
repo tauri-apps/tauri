b400c2adcc656d6b5b33ff72dbb47fcc54432972
Lucas Nogueira <lucas@tauri.app>
2024-06-04 09:52:06 -0300

    license headers

--- tooling/macos-sign/src/keychain.rs
+++ tooling/macos-sign/src/keychain.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use std::{
   ffi::OsString,
   path::{Path, PathBuf},

c81a0634e4ce00abbcd00324012922e3fb78cf9e
Pylogmon <pylogmon@outlook.com>
2024-06-05 08:48:35 +0800

    chore: create nsis tools path

--- tooling/bundler/src/bundle/windows/nsis.rs
+++ tooling/bundler/src/bundle/windows/nsis.rs
@@ -68,6 +68,10 @@ pub fn bundle_project(settings: &Settings, updater: bool) -> crate::Result<Vec<P
     .map(|d| d.join(".tauri"))
     .unwrap_or_else(|| dirs_next::cache_dir().unwrap().join("tauri"));
 
+  if !tauri_tools_path.exists() {
+    create_dir_all(&tauri_tools_path)?;
+  }
+
   let nsis_toolset_path = tauri_tools_path.join("NSIS");
 
   if !nsis_toolset_path.exists() {

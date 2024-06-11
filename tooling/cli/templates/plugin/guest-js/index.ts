fadb39eac84ac7e91fecc80f2c1948e370cae789
amrbashir <amr.bashir2015@gmail.com>
2024-06-11 04:40:47 +0300

    replace execute usage with ping

--- tooling/cli/templates/plugin/guest-js/index.ts
+++ tooling/cli/templates/plugin/guest-js/index.ts
@@ -3,6 +3,10 @@
 {{/if}}
 import { invoke } from '@tauri-apps/api/core'
 
-export async function execute() {
-  await invoke('plugin:{{ plugin_name }}|execute')
+export async function ping(value: string): Promise<string | null> {
+  return await invoke<{value?: string}>('plugin:{{ plugin_name }}|ping', {
+    payload: {
+      value,
+    },
+  }).then((r) => (r.value ? r.value : null));
 }

51173e7ee00355c9e3f6a18cc335ce6d90012384
Tony <legendmastertony@gmail.com>
2024-06-13 11:46:43 +0800

    Update dependencies

---
+++ tooling/api/eslint.config.js
@@ -0,0 +1,51 @@
+import globals from 'globals'
+import eslint from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import securityPlugin from 'eslint-plugin-security'
+import tseslint from 'typescript-eslint'
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  eslint.configs.recommended,
+  prettierConfig,
+  securityPlugin.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        __statics: true,
+        process: true
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    },
+
+    // add your custom rules here
+    rules: {
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'no-process-exit': 'off',
+      'security/detect-non-literal-fs-filename': 'warn',
+      'security/detect-unsafe-regex': 'error',
+      'security/detect-buffer-noassert': 'error',
+      'security/detect-child-process': 'warn',
+      'security/detect-disable-mustache-escape': 'error',
+      'security/detect-eval-with-expression': 'error',
+      'security/detect-no-csrf-before-method-override': 'error',
+      'security/detect-non-literal-regexp': 'error',
+      'security/detect-non-literal-require': 'warn',
+      'security/detect-object-injection': 'warn',
+      'security/detect-possible-timing-attacks': 'error',
+      'security/detect-pseudoRandomBytes': 'error',
+      'space-before-function-paren': 'off',
+      '@typescript-eslint/default-param-last': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 0,
+      'no-return-await': 'warn',
+      '@typescript-eslint/return-await': 'off'
+    }
+  }
+]

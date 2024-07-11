249cdde9b681b96463cb38ab740165d966396522
Tony <68118705+Legend-Master@users.noreply.github.com>
2024-07-11 17:11:15 +0800

    Fix Serialize impl missing TSend (#10252)

--- examples/api/src-tauri/tauri-plugin-sample/src/desktop.rs
+++ examples/api/src-tauri/tauri-plugin-sample/src/desktop.rs
@@ -17,14 +17,12 @@ pub fn init<R: Runtime, C: DeserializeOwned>(
 /// A helper class to access the sample APIs.
 pub struct Sample<R: Runtime>(AppHandle<R>);
 
-#[derive(serde::Serialize)]
-struct Event {
-  kind: &'static str,
-}
-
 impl<R: Runtime> Sample<R> {
   pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
-    let _ = payload.on_event.send(Event { kind: "ping" });
+    let _ = payload.on_event.send(Event {
+      kind: "ping".to_string(),
+      value: payload.value.clone(),
+    });
     Ok(PingResponse {
       value: payload.value,
     })

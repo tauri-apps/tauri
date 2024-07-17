0f6e245171588319280ab20746f1ffac3960b268
Jason Tsai <jason@pews.dev>
2024-07-17 11:29:39 +0800

    chore: add TODO

--- core/tauri-runtime-wry/src/lib.rs
+++ core/tauri-runtime-wry/src/lib.rs
@@ -720,6 +720,11 @@ impl WindowBuilder for WindowBuilderWrapper {
 
     #[cfg(target_os = "macos")]
     {
+      // TODO: find a proper way to prevent webview being pushed out of the window.
+      // Workround for issue: https://github.com/tauri-apps/tauri/issues/10225
+      // The window requies `NSFullSizeContentViewWindowMask` flag to prevent devtools
+      // pushing the content view out of the window.
+      // By setting the default style to `TitleBarStyle::Visible` should fix the issue for most of the users.
       builder = builder.title_bar_style(TitleBarStyle::Visible);
     }
 

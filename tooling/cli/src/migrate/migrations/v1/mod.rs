fc224346c88036ca36a20e5969b0f8a0008f0e45
Lucas Nogueira <lucas@tauri.app>
2024-07-26 14:10:56 -0300

    license headers

--- tooling/cli/src/migrate/migrations/v1/mod.rs
+++ tooling/cli/src/migrate/migrations/v1/mod.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use crate::{
   helpers::app_paths::{app_dir, tauri_dir},
   Result,

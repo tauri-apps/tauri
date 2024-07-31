ddc64b706a7f2db271d40e9b216187b1aa153efa
Oscar Beaumont <oscar@otbeaumont.me>
2024-07-31 22:25:14 +0800

    Don't rely on `DataType` reexport

--- core/tauri/src/lib.rs
+++ core/tauri/src/lib.rs
@@ -992,7 +992,7 @@ pub mod test;
 
 #[cfg(feature = "specta")]
 const _: () = {
-  use specta::{function::FunctionArg, DataType, TypeMap};
+  use specta::{function::FunctionArg, datatype::DataType, TypeMap};
 
   impl<'r, T: Send + Sync + 'static> FunctionArg for crate::State<'r, T> {
     fn to_datatype(_: &mut TypeMap) -> Option<DataType> {

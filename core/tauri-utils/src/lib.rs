b32295de1804bc02ab34d60a7c9b688428149f91
chip <chip@chip.sh>
2024-08-02 19:30:02 +0900

    prevent unnecessary rebuilds when working in the cargo workspace (#10442)
    * hash codegen image cache output
    
    * remove left over dbg! statement
    
    * prevent info.plist from workspace rebuilds
    
    * prevent schema generation from workspace rebuilds
    
    * use new `Cached` struct in `CachedIcon`
    
    * fmt
    
    * use full import for cached plist
    
    * use `to_vec()` for raw icons

--- core/tauri-utils/src/lib.rs
+++ core/tauri-utils/src/lib.rs
@@ -380,3 +380,20 @@ pub fn display_path<P: AsRef<Path>>(p: P) -> String {
     .display()
     .to_string()
 }
+
+/// Write the file only if the content of the existing file (if any) is different.
+///
+/// This will always write unless the file exists with identical content.
+pub fn write_if_changed<P, C>(path: P, content: C) -> std::io::Result<()>
+where
+  P: AsRef<Path>,
+  C: AsRef<[u8]>,
+{
+  if let Ok(existing) = std::fs::read(&path) {
+    if existing == content.as_ref() {
+      return Ok(());
+    }
+  }
+
+  std::fs::write(path, content)
+}

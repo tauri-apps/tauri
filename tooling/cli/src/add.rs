tauri-apps/tauri

commit 2d47352a07a7d742e62291a5e6810aed79fc8b50

fix(cli): make app_dir() logic consistent (#10418)
* fix(cli): Make app_dir() consistent by basing it on the explicit invocation directory rather than the current working directory

* resolve app paths before everything else

* fix xcode script

* fix test

---------

Co-authored-by: Lucas Nogueira <lucas@tauri.app>

tooling/cli/src/add.rs
@@ -9,7 +9,7 @@ use regex::Regex;
 use crate::{
   acl,
   helpers::{
-    app_paths::{app_dir, tauri_dir},
+    app_paths::{resolve_app_dir, tauri_dir},
     cargo,
     npm::PackageManager,
   },
@@ -92,6 +92,8 @@ pub struct Options {
 }
 
 pub fn command(options: Options) -> Result<()> {
+  crate::helpers::app_paths::resolve();
+
   let (plugin, version) = options
     .plugin
     .split_once('@')
@@ -105,6 +107,7 @@ pub fn command(options: Options) -> Result<()> {
   let mut plugins = plugins();
   let metadata = plugins.remove(plugin).unwrap_or_default();
 
+  let app_dir = resolve_app_dir();
   let tauri_dir = tauri_dir();
 
   let target_str = metadata
@@ -122,14 +125,12 @@ pub fn command(options: Options) -> Result<()> {
     branch: options.branch.as_deref(),
     rev: options.rev.as_deref(),
     tag: options.tag.as_deref(),
-    cwd: Some(&tauri_dir),
+    cwd: Some(tauri_dir),
     target: target_str,
   })?;
 
   if !metadata.rust_only {
-    if let Some(manager) = std::panic::catch_unwind(app_dir)
-      .map(Some)
-      .unwrap_or_default()
+    if let Some(manager) = app_dir
       .map(PackageManager::from_project)
       .and_then(|managers| managers.into_iter().next())
     {
@@ -149,7 +150,7 @@ pub fn command(options: Options) -> Result<()> {
         (None, None, None, None) => npm_name,
         _ => anyhow::bail!("Only one of --tag, --rev and --branch can be specified"),
       };
-      manager.install(&[npm_spec], &tauri_dir)?;
+      manager.install(&[npm_spec], tauri_dir)?;
     }
 
     let _ = acl::permission::add::command(acl::permission::add::Options {
@@ -193,7 +194,7 @@ pub fn command(options: Options) -> Result<()> {
         log::info!("Running `cargo fmt`...");
         let _ = Command::new("cargo")
           .arg("fmt")
-          .current_dir(&tauri_dir)
+          .current_dir(tauri_dir)
           .status();
       }
 

tooling/cli/src/migrate/migrations/v1/mod.rs
@@ -17,9 +17,9 @@ pub fn run() -> Result<()> {
   let tauri_dir = tauri_dir();
   let app_dir = app_dir();
 
-  let migrated = config::migrate(&tauri_dir).context("Could not migrate config")?;
-  manifest::migrate(&tauri_dir).context("Could not migrate manifest")?;
-  frontend::migrate(app_dir, &tauri_dir)?;
+  let migrated = config::migrate(tauri_dir).context("Could not migrate config")?;
+  manifest::migrate(tauri_dir).context("Could not migrate manifest")?;
+  frontend::migrate(app_dir, tauri_dir)?;
 
   // Add plugins
   for plugin in migrated.plugins {

tooling/cli/src/acl/permission/add.rs
@@ -7,7 +7,7 @@ use std::path::Path;
 use clap::Parser;
 
 use crate::{
-  helpers::{app_paths::tauri_dir_opt, prompts},
+  helpers::{app_paths::resolve_tauri_dir, prompts},
   Result,
 };
 
@@ -87,7 +87,7 @@ pub struct Options {
 }
 
 pub fn command(options: Options) -> Result<()> {
-  let dir = match tauri_dir_opt() {
+  let dir = match resolve_tauri_dir() {
     Some(t) => t,
     None => std::env::current_dir()?,
   };

tooling/cli/src/helpers/config.rs
@@ -132,7 +132,7 @@ fn get_internal(
   let mut extensions = HashMap::new();
 
   if let Some((platform_config, config_path)) =
-    tauri_utils::config::parse::read_platform(target, tauri_dir)?
+    tauri_utils::config::parse::read_platform(target, tauri_dir.to_path_buf())?
   {
     merge(&mut config, &platform_config);
     extensions.insert(

tooling/cli/src/info/mod.rs
@@ -7,10 +7,7 @@ use clap::Parser;
 use colored::{ColoredString, Colorize};
 use dialoguer::{theme::ColorfulTheme, Confirm};
 use serde::Deserialize;
-use std::{
-  fmt::{self, Display, Formatter},
-  panic,
-};
+use std::fmt::{self, Display, Formatter};
 
 mod app;
 mod env_nodejs;
@@ -259,17 +256,15 @@ pub struct Options {
 
 pub fn command(options: Options) -> Result<()> {
   let Options { interactive } = options;
-  let hook = panic::take_hook();
-  panic::set_hook(Box::new(|_info| {
-    // do nothing
-  }));
-  let app_dir = panic::catch_unwind(crate::helpers::app_paths::app_dir)
-    .map(Some)
-    .unwrap_or_default();
-  let tauri_dir = panic::catch_unwind(crate::helpers::app_paths::tauri_dir)
-    .map(Some)
-    .unwrap_or_default();
-  panic::set_hook(hook);
+
+  let app_dir = crate::helpers::app_paths::resolve_app_dir();
+  let tauri_dir = crate::helpers::app_paths::resolve_tauri_dir();
+
+  if tauri_dir.is_some() {
+    // safe to initialize
+    crate::helpers::app_paths::resolve();
+  }
+
   let metadata = version_metadata()?;
 
   let mut environment = Section {
@@ -289,17 +284,19 @@ pub fn command(options: Options) -> Result<()> {
   };
   packages
     .items
-    .extend(packages_rust::items(app_dir, tauri_dir.as_deref()));
+    .extend(packages_rust::items(app_dir.as_ref(), tauri_dir.as_deref()));
   packages
     .items
-    .extend(packages_nodejs::items(app_dir, &metadata));
+    .extend(packages_nodejs::items(app_dir.as_ref(), &metadata));
 
   let mut app = Section {
     label: "App",
     interactive,
     items: Vec::new(),
   };
-  app.items.extend(app::items(app_dir, tauri_dir.as_deref()));
+  app
+    .items
+    .extend(app::items(app_dir.as_ref(), tauri_dir.as_deref()));
 
   environment.display();
   packages.display();

tooling/cli/src/migrate/mod.rs
@@ -18,6 +18,8 @@ use anyhow::Context;
 mod migrations;
 
 pub fn command() -> Result<()> {
+  crate::helpers::app_paths::resolve();
+
   let tauri_dir = tauri_dir();
 
   let manifest_contents =
@@ -36,7 +38,7 @@ pub fn command() -> Result<()> {
     None
   };
 
-  let tauri_version = crate_version(&tauri_dir, Some(&manifest), lock.as_ref(), "tauri").version;
+  let tauri_version = crate_version(tauri_dir, Some(&manifest), lock.as_ref(), "tauri").version;
   let tauri_version = semver::Version::from_str(&tauri_version)?;
 
   if tauri_version.major == 1 {

tooling/cli/src/mobile/android/mod.rs
@@ -75,12 +75,15 @@ enum Commands {
 pub fn command(cli: Cli, verbosity: u8) -> Result<()> {
   let noise_level = NoiseLevel::from_occurrences(verbosity as u64);
   match cli.command {
-    Commands::Init(options) => init_command(
-      MobileTarget::Android,
-      options.ci,
-      false,
-      options.skip_targets_install,
-    )?,
+    Commands::Init(options) => {
+      crate::helpers::app_paths::resolve();
+      init_command(
+        MobileTarget::Android,
+        options.ci,
+        false,
+        options.skip_targets_install,
+      )?
+    }
     Commands::Dev(options) => dev::command(options, noise_level)?,
     Commands::Build(options) => build::command(options, noise_level)?,
     Commands::AndroidStudioScript(options) => android_studio_script::command(options)?,

tooling/cli/src/mobile/ios/mod.rs
@@ -86,12 +86,15 @@ enum Commands {
 pub fn command(cli: Cli, verbosity: u8) -> Result<()> {
   let noise_level = NoiseLevel::from_occurrences(verbosity as u64);
   match cli.command {
-    Commands::Init(options) => init_command(
-      MobileTarget::Ios,
-      options.ci,
-      options.reinstall_deps,
-      options.skip_targets_install,
-    )?,
+    Commands::Init(options) => {
+      crate::helpers::app_paths::resolve();
+      init_command(
+        MobileTarget::Ios,
+        options.ci,
+        options.reinstall_deps,
+        options.skip_targets_install,
+      )?
+    }
     Commands::Dev(options) => dev::command(options, noise_level)?,
     Commands::Build(options) => build::command(options, noise_level)?,
     Commands::XcodeScript(options) => xcode_script::command(options)?,

commit d4d3b3b7f0356799b799600e637452424cf03fb2

fix(cli): migrate v1 plugin dependencies, prevent dup, closes #10650
also fixes a small regression from #10418

tooling/cli/src/add.rs
@@ -39,7 +39,10 @@ pub struct Options {
 
 pub fn command(options: Options) -> Result<()> {
   crate::helpers::app_paths::resolve();
+  run(options)
+}
 
+pub fn run(options: Options) -> Result<()> {
   let (plugin, version) = options
     .plugin
     .split_once('@')

tooling/cli/src/migrate/migrations/v1/config.rs
@@ -386,13 +386,16 @@ fn allowlist_to_permissions(
   allowlist: tauri_utils_v1::config::AllowlistConfig,
 ) -> Vec<PermissionEntry> {
   macro_rules! permissions {
-    ($allowlist: ident, $permissions_list: ident, $object: ident, $field: ident => $associated_permission: expr) => {
+    ($allowlist: ident, $permissions_list: ident, $object: ident, $field: ident => $associated_permission: expr) => {{
       if $allowlist.all || $allowlist.$object.all || $allowlist.$object.$field {
         $permissions_list.push(PermissionEntry::PermissionRef(
           $associated_permission.to_string().try_into().unwrap(),
         ));
+        true
+      } else {
+        false
       }
-    };
+    }};
   }
 
   let mut permissions = Vec::new();
@@ -474,8 +477,11 @@ fn allowlist_to_permissions(
 
   // shell
   if allowlist.shell.scope.0.is_empty() {
-    permissions!(allowlist, permissions, shell, execute => "shell:allow-execute");
-    permissions!(allowlist, permissions, shell, sidecar => "shell:allow-execute");
+    let added = permissions!(allowlist, permissions, shell, execute => "shell:allow-execute");
+    // prevent duplicated permission
+    if !added {
+      permissions!(allowlist, permissions, shell, sidecar => "shell:allow-execute");
+    }
   } else {
     let allowed = allowlist
       .shell

tooling/cli/src/migrate/migrations/v1/manifest.rs
@@ -45,6 +45,22 @@ fn migrate_manifest(manifest: &mut Document) -> Result<()> {
     ("tauri-codegen", "dependencies"),
     ("tauri-macros", "dependencies"),
     ("tauri-runtime-wry", "dependencies"),
+    // normal deps - plugins
+    ("tauri-plugin-authenticator", "dependencies"),
+    ("tauri-plugin-autostart", "dependencies"),
+    ("tauri-plugin-fs-extra", "dependencies"),
+    ("tauri-plugin-fs-watch", "dependencies"),
+    ("tauri-plugin-localhost", "dependencies"),
+    ("tauri-plugin-log", "dependencies"),
+    ("tauri-plugin-persisted-scope", "dependencies"),
+    ("tauri-plugin-positioner", "dependencies"),
+    ("tauri-plugin-single-instance", "dependencies"),
+    ("tauri-plugin-sql", "dependencies"),
+    ("tauri-plugin-store", "dependencies"),
+    ("tauri-plugin-stronghold", "dependencies"),
+    ("tauri-plugin-upload", "dependencies"),
+    ("tauri-plugin-websocket", "dependencies"),
+    ("tauri-plugin-window-state", "dependencies"),
     // dev
     ("tauri", "dev-dependencies"),
     ("tauri-utils", "dev-dependencies"),
@@ -207,6 +223,10 @@ fn migrate_dependency_table<D: TableLike>(
   remove: &[&str],
   rename: &[(&str, &str)],
 ) {
+  dep.remove("rev");
+  dep.remove("git");
+  dep.remove("branch");
+  dep.remove("tag");
   *dep.entry("version").or_insert(Item::None) = Item::Value(version.into());
   let manifest_features = dep.entry("features").or_insert(Item::None);
   if let Some(features_array) = manifest_features.as_array_mut() {

tooling/cli/src/migrate/migrations/v1/mod.rs
@@ -25,7 +25,7 @@ pub fn run() -> Result<()> {
 
   // Add plugins
   for plugin in migrated.plugins {
-    crate::add::command(crate::add::Options {
+    crate::add::run(crate::add::Options {
       plugin: plugin.clone(),
       branch: None,
       tag: None,
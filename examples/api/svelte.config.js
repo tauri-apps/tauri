be77537653f22f0895b217201744fa8e11e848e9
amrbashir <github@amrbashir.me>
2024-08-22 21:43:38 +0300

    fmt

--- examples/api/svelte.config.js
+++ examples/api/svelte.config.js
@@ -3,5 +3,5 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess()
 }

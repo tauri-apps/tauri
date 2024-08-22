784eb23d71921e07cf14cf4322ba87f972f6bd64
amrbashir <github@amrbashir.me>
2024-08-22 21:43:10 +0300

    chore: cleanup and simplify examples
    - Updated `.gitignore` file
    - Simplified and cleanedup examples
    - Removed examples that serve almost zero value:
      - `navigation`
      - `parent-window`
      - `web`
      - `workspace`
      - `run-iteration`
      - `plugins/tauri-plugin-sample`: the plugin example in `api` example is enough

--- examples/api/svelte.config.js
+++ examples/api/svelte.config.js
@@ -1,11 +1,7 @@
-// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-License-Identifier: MIT
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
 }
--- examples/run-iteration/README.md
+++
@@ -1,3 +0,0 @@
-# Run Iteration Example
-
-To execute run the following on the root directory of the repository: `cargo run --example run-iteration`.

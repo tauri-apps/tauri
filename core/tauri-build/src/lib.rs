503e26c5208c71780ce599d701930faf30ef2714
amrbashir <github@amrbashir.me>
2024-08-24 10:02:51 +0300

    fix doctest

--- core/tauri-build/src/lib.rs
+++ core/tauri-build/src/lib.rs
@@ -369,7 +369,7 @@ impl WindowsAttributes {
   ///
   /// ```rust,no_run
   /// let mut windows = tauri_build::WindowsAttributes::new();
-  /// windows = windows.app_manifest_file("./path/to/app-manifest.xml");
+  /// windows = windows.app_manifest_path("./path/to/app-manifest.xml");
   /// let attrs =  tauri_build::Attributes::new().windows_attributes(windows);
   /// tauri_build::try_build(attrs).expect("failed to run build script");
   /// ```

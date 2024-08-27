7fdc0472b933ff36042ffe84034adeb8ea28208c
Lucas Nogueira <lucas@tauri.app>
2024-08-27 18:29:51 -0300

    chore(cli): remove unused code

--- tooling/cli/src/info/mod.rs
+++ tooling/cli/src/info/mod.rs
@@ -37,40 +37,6 @@ fn version_metadata() -> Result<VersionMetadata> {
   serde_json::from_str::<VersionMetadata>(include_str!("../../metadata.json")).map_err(Into::into)
 }
 
-#[cfg(not(debug_assertions))]
-pub(crate) fn cli_current_version() -> Result<String> {
-  version_metadata().map(|meta| meta.js_cli.version)
-}
-
-#[cfg(not(debug_assertions))]
-pub(crate) fn cli_upstream_version() -> Result<String> {
-  let upstream_metadata = match ureq::get(
-    "https://raw.githubusercontent.com/tauri-apps/tauri/dev/tooling/cli/metadata.json",
-  )
-  .timeout(std::time::Duration::from_secs(3))
-  .call()
-  {
-    Ok(r) => r,
-    Err(ureq::Error::Status(code, _response)) => {
-      let message = format!("Unable to find updates at the moment. Code: {}", code);
-      return Err(anyhow::Error::msg(message));
-    }
-    Err(ureq::Error::Transport(transport)) => {
-      let message = format!(
-        "Unable to find updates at the moment. Error: {:?}",
-        transport.kind()
-      );
-      return Err(anyhow::Error::msg(message));
-    }
-  };
-
-  upstream_metadata
-    .into_string()
-    .and_then(|meta_str| Ok(serde_json::from_str::<VersionMetadata>(&meta_str)))
-    .and_then(|json| Ok(json.unwrap().js_cli.version))
-    .map_err(|e| anyhow::Error::new(e))
-}
-
 pub fn cross_command(bin: &str) -> Command {
   #[cfg(target_os = "windows")]
   let cmd = {

tauri-apps/tauri

commit bfc49cc7a1d43e3378e93865b9b37ce4bddfa6e6

refactor(android): Remove targetSdk from gradle config  (#10558)
* refactor: remove targetSdk as it is being removed in DSL 9.0

* note

* fix: typo

* update: changelog

Co-authored-by: Lucas Fernandes Nogueira <lucas@tauri.app>

---------

Co-authored-by: Lucas Fernandes Nogueira <lucas@tauri.app>

tooling/cli/templates/mobile/android/app/build.gradle.kts
@@ -22,7 +22,6 @@ android {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "{{reverse-domain app.identifier}}"
         minSdk = {{android.min-sdk-version}}
-        targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }

core/tauri/mobile/android/build.gradle.kts
@@ -9,7 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("proguard-rules.pro")

examples/api/src-tauri/tauri-plugin-sample/android/build.gradle.kts
@@ -9,7 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")

tooling/cli/templates/plugin/android/build.gradle.kts
@@ -9,7 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")

commit a5e62498bc5591e4d150ecc71a56d83b65a12534

fix(cli): readd targetSdk to Android app template, closes #10712
Regression from #10558

tooling/cli/templates/mobile/android/app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "{{app.identifier}}"
         minSdk = {{android.min-sdk-version}}
+        targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
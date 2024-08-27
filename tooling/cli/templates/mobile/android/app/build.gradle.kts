a5e62498bc5591e4d150ecc71a56d83b65a12534
Lucas Nogueira <lucas@tauri.app>
2024-08-27 08:47:44 -0300

    fix(cli): readd targetSdk to Android app template, closes #10712
    Regression from #10558

--- tooling/cli/templates/mobile/android/app/build.gradle.kts
+++ tooling/cli/templates/mobile/android/app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "{{app.identifier}}"
         minSdk = {{android.min-sdk-version}}
+        targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }

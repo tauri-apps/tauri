ac90b08c60e4da4eae6efce6c0415ef2c4b0ebe0
thepoy <thepoy@163.com>
2024-09-02 22:07:36 +0800

    feat(bundler): Enhance download mechanism for webview2 offline installer to support GitHub CDN
    This commit introduces a feature that checks for the presence of an environment variable `GITHUB_CDN` before downloading the webview2 offline installer. If `GITHUB_CDN` is set, the download URL is rewritten to use a specified GitHub CDN, which can improve download speeds for users in regions where direct access to GitHub might be slow or unreliable. This change allows users to benefit from faster downloads without needing to configure proxy servers, particularly beneficial for those in mainland China where alternative CDNs are commonly used.

--- crates/tauri-bundler/src/bundle/windows/util.rs
+++ crates/tauri-bundler/src/bundle/windows/util.rs
@@ -70,8 +70,20 @@ pub fn download_webview2_offline_installer(base_path: &Path, arch: &str) -> crat
 pub fn download(url: &str) -> crate::Result<Vec<u8>> {
   log::info!(action = "Downloading"; "{}", url);
 
-  let agent = ureq::AgentBuilder::new().try_proxy_from_env(true).build();
-  let response = agent.get(url).call().map_err(Box::new)?;
+  let (agent, url) = match std::env::var("GITHUB_CDN") {
+    Ok(cdn) => {
+      let mut parsed_cdn = url::Url::parse(&cdn).expect("Invalid cdn");
+      parsed_cdn.set_path(url);
+
+      (ureq::AgentBuilder::new().build(), parsed_cdn.to_string())
+    }
+    Err(_) => (
+      ureq::AgentBuilder::new().try_proxy_from_env(true).build(),
+      url.to_owned(),
+    ),
+  };
+
+  let response = agent.get(&url).call().map_err(Box::new)?;
   let mut bytes = Vec::new();
   response.into_reader().read_to_end(&mut bytes)?;
   Ok(bytes)

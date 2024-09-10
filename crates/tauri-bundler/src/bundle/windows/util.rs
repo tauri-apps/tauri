2199e4838332300bc57acf1f406149106865b175
thepoy <thepoy@163.com>
2024-09-10 18:32:56 +0800

    Refactor download function to improve readability and maintainability
    - Extracted the creation of the HTTP agent and URL into a separate function `create_agent_and_url`.
    - Ensured that the `TAURI_BUNDLER_TOOLS_DOWNLOAD_GITHUB_CDN` environment variable is used when downloading from GitHub CDN.

--- crates/tauri-bundler/src/bundle/windows/util.rs
+++ crates/tauri-bundler/src/bundle/windows/util.rs
@@ -9,6 +9,8 @@ use std::{
 };
 
 use sha2::Digest;
+use ureq::AgentBuilder;
+use url::Url;
 use zip::ZipArchive;
 
 pub const WEBVIEW2_BOOTSTRAPPER_URL: &str = "https://go.microsoft.com/fwlink/p/?LinkId=2124703";
@@ -67,23 +69,26 @@ pub fn download_webview2_offline_installer(base_path: &Path, arch: &str) -> crat
   Ok(file_path)
 }
 
+fn create_agent_and_url(url: &str) -> crate::Result<(ureq::Agent, String)> {
+  match std::env::var("TAURI_BUNDLER_TOOLS_DOWNLOAD_GITHUB_CDN") {
+    Ok(cdn) if url.starts_with("https://github.com/") => {
+      let mut parsed_cdn = Url::parse(&cdn)?;
+      parsed_cdn.set_path(url);
+      Ok((AgentBuilder::new().build(), parsed_cdn.into()))
+    }
+    _ => Ok((
+      AgentBuilder::new().try_proxy_from_env(true).build(),
+      url.to_owned(),
+    )),
+  }
+}
+
 pub fn download(url: &str) -> crate::Result<Vec<u8>> {
   log::info!(action = "Downloading"; "{}", url);
 
-  let (agent, url) = match std::env::var("TAURI_BUNDLER_TOOLS_DOWNLOAD_GITHUB_CDN") {
-    Ok(cdn) => {
-      let mut parsed_cdn = url::Url::parse(&cdn)?;
-      parsed_cdn.set_path(url);
+  let (agent, final_url) = create_agent_and_url(url)?;
 
-      (ureq::AgentBuilder::new().build(), parsed_cdn.to_string())
-    }
-    Err(_) => (
-      ureq::AgentBuilder::new().try_proxy_from_env(true).build(),
-      url.to_owned(),
-    ),
-  };
-
-  let response = agent.get(&url).call().map_err(Box::new)?;
+  let response = agent.get(&final_url).call().map_err(Box::new)?;
   let mut bytes = Vec::new();
   response.into_reader().read_to_end(&mut bytes)?;
   Ok(bytes)

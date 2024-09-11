9dcfd7e470fb29595af4364ca5a35fd10e034eea
Lucas Nogueira <lucas@tauri.studio>
2024-09-11 15:21:49 -0300

    add v1 -> v2 migration e2e test

---
+++ core/tests/app-updater/tests/fixtures/tauri-v2/src-tauri/src/main.rs
@@ -0,0 +1,47 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri_plugin_updater::UpdaterExt;
+
+fn main() {
+  tauri::Builder::default()
+    .plugin(tauri_plugin_shell::init())
+    .plugin(tauri_plugin_updater::Builder::new().build())
+    .setup(|app| {
+      let handle = app.handle().clone();
+      tauri::async_runtime::spawn(async move {
+        match handle.updater().unwrap().check().await {
+          Ok(Some(update)) => {
+            println!("got update {}", update.version);
+
+            if let Err(e) = update
+              .download_and_install(
+                |chunk, _content_length| {
+                  println!("downloaded {chunk} bytes");
+                },
+                || {
+                  println!("finished downloading");
+                },
+              )
+              .await
+            {
+              println!("{e}");
+              std::process::exit(1);
+            } else {
+              std::process::exit(0);
+            }
+          }
+          Ok(None) => {
+            println!("update not found");
+            std::process::exit(2)
+          }
+          Err(e) => {
+            println!("{e}");
+            std::process::exit(1);
+          }
+        }
+      });
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}

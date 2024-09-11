b9ec4a3096c15ac852c3585837679b3e66367c10
Lucas Nogueira <lucas@tauri.studio>
2024-09-11 17:22:19 -0300

    add lcense headers

--- core/tests/app-updater/tests/fixtures/tauri-v2/src-tauri/src/main.rs
+++ core/tests/app-updater/tests/fixtures/tauri-v2/src-tauri/src/main.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri_plugin_updater::UpdaterExt;

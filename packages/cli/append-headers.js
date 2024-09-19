a9b47489ddc542f2bc1bd3faa266a5fda7a847cf
amrbashir <github@amrbashir.me>
2024-09-19 20:47:06 +0300

    fix(cli/add): use `>=` for npm and `~` for other package managers
    closes #10906

---
+++ packages/cli/append-headers.js
@@ -0,0 +1,11 @@
+const HEADERS = `// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT`
+
+const fs = require('fs')
+
+for (const file of ['index.js', 'index.d.ts']) {
+  const content = fs.readFileSync(file, 'utf8')
+  const newContent = `${HEADERS}\n\n${content}`
+  fs.writeFileSync(file, newContent, 'utf8')
+}

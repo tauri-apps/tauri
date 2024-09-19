863003bee5cf699c4834850aefcc7ce3390b2a11
amrbashir <github@amrbashir.me>
2024-09-19 20:48:46 +0300

    headers

--- packages/cli/append-headers.js
+++ packages/cli/append-headers.js
@@ -1,3 +1,7 @@
+// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT`
+
 const HEADERS = `// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT`

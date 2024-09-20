b88e22a5fe4e2e4376d6cad64d1e74d104ca8927
Fabian-Lars <fabianlars@fabianlars.de>
2024-09-20 12:31:51 +0200

    fix(cli): set ios deployment target only when building for ios (#11063)

--- crates/tauri-cli/src/interface/rust.rs
+++ crates/tauri-cli/src/interface/rust.rs
@@ -151,10 +151,14 @@ impl Interface for Rust {
       std::env::set_var("MACOSX_DEPLOYMENT_TARGET", minimum_system_version);
     }
 
-    std::env::set_var(
-      "IPHONEOS_DEPLOYMENT_TARGET",
-      &config.bundle.ios.minimum_system_version,
-    );
+    if let Some(target) = &target {
+      if target.ends_with("ios") || target.ends_with("ios-sim") {
+        std::env::set_var(
+          "IPHONEOS_DEPLOYMENT_TARGET",
+          &config.bundle.ios.minimum_system_version,
+        );
+      }
+    }
 
     let app_settings = RustAppSettings::new(config, manifest, target)?;
 

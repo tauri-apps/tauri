84a6ae2ececac28a8befac1b7562d746ac36e4ea
Lucas Nogueira <lucas@tauri.app>
2024-09-30 20:49:42 -0300

    fix header

--- crates/tauri-macos-sign/src/certificate.rs
+++ crates/tauri-macos-sign/src/certificate.rs
@@ -1,3 +1,7 @@
+// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use anyhow::{Context, Result};
 use apple_codesign::create_self_signed_code_signing_certificate;
 use x509_certificate::{EcdsaCurve, KeyAlgorithm};

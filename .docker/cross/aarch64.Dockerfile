tauri-apps/tauri

commit 3c7258d6518246da4d188a10e0a53437fe060021

fix(ci): run on ubuntu 22.04 (#11358)
* fix(ci): install libsoup2.4-dev

* update action

* ubuntu 22.04

* fix image

* latest for cli checks

* ubuntu 20.04

* Revert "ubuntu 20.04"

This reverts commit 6fd0ebb2c342accfd73b0a00963e926976a03dd2.

* fix audit

* fix cli audit

.docker/cross/aarch64.Dockerfile
@@ -41,4 +41,4 @@ ENV CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc \
 
 RUN dpkg --add-architecture arm64
 RUN apt-get update
-RUN apt-get install --assume-yes --no-install-recommends libssl-dev:arm64 libdbus-1-dev:arm64 libsoup2.4-dev:arm64 libssl-dev:arm64 libgtk-3-dev:arm64 webkit2gtk-4.0-dev:arm64 libappindicator3-1:arm64 librsvg2-dev:arm64 patchelf:arm64
+RUN apt-get install --assume-yes --no-install-recommends libssl-dev:arm64 libdbus-1-dev:arm64: libsoup2.4-devarm64 libssl-dev:arm64 libgtk-3-dev:arm64 webkit2gtk-4.0-dev:arm64 libappindicator3-1:arm64 librsvg2-dev:arm64 patchelf:arm64

.github/workflows/audit.yml
@@ -22,7 +22,7 @@ concurrency:
 
 jobs:
   audit-rust:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     steps:
       - uses: actions/checkout@v2
       - name: rust audit
@@ -31,7 +31,7 @@ jobs:
           token: ${{ secrets.GITHUB_TOKEN }}
 
   audit-js:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     steps:
       - uses: actions/checkout@v2
       - name: audit workspace

.github/workflows/check-change-tags.yml
@@ -15,7 +15,7 @@ concurrency:
 
 jobs:
   check:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     steps:
       - uses: actions/checkout@v3
 

.github/workflows/check-generated-files.yml
@@ -20,7 +20,7 @@ concurrency:
 
 jobs:
   changes:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     outputs:
       api: ${{ steps.filter.outputs.api }}
       schema: ${{ steps.filter.outputs.schema }}
@@ -39,7 +39,7 @@ jobs:
               - 'core/tauri-config-schema/schema.json'
 
   api:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     needs: changes
     if: needs.changes.outputs.api == 'true'
     steps:
@@ -52,7 +52,7 @@ jobs:
         run: ./.scripts/ci/has-diff.sh
 
   schema:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     needs: changes
     if: needs.changes.outputs.schema == 'true'
     steps:

.github/workflows/check-license-header.yml
@@ -13,7 +13,7 @@ concurrency:
 
 jobs:
   check:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     steps:
       - uses: actions/checkout@v2
       - uses: dorny/paths-filter@v2

.github/workflows/covector-status.yml
@@ -7,7 +7,7 @@ on: [pull_request]
 
 jobs:
   covector:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
 
     steps:
       - uses: actions/checkout@v2

.github/workflows/covector-version-or-publish-v1.yml
@@ -34,7 +34,7 @@ jobs:
             }
           - {
               target: x86_64-unknown-linux-gnu,
-              os: ubuntu-latest,
+              os: ubuntu-22.04,
               toolchain: '1.60.0'
             }
           - {
@@ -165,7 +165,7 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        platform: [ubuntu-latest, macos-13, windows-latest]
+        platform: [ubuntu-22.04, macos-13, windows-latest]
 
     steps:
       - uses: actions/checkout@v4
@@ -174,7 +174,7 @@ jobs:
       - name: install stable
         uses: dtolnay/rust-toolchain@stable
       - name: install Linux dependencies
-        if: matrix.platform == 'ubuntu-latest'
+        if: matrix.platform == 'ubuntu-22.04'
         run: |
           sudo apt-get update
           sudo apt-get install -y webkit2gtk-4.0 webkit2gtk-4.1 libayatana-appindicator3-dev libfuse2
@@ -203,7 +203,7 @@ jobs:
           yarn test
 
   version-or-publish:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     timeout-minutes: 65
     permissions:
       actions: write # required for workflow_dispatch

.github/workflows/covector-version-or-publish.yml
@@ -16,7 +16,7 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        platform: [ubuntu-latest, macos-latest, windows-latest]
+        platform: [ubuntu-22.04, macos-latest, windows-latest]
 
     steps:
       - uses: actions/checkout@v2
@@ -27,7 +27,7 @@ jobs:
         with:
           toolchain: stable
       - name: install Linux dependencies
-        if: matrix.platform == 'ubuntu-latest'
+        if: matrix.platform == 'ubuntu-22.04'
         run: |
           sudo apt-get update
           sudo apt-get install -y webkit2gtk-4.0 libayatana-appindicator3-dev libfuse2
@@ -56,7 +56,7 @@ jobs:
           yarn test
 
   version-or-publish:
-    runs-on: ubuntu-latest
+    runs-on: ubuntu-22.04
     timeout-minutes: 65
     outputs:
       change: ${{ steps.covector.outputs.change }}

commit 16cf0a8f5c9f23287501a002a9b581ed2c89b78c

fix: aarch64 docker image regression from #11358

.docker/cross/aarch64.Dockerfile
@@ -41,4 +41,4 @@ ENV CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc \
 
 RUN dpkg --add-architecture arm64
 RUN apt-get update
-RUN apt-get install --assume-yes --no-install-recommends libssl-dev:arm64 libdbus-1-dev:arm64: libsoup2.4-devarm64 libssl-dev:arm64 libgtk-3-dev:arm64 webkit2gtk-4.0-dev:arm64 libappindicator3-1:arm64 librsvg2-dev:arm64 patchelf:arm64
+RUN apt-get install --assume-yes --no-install-recommends libssl-dev:arm64 libdbus-1-dev:arm64 libsoup2.4-dev:arm64 libssl-dev:arm64 libgtk-3-dev:arm64 webkit2gtk-4.0-dev:arm64 libappindicator3-1:arm64 librsvg2-dev:arm64 patchelf:arm64
ada6cd0c2cf09ee1f50c0714ceb0a2e31723ffe7
Daniel Mader <daniel.mader@impierce.com>
2025-02-25 09:11:25 +0100

    build: bump Android `minSdk` to API level 24

--- crates/tauri-cli/templates/plugin/android/build.gradle.kts
+++ crates/tauri-cli/templates/plugin/android/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 21
+        minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")

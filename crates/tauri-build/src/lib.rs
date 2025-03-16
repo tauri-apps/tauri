00492d17c32ae73986b0ef89d4e86fb0b9e07f48
WofWca <wofwca@protonmail.com>
2025-03-17 00:08:04 +0400

    docs: remove outdated `tauri_build::build` docs
    The docstring has been unchanged ever since
    40ac52971eb350e839f41401043c5cde31fa8974 (2021).
    The `build()` function has grown massivly since then,
    now it's not only about Windows.

--- crates/tauri-build/src/lib.rs
+++ crates/tauri-build/src/lib.rs
@@ -419,18 +419,14 @@ pub fn is_dev() -> bool {
 
 /// Run all build time helpers for your Tauri Application.
 ///
-/// The current helpers include the following:
-/// * Generates a Windows Resource file when targeting Windows.
-///
 /// # Platforms
 ///
-/// [`build()`] should be called inside of `build.rs` regardless of the platform:
-/// * New helpers may target more platforms in the future.
-/// * Platform specific code is handled by the helpers automatically.
-/// * A build script is required in order to activate some cargo environmental variables that are
-///   used when generating code and embedding assets - so [`build()`] may as well be called.
+/// [`build()`] should be called inside of `build.rs` regardless of the platform.
 ///
-/// In short, this is saying don't put the call to [`build()`] behind a `#[cfg(windows)]`.
+/// Platform specific code is handled by the helpers automatically.
+///
+/// A build script is required in order to activate some cargo environmental variables that are
+/// used when generating code and embedding assets.
 ///
 /// # Panics
 ///

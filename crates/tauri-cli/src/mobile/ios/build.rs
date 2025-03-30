61eb23317f03e19dfc2101865e7e39992d13e9e4
Pietagorh <pietagorhh@proton.me>
2025-03-30 18:11:47 +0200

    feat(cli): allow for toml and json5 files in --config arg (#13079)

--- crates/tauri-cli/src/mobile/ios/build.rs
+++ crates/tauri-cli/src/mobile/ios/build.rs
@@ -60,7 +60,7 @@ pub struct Options {
   /// List of cargo features to activate
   #[clap(short, long, action = ArgAction::Append, num_args(0..))]
   pub features: Option<Vec<String>>,
-  /// JSON strings or path to JSON files to merge with the default configuration file
+  /// JSON strings or paths to JSON, JSON5 or TOML files to merge with the default configuration file
   ///
   /// Configurations are merged in the order they are provided, which means a particular value overwrites previous values when a config key-value pair conflicts.
   ///

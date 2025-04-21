208f65157a01101e93fe35f04b800bb8d72f5be9
situ2001 <yongcong2001@outlook.com>
2025-04-21 23:28:20 +0800

    docs: update example for  in webview ts api

--- packages/api/src/webview.ts
+++ packages/api/src/webview.ts
@@ -108,11 +108,23 @@ export type WebviewLabel = string
  *
  * // loading embedded asset:
  * const webview = new Webview(appWindow, 'theUniqueLabel', {
- *   url: 'path/to/page.html'
+ *   url: 'path/to/page.html',
+ *
+ *   // create a webview with specific logical position and size
+ *   x: 0,
+ *   y: 0,
+ *   width: 800,
+ *   height: 600,
  * });
  * // alternatively, load a remote URL:
  * const webview = new Webview(appWindow, 'theUniqueLabel', {
  *   url: 'https://github.com/tauri-apps/tauri'
+ *
+ *   // create a webview with specific logical position and size
+ *   x: 0,
+ *   y: 0,
+ *   width: 800,
+ *   height: 600,
  * });
  *
  * webview.once('tauri://created', function () {
@@ -148,7 +160,13 @@ class Webview {
    * import { Webview } from '@tauri-apps/api/webview'
    * const appWindow = new Window('my-label')
    * const webview = new Webview(appWindow, 'my-label', {
-   *   url: 'https://github.com/tauri-apps/tauri'
+   *   url: 'https://github.com/tauri-apps/tauri',
+   *
+   *   // create a webview with specific logical position and size
+   *   x: 0,
+   *   y: 0,
+   *   width: 800,
+   *   height: 600,
    * });
    * webview.once('tauri://created', function () {
    *  // webview successfully created

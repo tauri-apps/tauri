2a18e647c22d2fa409dd9c0c4e6ff7a6be0fb434
Kingsword <kingsword09@gmail.com>
2025-04-27 10:46:56 +0800

    fix: Allow the use of target dir inside frontendDist/distDir(#13287)

--- crates/tauri-cli/src/build.rs
+++ crates/tauri-cli/src/build.rs
@@ -9,12 +9,12 @@ use crate::{
     app_paths::tauri_dir,
     config::{get as get_config, ConfigHandle, FrontendDist},
   },
-  interface::{AppInterface, Interface},
+  interface::{rust::get_cargo_target_dir, AppInterface, Interface},
   ConfigValue, Result,
 };
 use anyhow::Context;
 use clap::{ArgAction, Parser};
-use std::env::set_current_dir;
+use std::{env::set_current_dir, fs};
 use tauri_utils::platform::Target;
 
 #[derive(Debug, Clone, Parser)]
@@ -172,19 +172,36 @@ pub fn setup(
         ));
     }
 
+    // Issue #13287 - Allow the use of target dir inside frontendDist/distDir
+    // https://github.com/tauri-apps/tauri/issues/13287
+    let mut target_path = get_cargo_target_dir(&options.args)?;
+    target_path = tauri_path.join(target_path);
+    let target_path = fs::canonicalize(target_path)?;
+
     let mut out_folders = Vec::new();
-    for folder in &["node_modules", "src-tauri", "target"] {
-      if web_asset_path.join(folder).is_dir() {
+    if target_path.starts_with(web_asset_path) {
+      // Get the path of target_path relative to web_asset_path
+      if let Ok(relative_path) = target_path.strip_prefix(web_asset_path) {
+        let relative_str = relative_path.to_string_lossy();
+        if !relative_str.is_empty() {
+          out_folders.push(relative_str.to_string());
+        }
+      }
+    }
+
+    for folder in &["node_modules", "src-tauri"] {
+      let sub_path = web_asset_path.join(folder);
+      // If the subfolder exists and its canonicalized path equals the target path, add to out_folders
+      if sub_path.is_dir() && fs::canonicalize(&sub_path)? == target_path {
         out_folders.push(folder.to_string());
       }
     }
     if !out_folders.is_empty() {
       return Err(anyhow::anyhow!(
-          "The configured frontendDist includes the `{:?}` {}. Please isolate your web assets on a separate folder and update `tauri.conf.json > build > frontendDist`.",
-          out_folders,
-          if out_folders.len() == 1 { "folder" } else { "folders" }
-        )
-      );
+        "The configured frontendDist includes the `{:?}` {}. Please isolate your web assets on a separate folder and update `tauri.conf.json > build > frontendDist`.",
+        out_folders,
+        if out_folders.len() == 1 { "folder" } else { "folders" }
+      ));
     }
   }
 

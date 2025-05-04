6e971a7e57a81b9b2eb4379852df073366256878
Miguel Duarte <miguelpduarte98@gmail.com>
2025-05-04 12:49:32 +0200

    Remove unnecesary reference

--- crates/tauri/src/webview/webview_window.rs
+++ crates/tauri/src/webview/webview_window.rs
@@ -136,7 +136,7 @@ impl<'a, R: Runtime, M: Manager<R>> WebviewWindowBuilder<'a, R, M> {
   /// ```
   /// #[tauri::command]
   /// async fn open_window_multiple(app: tauri::AppHandle) {
-  ///   let mut conf = &app.config().app.windows.iter().find(|c| c.label == "template-for-multiwindow").unwrap().clone();
+  ///   let mut conf = app.config().app.windows.iter().find(|c| c.label == "template-for-multiwindow").unwrap().clone();
   ///   // This should be a unique label for all windows. For example, we can use a random UUID:
   ///   conf.label = format!("my-multiwindow-{}", uuid::Uuid::new_v4());
   ///   let webview_window = tauri::WebviewWindowBuilder::from_config(&app, &conf)

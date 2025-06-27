a42a15d16fef10f07a8009a4c3a33140db46d403
FabianLars <github@fabianlars.de>
2025-06-27 17:00:53 +0200

    don't revert everything 2

--- crates/tauri-utils/src/config_v1/mod.rs
+++ crates/tauri-utils/src/config_v1/mod.rs
@@ -722,7 +722,7 @@ pub struct BundleConfig {
   /// The application identifier in reverse domain name notation (e.g. `com.tauri.example`).
   /// This string must be unique across applications since it is used in system configurations like
   /// the bundle ID and path to the webview data directory.
-  /// This string must contain only alphanumeric characters (A–Z, a–z, and 0–9), hyphens (-),
+  /// This string must contain only alphanumeric characters (A-Z, a-z, and 0-9), hyphens (-),
   /// and periods (.).
   pub identifier: String,
   /// The application's publisher. Defaults to the second element in the identifier string.

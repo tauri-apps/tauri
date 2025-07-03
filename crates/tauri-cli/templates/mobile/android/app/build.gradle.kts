52482c73524ba6f388250df5ffc195c4d757f981
owjs3901 <owjs3901@naver.com>
2025-07-03 22:57:54 +0900

    Update compileSdk targetSdk, Fix build cmd

--- crates/tauri-cli/templates/mobile/android/app/build.gradle.kts
+++ crates/tauri-cli/templates/mobile/android/app/build.gradle.kts
@@ -16,13 +16,13 @@ val tauriProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     namespace = "{{app.identifier}}"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "{{app.identifier}}"
         minSdk = {{android.min-sdk-version}}
-        targetSdk = 34
+        targetSdk = 35
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }

ea1d89e2d377831ca93cf291880a179392d9f066
Tony <legendmastertony@gmail.com>
2025-07-24 21:59:31 +0800

    Apparently I'm so used to a higher rust version

--- crates/tauri-cli/src/interface/rust.rs
+++ crates/tauri-cli/src/interface/rust.rs
@@ -425,20 +425,19 @@ fn get_watch_folders(additional_watch_folders: &[PathBuf]) -> crate::Result<Vec<
 
   // Add the additional watch folders, resolving the path from the tauri path if it is relative
   watch_folders.extend(additional_watch_folders.iter().filter_map(|dir| {
-    let path = if dir.is_absolute() {
-      dir
+    if dir.is_absolute() {
+      Some(dir.to_owned())
     } else {
-      &tauri_path.join(dir)
-    };
-
-    let canonicalized = canonicalize(path).ok();
-    if canonicalized.is_none() {
-      log::warn!(
-        "Additional watch folder '{}' not found, ignoring",
-        path.display()
-      );
+      let path = tauri_path.join(dir);
+      let canonicalized = canonicalize(&path).ok();
+      if canonicalized.is_none() {
+        log::warn!(
+          "Additional watch folder '{}' not found, ignoring",
+          path.display()
+        );
+      }
+      canonicalized
     }
-    canonicalized
   }));
 
   // We also try to watch workspace members, no matter if the tauri cargo project is the workspace root or a workspace member

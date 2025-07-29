eb6fe90c16f6561dbfb7b7f2074f1c0d35f46848
Tony <legendmastertony@gmail.com>
2025-07-29 16:23:38 +0800

    Migrate the code to svelte 5 instead

--- crates/tauri-cli/templates/plugin/__example-api/tauri-app/vite.config.js
+++ crates/tauri-cli/templates/plugin/__example-api/tauri-app/vite.config.js
@@ -5,13 +5,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-    compilerOptions:{
-      compatibility:{
-        componentApi: 4
-      }
-    }
-  })],
+  plugins: [svelte()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent Vite from obscuring rust errors

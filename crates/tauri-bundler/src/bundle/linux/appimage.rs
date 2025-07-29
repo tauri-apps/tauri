c53e4b1d02c5c7e1e708862793f1745024cb70df
FabianLars <github@fabianlars.de>
2025-07-29 21:48:54 +0200

    fix(bundler/linux): pull latest appimage plugin

--- crates/tauri-bundler/src/bundle/linux/appimage.rs
+++ crates/tauri-bundler/src/bundle/linux/appimage.rs
@@ -245,6 +245,18 @@ fn prepare_tools(tools_path: &Path, arch: &str) -> crate::Result<PathBuf> {
     write_and_make_executable(&gstreamer, data)?;
   }
 
+  let appimage = tools_path.join("linuxdeploy-plugin-appimage.AppImage");
+  if !appimage.exists() {
+    // This is optional, linuxdeploy will fall back to its built-in version if the download failed.
+    let data = download(&format!("https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/linuxdeploy-plugin-appimage-{arch}.AppImage"));
+    match data {
+      Ok(data) => write_and_make_executable(&appimage, data)?,
+      Err(err) => {
+        log::error!("Download of AppImage plugin failed. Using older built-in version instead.")
+      }
+    }
+  }
+
   // This should prevent linuxdeploy to be detected by appimage integration tools
   let _ = Command::new("dd")
     .args([

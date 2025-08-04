881b64bd2f5f16ef0f8f5441130214bbbbf9972c
FabianLars <github@fabianlars.de>
2025-08-04 15:21:22 +0200

    better? alignment

--- crates/tauri-bundler/src/bundle/windows/sign.rs
+++ crates/tauri-bundler/src/bundle/windows/sign.rs
@@ -214,7 +214,7 @@ pub fn sign_custom<P: AsRef<Path>>(
   let output = cmd.output_ok()?;
 
   let stdout = String::from_utf8_lossy(output.stdout.as_slice()).into_owned();
-  log::info!("{stdout}");
+  log::info!(action = "Signing";"Output of signing command:\n{stdout}");
 
   Ok(())
 }
@@ -233,7 +233,7 @@ pub fn sign_default<P: AsRef<Path>>(path: P, params: &SignParams) -> crate::Resu
   let output = cmd.output_ok()?;
 
   let stdout = String::from_utf8_lossy(output.stdout.as_slice()).into_owned();
-  log::info!("{stdout}");
+  log::info!(action = "Signing";"Output of signing command:\n{stdout}");
 
   Ok(())
 }

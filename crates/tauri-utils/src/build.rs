cc11a91d2dcfb456adebc2672e76068a508a7a0f
Will Evans <will@myndstream.com>
2025-08-13 11:13:12 +0100

    fmt

--- crates/tauri-utils/src/build.rs
+++ crates/tauri-utils/src/build.rs
@@ -86,7 +86,10 @@ fn link_xcode_library(name: &str, source: impl AsRef<std::path::Path>) {
     .join("Products")
     .join(format!("{configuration}-{sdk}"));
 
-  println!("cargo::rustc-link-search=framework={}", lib_out_dir.display());
+  println!(
+    "cargo::rustc-link-search=framework={}",
+    lib_out_dir.display()
+  );
   println!("cargo:include={}", lib_out_dir.display());
   println!("cargo:rerun-if-changed={}", source.display());
   println!("cargo:rustc-link-search=native={}", lib_out_dir.display());

a6cc02fb1124528633f165465ea34e161ab9f45e
Lucas Nogueira <lucas@tauri.app>
2025-08-21 08:00:02 -0300

    install targets on init

--- crates/tauri-cli/src/mobile/init.rs
+++ crates/tauri-cli/src/mobile/init.rs
@@ -171,7 +171,7 @@ pub fn exec(
     Target::OpenHarmony => {
       let (config, _metadata) =
         super::open_harmony::get_config(&app, tauri_config_, None, &Default::default());
-      super::open_harmony::project::gen(&app, &config, (handlebars, map))?;
+      super::open_harmony::project::gen(&app, &config, (handlebars, map), skip_targets_install)?;
       app
     }
   };

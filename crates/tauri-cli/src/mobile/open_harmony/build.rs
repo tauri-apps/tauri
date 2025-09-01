ea31dbd7a147b5b75423c32fa339212c4164b800
Lucas Nogueira <lucas@tauri.app>
2025-09-01 17:30:47 -0300

    fix build

--- crates/tauri-cli/src/mobile/open_harmony/build.rs
+++ crates/tauri-cli/src/mobile/open_harmony/build.rs
@@ -90,6 +90,7 @@ impl From<Options> for BuildOptions {
       ci: options.ci,
       skip_stapling: false,
       ignore_version_mismatches: options.ignore_version_mismatches,
+      no_sign: false,
     }
   }
 }

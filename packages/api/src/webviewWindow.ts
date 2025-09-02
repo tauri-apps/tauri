e06aafbc51c2ff1e2e02a6bd590b4b340ef49fee
KushalMeghani1644 <kushalmeghani108@gmail.com>
2025-09-02 16:08:17 +0530

    Added back the documentation.

--- packages/api/src/webviewWindow.ts
+++ packages/api/src/webviewWindow.ts
@@ -57,7 +57,25 @@ class WebviewWindow {
   /** Local event listeners. */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   listeners: Record<string, Array<EventCallback<any>>>
-
+  /**
+   * Creates a new {@link Window} hosting a {@link Webview}.
+   * @example
+   * ```typescript
+   * import { WebviewWindow } from '@tauri-apps/api/webviewWindow'
+   * const webview = new WebviewWindow('my-label', {
+   *   url: 'https://github.com/tauri-apps/tauri'
+   * });
+   * webview.once('tauri://created', function () {
+   *  // webview successfully created
+   * });
+   * webview.once('tauri://error', function (e) {
+   *  // an error happened creating the webview
+   * });
+   * ```
+   *
+   * @param label The unique webview label. Must be alphanumeric: `a-zA-Z-/:_`.
+   * @returns The {@link WebviewWindow} instance to communicate with the window and webview.
+   */
   constructor(
     label: WebviewLabel,
     options: Omit<WebviewOptions, 'x' | 'y' | 'width' | 'height'>
@@ -84,6 +102,18 @@ class WebviewWindow {
     }
   }
 
+  /**
+   * Gets the Webview for the webview associated with the given label.
+   * @example
+   * ```typescript
+   * import { Webview } from '@tauri-apps/api/webviewWindow';
+   * const mainWebview = Webview.getByLabel('main');
+   * ```
+   *
+   * @param label The webview label.
+   * @returns The Webview instance to communicate with the webview or null if the webview doesn't exist.
+   */
+
   static async getByLabel(label: string): Promise<WebviewWindow | null> {
     const webview =
       (await getAllWebviewWindows()).find((w) => w.label === label) ?? null
@@ -93,6 +123,10 @@ class WebviewWindow {
     return null
   }
 
+  /**
+   * Get an instance of `Webview` for the current webview.
+   */
+
   static getCurrent(): WebviewWindow {
     return getCurrentWebviewWindow()
   }
@@ -100,6 +134,25 @@ class WebviewWindow {
   static async getAll(): Promise<WebviewWindow[]> {
     return getAllWebviewWindows()
   }
+  /**
+   * Listen to an emitted event on this webivew window.
+   *
+   * @example
+   * ```typescript
+   * import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
+   * const unlisten = await WebviewWindow.getCurrent().listen<string>('state-changed', (event) => {
+   *   console.log(`Got error: ${payload}`);
+   * });
+   *
+   * // you need to call unlisten if your handler goes out of scope e.g. the component is unmounted
+   * unlisten();
+   * ```
+   *
+   * @param event Event name. Must include only alphanumeric characters, `-`, `/`, `:` and `_`.
+   * @param handler Event handler.
+   * @returns A promise resolving to a function to unlisten to the event.
+   * Note that removing the listener is required if your listener goes out of scope e.g. the component is unmounted.
+   */
 
   async listen<T>(
     event: EventName,
@@ -117,6 +170,26 @@ class WebviewWindow {
     })
   }
 
+  /**
+   * Listen to an emitted event on this webview window only once.
+   *
+   * @example
+   * ```typescript
+   * import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
+   * const unlisten = await WebviewWindow.getCurrent().once<null>('initialized', (event) => {
+   *   console.log(`Webview initialized!`);
+   * });
+   *
+   * // you need to call unlisten if your handler goes out of scope e.g. the component is unmounted
+   * unlisten();
+   * ```
+   *
+   * @param event Event name. Must include only alphanumeric characters, `-`, `/`, `:` and `_`.
+   * @param handler Event handler.
+   * @returns A promise resolving to a function to unlisten to the event.
+   * Note that removing the listener is required if your listener goes out of scope e.g. the component is unmounted.
+   */
+
   async once<T>(
     event: EventName,
     handler: EventCallback<T>
@@ -128,9 +201,28 @@ class WebviewWindow {
         if (idx >= 0) listeners.splice(idx, 1)
       }
     }
-    return once(event, handler, {
-      target: { kind: 'WebviewWindow', label: this.label }
-    })
+    return once(
+      event,
+      handler,
+      /**
+       * Set the window and webview background color.
+       *
+       * #### Platform-specific:
+       *
+       * - **Android / iOS:** Unsupported for the window layer.
+       * - **macOS / iOS**: Not implemented for the webview layer.
+       * - **Windows**:
+       *   - alpha channel is ignored for the window layer.
+       *   - On Windows 7, alpha channel is ignored for the webview layer.
+       *   - On Windows 8 and newer, if alpha channel is not `0`, it will be ignored.
+       *
+       * @returns A promise indicating the success or failure of the operation.
+       *
+       * @since 2.1.0
+       */ {
+        target: { kind: 'WebviewWindow', label: this.label }
+      }
+    )
   }
 
   /**

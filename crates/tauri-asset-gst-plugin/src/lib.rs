a3b9a76ea7d00aee518033eec4199a3e16e0efec
Siirko <contact@yanovskyy.com>
2025-12-23 18:17:40 +0100

    fix: MIT to MIT/X11 license for gstreamer plugin

--- crates/tauri-asset-gst-plugin/src/lib.rs
+++ crates/tauri-asset-gst-plugin/src/lib.rs
@@ -12,7 +12,7 @@ gst::plugin_define!(
   env!("CARGO_PKG_DESCRIPTION"),
   plugin_init,
   concat!(env!("CARGO_PKG_VERSION"), "-", env!("COMMIT_ID")),
-  "MIT",
+  "MIT/X11",
   env!("CARGO_PKG_NAME"),
   env!("CARGO_PKG_NAME"),
   env!("CARGO_PKG_REPOSITORY"),

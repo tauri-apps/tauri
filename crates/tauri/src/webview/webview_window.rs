e6b376bf0663b624c7fe097e479ce73c9fc67aad
lanyeeee <lanyeeeeee@gmail.com>
2026-02-11 00:06:18 +0800

    feat: add `eval_with_callback` to Webview and WebviewWindow

--- crates/tauri/src/webview/webview_window.rs
+++ crates/tauri/src/webview/webview_window.rs
@@ -2324,6 +2324,20 @@ impl<R: Runtime> WebviewWindow<R> {
     self.webview.eval(js)
   }
 
+  /// Evaluate JavaScript with callback function on this window.
+  /// The evaluation result will be serialized into a JSON string and passed to the callback function.
+  ///
+  /// Exception is ignored because of the limitation on windows. You can catch it yourself and return as string as a workaround.
+  ///
+  /// - **Android:** Not implemented yet.
+  pub fn eval_with_callback(
+    &self,
+    js: impl Into<String>,
+    callback: impl Fn(String) + Send + 'static,
+  ) -> crate::Result<()> {
+    self.webview.eval_with_callback(js, callback)
+  }
+
   /// Opens the developer tools window (Web Inspector).
   /// The devtools is only enabled on debug builds or with the `devtools` feature flag.
   ///

2c88eb19c13a49b714bf728390048096d208af6f
Chip Reed <chip@flightscience.ai>
2026-03-05 16:42:19 -0800

    refactor WindowsStore: simplify method implementations and reduce duplication

--- crates/tauri-runtime-wry/src/windows_store.rs
+++ crates/tauri-runtime-wry/src/windows_store.rs
@@ -4,7 +4,7 @@
 
 use crate::WindowWrapper;
 use std::{
-  cell::{BorrowError, BorrowMutError, Ref, RefCell, RefMut},
+  cell::{BorrowError, BorrowMutError, RefCell},
   collections::BTreeMap,
   fmt,
   fmt::Formatter,
@@ -43,45 +43,51 @@ impl From<Error> for tauri_runtime::Error {
 pub struct WindowsStore(RefCell<BTreeMap<WindowId, WindowWrapper>>);
 
 impl WindowsStore {
-  pub fn window<F, T>(&self, window: WindowId, f: F) -> Result<T>
+  pub fn window<F, T>(&self, id: WindowId, f: F) -> Result<T>
   where
     F: FnOnce(&WindowWrapper) -> T,
   {
-    let store = self.try_store()?;
-    let window = store.get(&window).ok_or(Error::WindowNotFound(window))?;
+    let store = self.0.try_borrow().map_err(Error::Borrow)?;
+    let window = store.get(&id).ok_or(Error::WindowNotFound(id))?;
     Ok(f(window))
   }
 
-  pub fn window_mut<F, T>(&self, window: WindowId, f: F) -> Result<T, Error>
+  pub fn window_mut<F, T>(&self, id: WindowId, f: F) -> Result<T>
   where
     F: FnOnce(&mut WindowWrapper) -> T,
   {
-    let mut store = self.try_store_mut()?;
-    let window = store
-      .get_mut(&window)
-      .ok_or(Error::WindowNotFound(window))?;
+    let mut store = self.0.try_borrow_mut().map_err(Error::BorrowMut)?;
+    let window = store.get_mut(&id).ok_or(Error::WindowNotFound(id))?;
     Ok(f(window))
   }
 
-  pub fn store<F, T>(&self, f: F) -> Result<T, Error>
+  pub fn store<F, T>(&self, f: F) -> Result<T>
   where
     F: FnOnce(&WindowMap) -> T,
   {
     self.0.try_borrow().map(|s| f(&s)).map_err(Error::Borrow)
   }
 
-  pub fn store_mut<F, T>(&self, f: F) -> Result<T, Error>
+  pub fn store_mut<F, T>(&self, f: F) -> Result<T>
   where
     F: FnOnce(&mut WindowMap) -> T,
   {
-    self.try_store_mut().map(|mut s| f(&mut s))
+    self
+      .0
+      .try_borrow_mut()
+      .map(|mut s| f(&mut s))
+      .map_err(Error::BorrowMut)
   }
 
-  fn try_store_mut(&self) -> Result<RefMut<'_, WindowMap>, Error> {
-    self.0.try_borrow_mut().map_err(Error::BorrowMut)
+  pub fn insert(&self, id: WindowId, window: WindowWrapper) -> Result<Option<WindowWrapper>> {
+    self.store_mut(|s| s.insert(id, window))
   }
 
-  fn try_store(&self) -> Result<Ref<'_, WindowMap>, Error> {
-    self.0.try_borrow().map_err(Error::Borrow)
+  /// Removes the window from the store and return `true` if the store is now empty.
+  pub fn remove(&self, id: WindowId) -> Result<bool> {
+    self.store_mut(|s| {
+      s.remove(&id);
+      s.is_empty()
+    })
   }
 }

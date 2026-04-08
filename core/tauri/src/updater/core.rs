cb26a918758daeeab0dc41f38615011e620da543
Fabian-Lars <30730186+FabianLars@users.noreply.github.com>
2026-04-08 21:15:37 +0200

    allow deprecated

--- core/tauri/src/updater/core.rs
+++ core/tauri/src/updater/core.rs
@@ -921,7 +921,8 @@ fn copy_files_and_run(
     },
   };
 
-  let temp_dir = tempfile::Builder::new().tempdir()?.keep();
+  #[allow(deprecated)]
+  let temp_dir = tempfile::Builder::new().tempdir()?.into_path();
 
   let updater_type = if infer::archive::is_zip(bytes) {
     let archive = Cursor::new(bytes);

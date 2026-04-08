f2ab5d65444e6279793e56f4dcb520b727303849
Kokoro2336 <2529677678@qq.com>
2026-04-08 15:02:13 +0800

    chore: update comments.

--- crates/tauri-bundler/src/bundle/settings.rs
+++ crates/tauri-bundler/src/bundle/settings.rs
@@ -472,6 +472,7 @@ pub struct NsisSettings {
   pub installer_icon: Option<PathBuf>,
   /// The path to an icon file used as the uninstaller icon.
   pub uninstaller_icon: Option<PathBuf>,
+  /// The path to a bitmap file to display on the header of uninstallers pages.
   /// Defaults to [`Self::header_image`]. If this is set but [`Self::header_image`] is not, a default image from NSIS will be applied to `header_image`
   ///
   /// The recommended dimensions are 150px x 57px.

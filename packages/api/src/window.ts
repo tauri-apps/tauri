73e2fc8a6b91ef97021ec7ffa9907b2d4fdf79fc
Fabian-Lars <30730186+FabianLars@users.noreply.github.com>
2026-07-01 12:11:07 +0200

    gremlin

--- packages/api/src/window.ts
+++ packages/api/src/window.ts
@@ -1448,7 +1448,7 @@ class Window {
   }
 
   /**
-   * On macOS, Toggles a fullscreen mode that doesn’t require a new macOS space. Returns a boolean indicating whether the transition was successful (this won't work if the window was already in the native fullscreen).
+   * On macOS, Toggles a fullscreen mode that doesn't require a new macOS space. Returns a boolean indicating whether the transition was successful (this won't work if the window was already in the native fullscreen).
    * This is how fullscreen used to work on macOS in versions before Lion. And allows the user to have a fullscreen window without using another space or taking control over the entire monitor.
    *
    * On other platforms, this is the same as {@link Window.setFullscreen}.

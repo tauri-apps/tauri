96014b638f6d369865fda02f5d7bce68ba6ecb36
Tony <68118705+Legend-Master@users.noreply.github.com>
2026-07-02 18:19:23 +0800

    fix: mock runtime run on main thread dead locks (#15450)
    * fix: mock runtime run on main thread dead locks
    
    * Try testing API example
    
    * Revert "Try testing API example"
    
    This reverts commit 1d04b9f50fbdc66b3866f56c03a8fb67fcb12c80.
    
    * Revert API example changes
    
    * Run unit tests in examples
    
    * Revert `not(test)`
    
    * Split out the example tests
    
    * Condition got reversed
    
    * Only android uses cross
    
    * Test api example
    
    * Install cross through `taiki-e/install-action@v2`
    
    * Install through cargo binstall instead
    
    * Revert "Install through cargo binstall instead"
    
    This reverts commit 2aec3905b4437c5e6c1f5b24b08db98fb63ef5cc.
    
    * Revert "Install cross through `taiki-e/install-action@v2`"
    
    This reverts commit b88035ac9a21c76c84cbd71a794ba8613fb79a57.
    
    * Add change file

--- examples/api/src-tauri/src/tray.rs
+++ examples/api/src-tauri/src/tray.rs
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-#![cfg(all(desktop, not(test)))]
-
 use std::sync::atomic::{AtomicBool, Ordering};
 use tauri::{
   include_image,

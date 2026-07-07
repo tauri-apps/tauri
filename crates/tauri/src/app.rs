548d67224e958a938d5de3fb207a6178fee42a71
Tony <legendmastertony@gmail.com>
2026-07-07 19:25:53 +0800

    ios clippy

--- crates/tauri/src/app.rs
+++ crates/tauri/src/app.rs
@@ -679,7 +679,7 @@ impl<R: Runtime> AppHandle<R> {
   #[cfg(target_os = "ios")]
   pub fn supports_multiple_windows(&self) -> bool {
     let (tx, rx) = std::sync::mpsc::channel();
-    self.run_on_main_thread(move || unsafe {
+    let _ = self.run_on_main_thread(move || unsafe {
       let mtm = objc2::MainThreadMarker::new().unwrap();
       let ui_application = objc2_ui_kit::UIApplication::sharedApplication(mtm);
       tx.send(ui_application.supportsMultipleScenes()).unwrap();

5ab317504aa2a77cbdaf46341456ff26f13861f6
Tony <68118705+Legend-Master@users.noreply.github.com>
2026-07-13 19:24:32 +0800

    refactor: migrate to svelte ts for API example (#15675)
    * Migrate to svelte ts for API example
    
    * Clean up
    
    * Use `$derived` `selectedWebview`
    
    * Do the change in onchange instead of state effects
    
    * Set initial x y and sizes
    
    * revert ai slop on main.ts
    
    * More cleanup
    
    * Remove ConsoleMessage
    
    * Remove View type
    
    * license header
    
    * Use `ViewProps`
    
    * Use same button spacing through out
    
    * Move theme example to top level theme switch
    
    * Fix menu views
    
    * More cleanups
    
    * Final cleanup
    
    * Move types to svelte files
    
    * fallback to empty string not false

--- examples/api/src/views/Tray.svelte
+++ examples/api/src/views/Tray.svelte
@@ -1,18 +1,22 @@
-<script>
+<script lang="ts">
   import { TrayIcon } from '@tauri-apps/api/tray'
-  import MenuBuilder from '../components/MenuBuilder.svelte'
+  import MenuBuilder, {
+    type BuiltMenuItem,
+    type MenuItemClickDetail
+  } from '../components/MenuBuilder.svelte'
   import { Menu } from '@tauri-apps/api/menu'
+  import type { ViewProps } from '../App.svelte'
 
-  let { onMessage } = $props()
+  let { onMessage }: ViewProps = $props()
 
-  let icon = $state(null)
-  let tooltip = $state(null)
-  let title = $state(null)
+  let icon = $state<string>()
+  let tooltip = $state<string>()
+  let title = $state<string>()
   let iconAsTemplate = $state(false)
   let menuOnLeftClick = $state(true)
-  let menuItems = $state([])
+  let menuItems = $state<BuiltMenuItem[]>([])
 
-  function onItemClick(detail) {
+  function onItemClick(detail: MenuItemClickDetail) {
     onMessage(`Item ${detail.text} clicked`)
   }
 

56dcc2a2e78acc95a0d057341d325341b8b32559
Tony <legendmastertony@gmail.com>
2026-07-15 18:14:03 +0800

    no border in dark mode

--- examples/api/src/components/MenuItemComponent.svelte
+++ examples/api/src/components/MenuItemComponent.svelte
@@ -34,7 +34,7 @@
 </script>
 
 <div
-  class="flex items-center gap-2 border border-solid border-neutral-200 dark:border-neutral-700 bg-neutral-100 dark:bg-neutral-700 p-inline-2 p-block-1 rounded-md select-none menu-item"
+  class="flex items-center gap-2 border border-solid border-neutral-200 dark:border-none bg-neutral-100 dark:bg-neutral-700 p-inline-2 p-block-1 rounded-md select-none menu-item"
   data-kind={kind}
   data-text={text}
   data-icon-path={iconPath}

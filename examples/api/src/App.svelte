62dc8398f691ce90d7b7fe0e13d446fcb3b0474b
Tony <legendmastertony@gmail.com>
2026-07-16 20:16:52 +0800

    chore(example): set `color-cheme`
    This allows the unstyled user agent elements match the selected theme colors

--- examples/api/src/App.svelte
+++ examples/api/src/App.svelte
@@ -102,9 +102,13 @@
 
   function applyTheme() {
     const isDark = theme === 'auto' ? preferDark.current : theme === 'dark'
-    isDark
-      ? document.documentElement.classList.add('dark')
-      : document.documentElement.classList.remove('dark')
+    if (isDark) {
+      document.documentElement.classList.add('dark')
+      document.documentElement.classList.remove('light')
+    } else {
+      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.add('light')
+    }
     setTheme(theme === 'auto' ? null : theme)
     localStorage.setItem('theme', theme)
   }
@@ -240,7 +244,7 @@
   id="sidebarToggle"
   bind:this={sidebarToggle}
   class="z-2000 hidden lt-sm:flex justify-center absolute items-center w-8 h-8 rd-8
-            bg-accent dark:bg-darkAccent active:bg-accentDark dark:active:bg-darkAccentDark"
+            bg-accent dark:bg-darkAccent active:bg-accentDark dark:active:bg-darkAccentDark text-accentText dark:text-darkAccentText"
 >
   {#if isSideBarOpen}
     <span class="i-codicon-close animate-duration-300ms animate-fade-in"></span>

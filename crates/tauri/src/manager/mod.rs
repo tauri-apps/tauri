0bf71877259fb351bc7291ed0c6c4dcaf9b1fff5
Tony <68118705+Legend-Master@users.noreply.github.com>
2026-07-16 19:48:22 +0800

    refactor: rename `extend_api` to `run_invoke_handler` (#15731)
    * refactor: rename `extend_api` to `run_invoke_handler`
    
    * Link docs
    
    * unwrap_or_else

--- crates/tauri/src/manager/mod.rs
+++ crates/tauri/src/manager/mod.rs
@@ -472,12 +472,15 @@ impl<R: Runtime> AppManager<R> {
     (self.webview.invoke_handler)(invoke)
   }
 
-  pub fn extend_api(&self, plugin: &str, invoke: Invoke<R>) -> bool {
+  /// Runs the plugin [`crate::plugin::Plugin::extend_api`] hook if it exists. Returns whether the invoke message was handled or not.
+  ///
+  /// The message is not handled when the plugin exists **and** the command does not.
+  pub fn run_plugin_invoke_handler(&self, plugin: &str, invoke: Invoke<R>) -> bool {
     self
       .plugins
       .lock()
       .expect("poisoned plugin store")
-      .extend_api(plugin, invoke)
+      .run_invoke_handler(plugin, invoke)
   }
 
   pub fn initialize_plugins(&self, app: &AppHandle<R>) -> crate::Result<()> {

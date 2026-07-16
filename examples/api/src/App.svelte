070381f93f2479e05fc2f083c63c2aaa1125b484
Tony <legendmastertony@gmail.com>
2026-07-16 11:09:31 +0800

    Revert back to margin-top: 0.5rem

--- examples/api/src/App.svelte
+++ examples/api/src/App.svelte
@@ -239,7 +239,7 @@
 <div
   id="sidebarToggle"
   bind:this={sidebarToggle}
-  class="z-2000 hidden lt-sm:flex justify-center items-center absolute top-2 left-2 w-8 h-8 rd-8
+  class="z-2000 hidden lt-sm:flex justify-center absolute items-center w-8 h-8 rd-8
             bg-accent dark:bg-darkAccent active:bg-accentDark dark:active:bg-darkAccentDark"
 >
   {#if isSideBarOpen}

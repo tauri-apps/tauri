ac124e874ae9b0ceec5b3f2721d8dffdd3fc789b
Tony <legendmastertony@gmail.com>
2026-08-19 17:03:20 +0800

    implement `display_handle` with the wrong lifetime

--- crates/tauri-runtime-wry/src/lib.rs
+++ crates/tauri-runtime-wry/src/lib.rs
@@ -16,7 +16,7 @@ use self::monitor::MonitorExt;
 use http::Request;
 #[cfg(target_os = "macos")]
 use objc2::ClassType;
-use raw_window_handle::{DisplayHandle, HasWindowHandle};
+use raw_window_handle::{DisplayHandle, HasDisplayHandle, HasWindowHandle};
 
 #[cfg(windows)]
 use tauri_runtime::webview::ScrollBarStyle;
@@ -2086,7 +2086,12 @@ impl<T: UserEvent> WindowDispatch<T> for WryWindowDispatcher<T> {
   ) -> std::result::Result<raw_window_handle::WindowHandle<'_>, raw_window_handle::HandleError> {
     get_raw_window_handle(self)
       .map_err(|_| raw_window_handle::HandleError::Unavailable)
-      .and_then(|r| r.map(|h| unsafe { raw_window_handle::WindowHandle::borrow_raw(h.0) }))
+      .and_then(|r| {
+        r.map(|h| {
+          // SAFETY: the lifetime is wrong here!
+          unsafe { raw_window_handle::WindowHandle::borrow_raw(h.0) }
+        })
+      })
   }
 
   // Setters
@@ -2741,8 +2746,16 @@ impl<T: UserEvent> RuntimeHandle<T> for WryHandle<T> {
   fn display_handle(
     &self,
   ) -> std::result::Result<DisplayHandle<'_>, raw_window_handle::HandleError> {
-    todo!();
-    // self.context.main_thread.window_target.display_handle()
+    let window_target = self
+      .context
+      .main_thread
+      .window_target
+      .upgrade()
+      .ok_or(raw_window_handle::HandleError::Unavailable)?;
+    window_target.display_handle().map(|handle| {
+      // SAFETY: the lifetime is wrong here, but the alternatives are worse, see https://github.com/tauri-apps/tauri/pull/15411
+      unsafe { raw_window_handle::DisplayHandle::borrow_raw(handle.as_raw()) }
+    })
   }
 
   fn primary_monitor(&self) -> Result<Option<Monitor>> {

a98c9b3930ef226e9808cee4977d028073451021
junaidmirr <89933278+junaidmirr@users.noreply.github.com>
2026-09-09 22:02:24 +0530

    fixed license header

--- crates/tauri-cli/tests/init.rs
+++ crates/tauri-cli/tests/init.rs
@@ -1,5 +1,6 @@
 // Copyright 2019-2024 Tauri Programme within The Commons Conservancy
-// SPDX-License-Identifier: Apache-2.0 OR MIT
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 
 use std::fs;
 use std::process::Command;

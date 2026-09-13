1fbadc8776b71c94ed858ba18721409e45696ff3
sftse <c@farsight.net>
2026-09-13 17:11:06 +0200

    Arc/Mutex refactors (#15952)
    * refactor(tauri): dont pass state manager separately if can get from webview
    
    * refactor(tauri): remove Mutex in build.rs
    
    * refactor(tauri): use env::var_os if possible
    
    * refactor(tauri): cleanup build script
    
    * Add change file
    
    * address review comments, reference unit test
    
    * fix!: add deprecation warnings
    
    * expand change file to cover method deprecation
    
    * Update .changes/change-pr-15952.md

--- crates/tauri/src/state.rs
+++ crates/tauri/src/state.rs
@@ -59,6 +59,7 @@ impl<T: std::fmt::Debug> std::fmt::Debug for State<'_, T> {
 impl<'r, 'de: 'r, T: 'static, R: Runtime> CommandArg<'de, R> for State<'r, T> {
   /// Grabs the [`State`] from the [`CommandItem`]. This will never fail.
   fn from_command(command: CommandItem<'de, R>) -> Result<Self, InvokeError> {
+    #[allow(deprecated)]
     command.message.state_ref().try_get().ok_or_else(|| {
       InvokeError::from_anyhow(anyhow::anyhow!(
         "state not managed for field `{}` on command `{}`. You must call `.manage()` before using this command",

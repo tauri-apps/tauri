e8d17c19d9cbd0086dcc5db784a288db12171b64
Lucas Nogueira <lucas@tauri.app>
2026-09-26 13:13:22 -0300

    fix(tauri-utils): doctests

---
+++ crates/tauri-utils/build.rs
@@ -0,0 +1,7 @@
+// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+// `tauri::generate_context!` writes its output to `OUT_DIR`, which Cargo only sets for the doc tests
+// of packages that have a build script.
+fn main() {}
---
+++ crates/tauri-utils/test/fixture/dist/index.html
@@ -0,0 +1,4 @@
+<html>
+  <head></head>
+  <body></body>
+</html>
